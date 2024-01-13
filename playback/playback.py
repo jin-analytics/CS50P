1d10503984f254a3526818bc2b2a3778253efaee
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-01-13 10:33:58 +0000

    Sat, Jan 13, 2024, 11:33 AM +01:00

--- playback/playback.py
+++ playback/playback.py
@@ -3,7 +3,7 @@ sentence=input('Write "This is CS50"\n')
 
 # Use a string method to add "..." between white space
 #sentence = sentence.split(' ')
-sentence = sentence.replace(' ',)
+sentence = sentence.replace(' ', '...')
 
 # Print the new sentence
 print(f"{sentence}")
