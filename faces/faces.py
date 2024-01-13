c7f60b5fa24b6050d5c328745509923700d247f7
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-01-13 11:58:18 +0000

    Sat, Jan 13, 2024, 12:58 PM +01:00

--- faces/faces.py
+++ faces/faces.py
@@ -1,16 +1,13 @@
 # Create a function called convert
-def convert(face):
+def convert():
 
-    #hello = input()
-    print(face)
-    # Replace the smiley ":)" and ":("
+    hello = input()
+
+    # Replace the smiley ":)" and ":(", then print it
     # Slightly Smiling Faces - Unicode: "U+1F642" - In Python: "\U0001F642"
     # Slightly Frowning Faces - Unicode: "U+1F641" - In Python: "\U0001F641"
     hello = hello.replace(f":)","\U0001F642").replace(f":(","\U0001F641")
     print(hello)
 
-    # print out smiley with converted
-face=input() #1 Input "Hello :)"
-convert(face) #2 Gives "Hello :)" to function convert()
-
+convert()
 
