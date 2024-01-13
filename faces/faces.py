0e26629d865189b45b1392016eea8ada472ea372
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-01-13 11:40:04 +0000

    Sat, Jan 13, 2024, 12:40 PM +01:00

--- faces/faces.py
+++ faces/faces.py
@@ -1,12 +1,13 @@
 # Create a function called convert
-def convert(smile):
+def convert():
 
     hello = input()
     #Slightly Smiling Faces - Unicode: "U+1F642" - In Python: "\U0001F642"
     hello = hello.replace(f":)","\U0001F642")
     #Slightly Frowning Faces - Unicode: "U+1F641" - In Python: "\U0001F641"
-    hello = hello.replace(f":(","\U0001F641")
+    #hello = hello.replace(f":(","\U0001F641")
+    print(hello)
 
 convert()
-print(smile)
+
 
