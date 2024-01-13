409804d383a3daf6dc8b674acfbf7601384943bf
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-01-13 16:08:19 +0000

    Sat, Jan 13, 2024, 5:08 PM +01:00

--- tip/tip.py
+++ tip/tip.py
@@ -6,17 +6,20 @@ def main():
 
 
 def dollars_to_float(d):
-    # string input which has to replace the "$"
+    # string input which has to remove the "$"
     d=d.replace('$','')
     # string input which has to be converted to a float number
     d=float(d)
+    # return the value "d" which is a float number now
     return d
 
 
 def percent_to_float(p):
-    # string input which has to be converted to a float number and divided to %
+    # string input which has to remove the "%"
     p=p.replace('%','')
+    # string input which has to be converted to a float number and divided to
     p=float(p)/100
+    # return the value "d" which is a float number now
     return p
 
 
