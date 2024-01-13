b803c6096c03d3afa28a38661d625cfad7067afb
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-01-13 11:55:05 +0000

    Sat, Jan 13, 2024, 12:55 PM +01:00

--- faces/faces.py
+++ faces/faces.py
@@ -2,11 +2,12 @@
 def convert(face):
 
     #hello = input()
-
+    print(face)
     # Replace the smiley ":)" and ":("
     # Slightly Smiling Faces - Unicode: "U+1F642" - In Python: "\U0001F642"
     # Slightly Frowning Faces - Unicode: "U+1F641" - In Python: "\U0001F641"
     hello = hello.replace(f":)","\U0001F642").replace(f":(","\U0001F641")
+    print(hello)
 
     # print out smiley with converted
 face=input() #1 Input "Hello :)"
