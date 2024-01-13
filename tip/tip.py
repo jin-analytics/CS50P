84c1836cbeab52daeed6bd555e84c705cd83a66e
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-01-13 16:00:09 +0000

    Sat, Jan 13, 2024, 5:00 PM +01:00

--- tip/tip.py
+++ tip/tip.py
@@ -1,28 +1,27 @@
 def main():
     dollars = dollars_to_float(input("How much was the meal? "))
     percent = percent_to_float(input("What percentage would you like to tip? "))
-    tip = dollars * percent
-    print(f"Leave ${tip:.2f}")
+
+
+    #tip = dollars * percent
+    #print(f"Leave ${tip:.2f}")
 
 
 def dollars_to_float(d):
-    # string input which has to be converted to a float number
-    dollars = d
     # string input which has to replace the "$"
-    dollars=dollars.replace('$','')
+    d=d.replace('$','')
     # string input which has to be converted to a float number
-    dollars=float(dollars)
-    print (dollars)
-    d=dollars
+    d=float(d)
+    print (d)
+
 
 
 def percent_to_float(p):
-    print(p)
-    percent=p
-    percent=float(p)
-    percent=percent/100
-    print (percent)
-    p=percent
+    # string input which has to be converted to a float number and divided to %
+    p=float(p)/100
+    print (p)
+    tip=p*50
+    print (tip)
 
 
 
