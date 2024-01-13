556fa657b7bf45266d146fa798e77582c5e2e6a8
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-01-13 11:14:23 +0000

    Sat, Jan 13, 2024, 12:14 PM +01:00

--- faces/faces.py
+++ faces/faces.py
@@ -1,9 +1,11 @@
 # Create a function called convert
-def convert():
-    #Slightly Smiling Faces - Unicode: "U+1F642" - Shortcode :slightly_smiling_face:
-    #Slightly Smiling Faces - Unicode: "U+1F642"
+#def convert():
 
+    #Slightly Smiling Faces - Unicode: "U+1F642" - Shortcode ":slightly_smiling_face:"
+
+    #Slightly Frowning Faces - Unicode: "U+1F641" - Shortcode ":slightly_frowning_face:"
 
 
 input()
-#print(f"hello :)")
+
+print()
