cabfcd6504037bc7c86ca74835afc6226dd724ee
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-01-15 11:50:47 +0000

    Mon, Jan 15, 2024, 12:50 PM +01:00

--- ProblemSet_1/deep/deep.py
+++ ProblemSet_1/deep/deep.py
@@ -2,13 +2,19 @@
 question = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
 print(question[0])
 
+if question[0] == " ":
+    print ("ü")
+
 # use match function for different input cases
 match question:
     # if the input is "42", "forty two" or "forty-two", then go into this case and print "yes"
     case "42" | "forty two" | "forty-two":
         print ("yes")
     # if the input start with a " ", then go into this case
-   # case question[0] == " ":
-    #    question = question.replace(" ","")
-    #    test = question
-    #    print (test)
+    case _:
+        if question[0] == " ":
+        question = question.replace(" ","")
+        print ("yes")
+        question = question.replace(" ","")
+        test = question
+        print (test)
