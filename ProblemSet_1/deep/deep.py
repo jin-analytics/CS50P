ca946befe0c2f62b42a0e39c5126cadfd9dc3d18
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-01-15 11:27:38 +0000

    Mon, Jan 15, 2024, 12:27 PM +01:00

--- ProblemSet_1/deep/deep.py
+++ ProblemSet_1/deep/deep.py
@@ -6,6 +6,8 @@ match question:
         print ("yes")
     case _:
         question = question.replace(" ","").lower()
+        test = question
+        print (test)
         if question == "42":
             print ("yes")
         elif question == "forty two":
