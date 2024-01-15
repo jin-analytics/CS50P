ad9ec066870b603b3a1eccf604f41e55f44c23b3
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-01-15 15:45:07 +0000

    Mon, Jan 15, 2024, 4:45 PM +01:00

--- bank/bank.py
+++ bank/bank.py
@@ -1,12 +1,12 @@
 # Get whatever greeting
 greeting = input()
-greeting = greeting[0:2].replace(' ','')
+greeting = greeting[0:2].replace(' ','').lower()
 
 match greeting:
     case "hello" | "Hello":
         print("$0")
     case _:
-        if greeting[0] == "h" | "H":
+        if greeting[0] == "h":
             print("$20")
         else:
             print("$100")
