71d291907c73626fafe7193394a242149b613c59
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-01-15 11:59:51 +0000

    Mon, Jan 15, 2024, 12:59 PM +01:00

--- ProblemSet_1/deep/deep.py
+++ ProblemSet_1/deep/deep.py
@@ -1,26 +1,30 @@
 # Get input
 question = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
-print(question[0])
-
-if question[0] == " ":
-    print ("ü")
 
 # use match function for different input cases
 match question:
+
     # if the input is "42", "forty two" or "forty-two", then go into this case and print "yes"
     case "42" | "forty two" | "forty-two":
         print ("yes")
+
     # if the input start with a " ", then go into this case
     case _:
         # All letters of question will be lowered
         question = question.lower()
+
         # if the input start with a " ", then go into this case
         if question[0] == " ":
-        question = question.replace(" ","")
+
+            # Replace all " " with no spaces
+            question = question.replace(" ","")
             if question == "42"
             print ("yes")
+
+        # If the input "Forty Two" had caps on, then they are lowered now and can be detected
         elif question == "forty two":
         print ("yes")
+        # If still no match, then print "no"
         else:
         print ("no")
 
