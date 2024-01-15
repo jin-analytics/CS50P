4ab1b5190657aa17202fdff1ed789a603b75ba9d
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-01-15 20:32:07 +0000

    Mon, Jan 15, 2024, 9:32 PM +01:00

--- bank/bank.py
+++ bank/bank.py
@@ -8,10 +8,11 @@ match greeting:
     case ["Hello," | "Hello" | "hello", *rest]:
         print("$0")
     case _:
+        greeting = ' '.join(greeting)
         if greeting[0] == "H":
             print("$20")
         else:
             print("$100")
 
-greeting = ' '.join(greeting)
+
 
