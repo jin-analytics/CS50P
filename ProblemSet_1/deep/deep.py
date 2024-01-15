c28bdb2424faa39df243445dac1dc82522773ec3
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-01-15 10:44:38 +0000

    Mon, Jan 15, 2024, 11:44 AM +01:00

--- ProblemSet_1/deep/deep.py
+++ ProblemSet_1/deep/deep.py
@@ -1,12 +1,11 @@
 # Get input
 question = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
 
-def fortytwo(input):
-    match input:
-    case "42" | "forty-two" | "forty two"
-    input = true
-
-    case _:
-    input = false
-
-    return input
+if "42":
+    print ("yes")
+elif "forty-two":
+    print ("yes")
+elif "forty two":
+    print ("yes")
+else
+    print ("no")
