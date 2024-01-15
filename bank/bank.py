e0da5d9af9a256ce50f9175068db305690d5c296
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-01-15 15:23:33 +0000

    Mon, Jan 15, 2024, 4:23 PM +01:00

--- bank/bank.py
+++ bank/bank.py
@@ -2,7 +2,7 @@
 greeting = input()
 
 match greeting:
-case greeting == "hello":
-    print("$0")
-case greeting[0] == "h":
-    print("$20")
+    case greeting == "hello":
+        print("$0")
+    case greeting[0] == "h":
+        print("$20")
