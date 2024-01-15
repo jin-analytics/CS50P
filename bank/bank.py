55a6ee587864d797bf1cb7d4d303f4fd7ec76031
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-01-15 20:27:39 +0000

    Mon, Jan 15, 2024, 9:27 PM +01:00

--- bank/bank.py
+++ bank/bank.py
@@ -1,5 +1,6 @@
 # Capatalize the first letter
-greeting = input().capitalize()
+greeting = input()
+#greeting = input().capitalize()
 # Get whatever greeting and split it in a matrix (1 x n) | n= number of words
 greeting = greeting.split()
 
