56c6180aaf8d6e1992ffde21bcf7264601c12277
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-01-15 10:51:54 +0000

    Mon, Jan 15, 2024, 11:51 AM +01:00

--- ProblemSet_1/deep/deep.py
+++ ProblemSet_1/deep/deep.py
@@ -1,11 +1,24 @@
 # Get input
 question = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
 
-if question == "42":
-    print ("yes")
-elif question == "forty-two":
-    print ("yes")
-elif question == "forty two":
-    print ("yes")
-else:
-    print ("no")
+#if question == "42":
+#    print ("yes")
+#elif question == "forty-two":
+#    print ("yes")
+#elif question == "forty two":
+#    print ("yes")
+#else:
+#    print ("no")
+
+def answer():
+
+    if question == "42":
+        print ("yes")
+    elif question == "forty-two":
+        print ("yes")
+    elif question == "forty two":
+        print ("yes")
+    else:
+        print ("no")
+
+answer()
