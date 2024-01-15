1721a6be3b7f542cf6fd6bead50fe561ff53fc6a
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-01-15 20:15:59 +0000

    Mon, Jan 15, 2024, 9:15 PM +01:00

--- bank/bank.py
+++ bank/bank.py
@@ -1,5 +1,5 @@
 # Get whatever greeting and split it in a matrix (1 x n) | n= number of words
-greeting = input().lower()
+greeting = input().capitalize()
 greeting = greeting.split()
 # Lowers all letters in the first colum of the matrix
 print(greeting)
@@ -8,10 +8,11 @@ match greeting:
     case ["hello" | "Hello", *rest]:
         print("$0")
     case _:
-        if greeting[0] == "h":
-            print("$20")
-        elif greeting[0] == "H":
+        if greeting[0] == "H":
             print("$20")
         else:
             print("$100")
+
+greeting = greeting.join()
+
 
