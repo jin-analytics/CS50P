b29060ce477912e593fda1c0bd6b1443bafa9dfd
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-01-15 20:35:25 +0000

    Mon, Jan 15, 2024, 9:35 PM +01:00

--- bank/bank.py
+++ bank/bank.py
@@ -1,13 +1,16 @@
-# Capatalize the first letter
 greeting = input("Greeting: ")
 
-# Get whatever greeting and split it in a matrix (1 x n) | n= number of words
+# Split it in a matrix (1 x n) to select the first "Hello"
 greeting = greeting.split()
 
 match greeting:
+    # Select different types of hello in the first column of the matrix...
+    # ... with *rest will be the rest of the column entrees ignored!
     case ["Hello," | "Hello" | "hello", *rest]:
         print("$0")
     case _:
+        # Joing the matrix back together, so that you can...
+        # ... select the letter and not the full word!
         greeting = ' '.join(greeting)
         if greeting[0] == "H":
             print("$20")
