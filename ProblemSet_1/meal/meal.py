602f2d88010fbfafba0edd9ef3d28384743e2fa3
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-01-17 16:58:43 +0000

    Wed, Jan 17, 2024, 5:58 PM +01:00

--- ProblemSet_1/meal/meal.py
+++ ProblemSet_1/meal/meal.py
@@ -2,13 +2,16 @@
 # lunch      time [12:00;13:00]
 # dinner     time [18:00;19:00]
 
+input("What time is it? ")
+
 def main():
     ...
 
 
 def convert(time):
-    ...
+    print(time)
 
-
+# main function - helps to prevent errors due to not defined variables yet
+# of we call main() inside there
 if __name__ == "__main__":
     main()
