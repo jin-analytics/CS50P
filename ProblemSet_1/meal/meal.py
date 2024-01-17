7e06bbbf3b9e59c3f8d71eb481657ac0ea542017
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-01-17 17:56:01 +0000

    Wed, Jan 17, 2024, 6:56 PM +01:00

--- ProblemSet_1/meal/meal.py
+++ ProblemSet_1/meal/meal.py
@@ -9,37 +9,37 @@ def main():
 
     # Gives the variable "t" to the convert funciton
     t=input("What time is it? ")
-   # print("Before convert function ",t)
+    print("Before convert function ",t)
     convert(t)
-    #print("After convert function ",t)
-
-    # Join the float number back together to a string
-    t = ' '.join(t)
+    print("After convert function ",t)
 
 
 
 
 
 def convert(time):
-    #print(time)
+    # replaces the ":" to a " " space for the split function
     time = time.replace(':',' ')
+    # splits the time variable
     time = time.split()
+    # frist column to an integer
     time[0] = int(time[0])
+    # second cloumn to a float, so that we can later compare the number and not the time
     time[1] = float(time[1])
+    # multiplies the minutes (1 minute = 1.666...%) to get the percentage of a full hour
     time[1] = round(time[1]*1.666)
-    # Adds both colums into one number
+    # Adds both colums into a string
     time[0] = str(time[0])
     time[1] = str(time[1])
-
-
-    # 1 minute = 1.66666... %
-    #time = float(time)
-    print("Check: ",time)
+    # Join the string back together
+    time = '.'.join(time)
+    # convert it back to a float number as a single number now with the estimated percentage per hour
+    time = float(time)+1
+    #print("Check: ",time)
     return time
 
 
-# if __name__ == "__main__": - helps to prevent errors due to not defined
-# global variables yet of we call main() inside there
+# if __name__ == "__main__": - helps to prevent errors due to not defined global variables yet of we call main() inside there
 if __name__ == "__main__":
     main()
     #print("TEST function")
