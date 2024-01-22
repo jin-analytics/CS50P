61b43de6e472a295cb3aeed38d2daefe19a3f153
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-01-22 10:09:56 +0000

    Mon, Jan 22, 2024, 11:09 AM +01:00

--- ProblemSet_1/interpreter/interpreter.py
+++ ProblemSet_1/interpreter/interpreter.py
@@ -2,9 +2,10 @@
 def main():
 # Input will be like "x y z"
 # Where x and z is an integer and y an calculation command
-    expression = input("Expression: ")
+    expression = input("Expression: ").split().replace(' ','')
     print(expression)
-    int2float(expression)
+
+   # int2float(expression)
 
     #solution = x y z
 
