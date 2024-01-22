77108a4f20f5b635ee5793b94a818874e67b2345
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-01-22 10:37:10 +0000

    Mon, Jan 22, 2024, 11:37 AM +01:00

--- ProblemSet_1/interpreter/interpreter.py
+++ ProblemSet_1/interpreter/interpreter.py
@@ -6,7 +6,7 @@ def main():
     #print("was geht" ,expression[0],expression[1],expression[2])
     int2float(expression)
     #int2float(expression[0],expression[1],expression[2])
-    print(expression)
+    print("Solutiuon",expression)
     #solution = x y z
 
 def int2float(expression):
