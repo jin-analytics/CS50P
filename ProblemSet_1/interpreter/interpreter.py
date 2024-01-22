c6563b6f612c6995e6324e651f8d91a09957dfb4
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-01-22 10:03:38 +0000

    Mon, Jan 22, 2024, 11:03 AM +01:00

--- ProblemSet_1/interpreter/interpreter.py
+++ ProblemSet_1/interpreter/interpreter.py
@@ -1,2 +1,17 @@
+
+def main():
 # Input will be like "x y z"
-# Where x and z is an integer and y an
+# Where x and z is an integer and y an calculation command
+    expression = input("Expression: ")
+    float(expression)
+
+    #solution = x y z
+
+def float():
+    print(expression)
+
+
+
+
+if __name__ == "__main__":
+    main()
