e1322dcab2acbe0f76e2361b8e8a6bd74ec3a096
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-01-29 18:22:44 +0000

    Mon, Jan 29, 2024, 7:22 PM +01:00

--- ProblemSet_2/camel/camel.py
+++ ProblemSet_2/camel/camel.py
@@ -1,14 +1,14 @@
 # This Program changes the CamelCase input to Snake_Case input
 CamelCase = input ("camelCase: ")
-splitAtUpperCase()
+
 # print (snake)
 
-def splitAtUpperCase(text):
-    result = ""
-    for char in text:
+for char in CamelCase:
+        result=""
         if char.isupper():
             result += " " + char
         else:
             result += char
             print (result)
-    return result.split()
+            
+print (result)
