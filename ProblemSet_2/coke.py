25cf141fb3830f4bba707ec7addfa05301da9e96
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-01-31 09:40:56 +0000

    Wed, Jan 31, 2024, 10:40 AM +01:00

--- ProblemSet_2/coke.py
+++ ProblemSet_2/coke.py
@@ -10,19 +10,17 @@ def coin_machine(price):
     while True:
         # insert a coin
         pay = int(input("Insert Coin: "))
+        # accepted coins 25, 10 or 5 cent
         match pay:
             case 25 | 10 | 5:
-            continue
-        else:
-            # calculates the price which has to be paid after inserting the coin
-            price = price - pay
-            # prints out which money still has to be paid
-            if price > 0:
-                print("Amount Due:", price)
-            # if the paid amount is 50 or more, the loop stops
-            elif price <= 0:
-                print("Change Owed:", -1*(price))
-                break
+                price = price - pay
+                # prints out which money still has to be paid
+                if price > 0:
+                    print("Amount Due:", price)
+                # if the paid amount is 50 or more, the loop stops
+                elif price <= 0:
+                    print("Change Owed:", -1*(price))
+                    break
 
 # if __name__ == "__main__": - helps to prevent errors due to not defined global variables yet, if we call main() inside there
 if __name__ == "__main__":
