e77d0eb42d32aa35bb7e74ae0145b0a9e45a143b
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-01-31 09:14:58 +0000

    Wed, Jan 31, 2024, 10:14 AM +01:00

--- ProblemSet_2/coke.py
+++ ProblemSet_2/coke.py
@@ -1,12 +1,17 @@
-
+# Initial price is 50 cent
 price = int(50)
+# Asks for the initial amount which is 50
 print("Amount Due:", price)
 
+# as long the price which has to be payed is bigger than 0, the loop continues
 while True:
-
+    # insert a coin
     pay = int(input("Insert Coin: "))
+    # calculates the price which has to be paid after inserting the coin
     price = price - pay
+    # prints out which money still has to be paid
     print("Amount Due:", price)
-    if price > 50:
+    # if the paid amount is 50 or more, the lopp stops
+    if price <= 0:
         break
 
