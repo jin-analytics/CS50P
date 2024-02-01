b7588749cd9cb7299e4c298dd1feb01ff0d7ec1b
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-01 14:37:58 +0000

    Thu, Feb 1, 2024, 3:37 PM +01:00

--- ProblemSet_2/twttr/twttr.py
+++ ProblemSet_2/twttr/twttr.py
@@ -1,22 +1,12 @@
 def main():
-    # Initial price is 50 cent
-    price = 50
-    coin_machine(price)
+    tweet = input("Input: ")
+    omitter(tweet)
+    print("Output")
 
-def coin_machine(price):
-    # as long the price which has to be payed is bigger than 0, the loop continues
-    while True:
-        # insert a coin
-        print("Amount Due:", price)
-        pay = int(input("Insert Coin: "))
-        # accepted coins 25, 10 or 5 cent
-        match pay:
-            case 25 | 10 | 5:
-                price = price - pay
-                # if the paid amount is 50 or more, the loop stops
-                if price <= 0:
-                    print("Change Owed:", -1*(price))
-                    break
+def ommiter(twt):
+    
+
+
 
 # if __name__ == "__main__": - helps to prevent errors due to not defined global variables yet, if we call main() inside there
 if __name__ == "__main__":
