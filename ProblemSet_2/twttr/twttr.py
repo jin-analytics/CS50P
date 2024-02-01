d2e2726541f9fdab6e95f8b120ad5866c8d1c998
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-01 14:59:59 +0000

    Thu, Feb 1, 2024, 3:59 PM +01:00

--- ProblemSet_2/twttr/twttr.py
+++ ProblemSet_2/twttr/twttr.py
@@ -1,20 +1,15 @@
 def main():
     tweet = input("Input: ")
-    omitter(tweet)
-   # print("Output")
+
+    tweet = omitter(tweet)
+    print(tweet)
 
 def omitter(twt):
     for vowels in twt:
-        #print(vowels)
         match vowels:
             case "a" | "e" | "i" | "o" | "u":
-                #print("case",vowels)
                 twt = twt.replace(vowels,"")
-                #print("case2",twt)
-
-    print("case3",twt)
-
-
+    
 
 
 # if __name__ == "__main__": - helps to prevent errors due to not defined global variables yet, if we call main() inside there
