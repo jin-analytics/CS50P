148b6a9174828f0f1d1b7824b16b5bb6357250b2
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-02 09:50:57 +0000

    Fri, Feb 2, 2024, 10:50 AM +01:00

--- ProblemSet_2/plates/plates.py
+++ ProblemSet_2/plates/plates.py
@@ -87,6 +87,7 @@ def seperator(plate_raw):
             plate_raw = plate_raw.replace(str(letters)," " + str(letters), 1)
             plate_split = plate_raw.split(" ")
             break
+    plate_split = plate_split.append("")
     print(plate_split)
     return plate_split
 
