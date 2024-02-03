6e67359d96c77c79e5c72c462ba908cb21cc19b6
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-03 16:54:01 +0000

    Sat, Feb 3, 2024, 5:54 PM +01:00

--- ProblemSet_2/plates.py
+++ ProblemSet_2/plates.py
@@ -8,19 +8,19 @@ def main():
 
 def is_valid(s):
 
+    length(s)
+    sign_detection
+
+
 
 # Check if the string Length of the input is in the allowed range from between 2 to 6
     def length(plate_length):
-    if 2 <= len(plate_length) <= 6:
-        return True
-    else:
-        exit("Invalid")
+        if 2 <= len(plate_length) <= 6:
+            return True
+        else:
+            exit("Invalid")
 
     # Checks for signs, spaces and punctuation in the input
     def sign_detection(plate_input):
-
         for signs in plate_input:
-            signs = sign_detection.isdigit()
-            signs = sign_detection.
-            if signs != False:
-                return True
+            if " " or "."
