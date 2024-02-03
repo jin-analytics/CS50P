ddb40e3b760c9ec3af856093912e881686f2383b
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-03 17:45:11 +0000

    Sat, Feb 3, 2024, 6:45 PM +01:00

--- ProblemSet_2/plates/plates.py
+++ ProblemSet_2/plates/plates.py
@@ -22,14 +22,14 @@ def is_valid(s):
 
 
 
-# Check if the string Length of the input is in the allowed range from between 2 to 6
+# Check if the string Length of the input is in the allowed range from between 2 to 6 and if in range - return False
 def length(plate_length):
     if 2 <= len(plate_length) <= 6:
         return True
     else:
         return False
 
-# Checks for periods, spaces and punctuation in the input
+# Checks for periods, spaces and punctuation in the input, if nothing found - return True
 def sign_detection(plate_input):
     for signs in plate_input:
         # detects: !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
