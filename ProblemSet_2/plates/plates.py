f5e954fdde7446736bec3850429db60cc085dd52
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-03 17:08:15 +0000

    Sat, Feb 3, 2024, 6:08 PM +01:00

--- ProblemSet_2/plates/plates.py
+++ ProblemSet_2/plates/plates.py
@@ -15,6 +15,7 @@ def is_valid(s):
 
 # Check if the string Length of the input is in the allowed range from between 2 to 6
     def length(plate_length):
+        print(plate_length)
         if 2 <= len(plate_length) <= 6:
             return True
         else:
