fabae8fc7ec8be8623732574f1e46d12ef305c49
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-03 17:15:05 +0000

    Sat, Feb 3, 2024, 6:15 PM +01:00

--- ProblemSet_2/plates/plates.py
+++ ProblemSet_2/plates/plates.py
@@ -9,29 +9,29 @@ def main():
 def is_valid(s):
 
     length(s)
-    sign_detection(s)
+#    sign_detection(s)
 
 
 
 # Check if the string Length of the input is in the allowed range from between 2 to 6
 def length(plate_length):
-    print(plate_length)
+    print(len(plate_length))
     if 2 <= len(plate_length) <= 6:
         return True
     else:
         return False
 
 # Checks for periods, spaces and punctuation in the input
-def sign_detection(plate_input):
-    for signs in plate_input:
+#def sign_detection(plate_input):
+   # for signs in plate_input:
         # punctuation() detects: !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
-        signs = signs.punctuation()
+        #signs = signs.punctuation()
         # whitespace detects: characters space, tab, linefeed, return, formfeed, and vertical tab
-        signs = signs.whitespace()
-        if signs != True:
-            return True
-        else:
-            return False
+      #  signs = signs.whitespace()
+       # if signs != True:
+       #     return True
+      #  else:
+       #     return False
 
 
 main()
