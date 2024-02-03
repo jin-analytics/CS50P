bafb6844cca13ed9c9b70f826a649b79afeb4844
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-03 19:07:49 +0000

    Sat, Feb 3, 2024, 8:07 PM +01:00

--- ProblemSet_2/plates/plates.py
+++ ProblemSet_2/plates/plates.py
@@ -32,7 +32,10 @@ def is_valid(s):
 # check if the length of the letter block is atleast 2 character, and then - returns True
     if letterblock_atleast_two_char(plate_split) == False: # Function #6
         return False
-
+    
+# Checks if there are only numbers in the number block (#2)
+    if first_number_zero(plate_split) == False: # Function #6
+        return False
 
 # If no functions used exit(), then the plate input is valid and return a "True"
     else:
@@ -102,6 +105,7 @@ def letterblock_atleast_two_char(plate_split):
     else:
         return False
 
+#_______________________________________________Function #7_____________________________________________________________
 # Checks if there are only numbers in the number block (#2), if yes - returns True
 def numberblock_only_number(plate_split):
         number_block = plate_split[1]
