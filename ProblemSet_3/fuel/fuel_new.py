e258603aeb5b01a656fbecceb4438be7dad2516e
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-04 15:26:12 +0000

    Sun, Feb 4, 2024, 4:26 PM +01:00

--- ProblemSet_3/fuel/fuel_new.py
+++ ProblemSet_3/fuel/fuel_new.py
@@ -3,6 +3,11 @@ def main():
         fraction = input("Fraction: ")
         fraction = fraction.split("/")
         if len(fraction) == 2:
-            
+            calculator(fraction)
+
+def calculator(f):
+    # if x and y is a number, the program calculates how much fuel is left, else it reprompts
+    try:
+
 
 main()
