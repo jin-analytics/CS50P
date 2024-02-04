0f7bedf4a84e3230a6c1d4494dfa3c3d8c2362a2
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-04 15:43:15 +0000

    Sun, Feb 4, 2024, 4:43 PM +01:00

--- ProblemSet_3/fuel/fuel.py
+++ ProblemSet_3/fuel/fuel.py
@@ -1,3 +1,5 @@
+# while loop continues until on of the exit() methods will stop the program
+# ask for input x/y and then splits it and gives x and y to the calculator() function
 def main():
     while True:
         fraction = input("Fraction: ")
@@ -5,7 +7,9 @@ def main():
         if len(fraction) == 2:
             calculator(fraction)
 
+
 def calculator(f):
+    # Checks if x and y is a number, then divides it to get the percentage of the fraction
     try:
         if f[0].isdigit() and f[1].isdigit() == True:
             percentage = (int(f[0])/int(f[1]))*100
@@ -13,19 +17,24 @@ def calculator(f):
         return False
 
     try:
+        # ]1%;99%[ print the actual number
         if 1 < percentage < 99:
             print(round(percentage), "%", sep="")
             exit()
+        # <= 1% prints E for Empty
         elif percentage <= 1:
             print("E")
             exit()
+        # <= 99% prints F for Full
         elif percentage <= 99:
+            print("F")
+            exit()
+        elif f[0] == f[1]:
             print("F")
             exit()
         else:
             return False
     except UnboundLocalError:
         return False
-
 
 main()
