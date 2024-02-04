fa0b6f6b2e095f2acc8596acc36da4d36ffe090c
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-04 11:58:36 +0000

    Sun, Feb 4, 2024, 12:58 PM +01:00

--- ProblemSet_3/fuel/fuel.py
+++ ProblemSet_3/fuel/fuel.py
@@ -1,13 +1,18 @@
 def main():
     while True:
         fraction = input("Fraction: ")
-        if x_y_splitter(fraction) == True:
-            fraction_calculator()
+        fraction = fraction.split("/")
+        if len(fraction) == 2:
+            calculator(fraction)
 
 
-def x_y_splitter(f):
-    f = f.split("/")
-    print(f)
-    return True
+        #fraction_calculator(x_y_splitter(fraction))
+
+
+def calculator(f):
+    #print(f)
+    if f[0].isdigit() and f[1].isdigit() == True:
+        percentage = (int(f[0])/int(f[1]))*100
+        print(percentage)
 
 main()
