0db79f46c50d6b509dc5a066f8b083103a0001da
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-04 12:38:03 +0000

    Sun, Feb 4, 2024, 1:38 PM +01:00

--- ProblemSet_3/fuel/fuel.py
+++ ProblemSet_3/fuel/fuel.py
@@ -9,10 +9,11 @@ def main():
 def calculator(f):
     # if x and y is a number, the program calculates how much fuel is left, else it reprompts
     if f[0].isdigit() and f[1].isdigit() == True:
-        # if y is a zero or x is greater than y the program repromts
-        if f[1] == "0" or f[0] < f[1]:
-            return False
         percentage = (int(f[0])/int(f[1]))*100
+        # if y is a zero or x is greater than y the program repromts
+        if f[1] == "0" or f[0] > f[1]:
+            return False
+
 
         if 1 < percentage < 99:
             print(round(percentage), "%", sep="")
@@ -20,7 +21,7 @@ def calculator(f):
         elif percentage <= 1:
             print("E")
             exit()
-        elif percentage >= 99:
+        elif percentage <= 99:
             print("F")
             exit()
         else:
