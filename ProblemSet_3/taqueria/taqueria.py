c4afdb89f2f1d6251b64c7fe714daa2773d785a2
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-04 17:12:47 +0000

    Sun, Feb 4, 2024, 6:12 PM +01:00

--- ProblemSet_3/taqueria/taqueria.py
+++ ProblemSet_3/taqueria/taqueria.py
@@ -16,7 +16,7 @@ def main():
         item = input("Item: ")
         try:
             amount = total(item)
-            print (format(amount, '.2f'))
+            print ("Amount:",format(amount, '.2f'))
             break
         except UnboundLocalError:
              pass
@@ -27,14 +27,14 @@ def total(i):
     for food in menu:
         if food == i:
             price = float(menu[food])
-            print (format(price, '.2f'))
+            print ("Amount:",format(price, '.2f'))
     try:
         while True:
             item = input("Item: ")
             for food in menu:
                 if food == item:
                     price = float(menu[food]) + price
-                    print (format(price, '.2f'))
+                    print ("Amount:",format(price, '.2f'))
     except EOFError:
          return price
 
