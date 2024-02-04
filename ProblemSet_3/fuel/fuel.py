44c0c33a454b1cb03c5c04b5cffe97241527db86
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-04 12:15:28 +0000

    Sun, Feb 4, 2024, 1:15 PM +01:00

--- ProblemSet_3/fuel/fuel.py
+++ ProblemSet_3/fuel/fuel.py
@@ -1,16 +1,19 @@
+# asks for input like "x/y", if not this type the program stops otherwise it goes in function calculator()
 def main():
     while True:
         fraction = input("Fraction: ")
         fraction = fraction.split("/")
         if len(fraction) == 2:
             calculator(fraction)
+        else:
+            exit()
 
 
         #fraction_calculator(x_y_splitter(fraction))
 
 
 def calculator(f):
-    #print(f)
+    # if x and y is a number, the program calculates how much fuel is left, else it stops
     if f[0].isdigit() and f[1].isdigit() == True:
         percentage = (int(f[0])/int(f[1]))*100
         if 1 < percentage < 99:
@@ -18,13 +21,12 @@ def calculator(f):
             exit()
         elif percentage <= 1:
             print("E")
+            exit()
         else:
             print("F")
-
+            exit()
     else:
         exit()
-
-
 
 
 main()
