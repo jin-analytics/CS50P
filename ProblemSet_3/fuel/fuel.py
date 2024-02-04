24a6d06ffe81bacb0428fc9495c29dfe16eb205c
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-04 11:51:29 +0000

    Sun, Feb 4, 2024, 12:51 PM +01:00

--- ProblemSet_3/fuel/fuel.py
+++ ProblemSet_3/fuel/fuel.py
@@ -1,9 +1,13 @@
 def main():
-    fraction = input("Fraction: ")
-    x_y_splitter(fraction)
+    while True:
+        fraction = input("Fraction: ")
+        if x_y_splitter(fraction) == True:
+            fraction_calculator()
+
 
 def x_y_splitter(f):
     f = f.split("/")
     print(f)
+    return True
 
 main()
