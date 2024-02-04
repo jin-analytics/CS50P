253d8d286f549f1ab0ba6ce2d202f09cad3828e4
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-04 15:36:04 +0000

    Sun, Feb 4, 2024, 4:36 PM +01:00

--- ProblemSet_3/fuel/fuel.py
+++ ProblemSet_3/fuel/fuel.py
@@ -1,4 +1,3 @@
-# asks for input like "x/y", if not this type the program stops otherwise it goes in function calculator()
 def main():
     while True:
         fraction = input("Fraction: ")
@@ -7,27 +6,26 @@ def main():
             calculator(fraction)
 
 def calculator(f):
-    # if x and y is a number, the program calculates how much fuel is left, else it reprompts
     try:
-
-
-
         if f[0].isdigit() and f[1].isdigit() == True:
             percentage = (int(f[0])/int(f[1]))*100
-            # if y is a zero or x is greater than y the program repromts
+    except ZeroDivisionError:
+        return False
+
+    try:
+        if 1 < percentage < 99:
+            print(round(percentage), "%", sep="")
+            exit()
+        elif percentage <= 1:
+            print("E")
+            exit()
+        elif percentage <= 99:
+            print("F")
+            exit()
+        else:
+            return False
+    except UnboundLocalError:
+        return False
 
 
-            if 1 < percentage < 99:
-                print(round(percentage), "%", sep="")
-                exit()
-            elif percentage <= 1:
-                print("E")
-                exit()
-            elif percentage <= 99:
-                print("F")
-                exit()
-            else:
-                return False
-    except ValueError:
-        
 main()
