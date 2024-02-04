b8e6410d79bd857e68439e9427e34676849d3ab8
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-04 12:01:46 +0000

    Sun, Feb 4, 2024, 1:01 PM +01:00

--- ProblemSet_3/fuel/fuel.py
+++ ProblemSet_3/fuel/fuel.py
@@ -13,6 +13,10 @@ def calculator(f):
     #print(f)
     if f[0].isdigit() and f[1].isdigit() == True:
         percentage = (int(f[0])/int(f[1]))*100
-        print(percentage)
+        match percentage:
+            case 1 < percentage < 99
+                print(round(percentage), "%", sep="")
+                exit()
+                
 
 main()
