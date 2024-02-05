b68fc1027b0b62bd9bda34ede84f6fd1fa168fd9
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-05 19:39:50 +0000

    Mon, Feb 5, 2024, 8:39 PM +01:00

--- ProblemSet_3/outdated/outdated.py
+++ ProblemSet_3/outdated/outdated.py
@@ -1,5 +1,7 @@
 def main():
+    # input will be MM/DD/YYYY
     date = get_date("What is the date? ")
+    # output will be YY/MM/DD
     print(f"The date is {date}")
 
 def get_date(prompt):
@@ -7,10 +9,14 @@ def get_date(prompt):
         try:
             date_split = input(prompt).split("/")
             print(date_split)
-            # add 0 if 
+            # add 0 if MM has only one character
             if len(date_split[0]) == 1:
                 date_split[0] = date_split[0].replace(date_split[0], "0" + date_split[0])
                 print (date_split[0])
+            # add 0 if DD has only one character
+            if len(date_split[1]) == 1:
+                date_split[1] = date_split[1].replace(date_split[1], "0" + date_split[1])
+                print (date_split[1])
 
             # Split the date string wherever "/" is
 
