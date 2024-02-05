77ce7ce86d6c621041eebe3179295c0af027530a
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-05 21:05:28 +0000

    Mon, Feb 5, 2024, 10:05 PM +01:00

--- ProblemSet_3/outdated/outdated.py
+++ ProblemSet_3/outdated/outdated.py
@@ -32,18 +32,8 @@ def get_date(prompt):
 
             # check if all inputs beside "/" are integer
             if date_split[0].isdigit() and date_split[1].isdigit() and date_split[2].isdigit() == True:
-                # add 0 if MM has only one character
-                if len(date_split[0]) == 1:
-                    date_split[0] = date_split[0].replace(date_split[0], "0" + date_split[0])
-                # add 0 if DD has only one character
-                if len(date_split[1]) == 1:
-                    date_split[1] = date_split[1].replace(date_split[1], "0" + date_split[1])
-                # change format MM.DD.YYYY to YYYY.MM.DD
-                date_split[0], date_split[1] = date_split[1], date_split[0] # Changes MM #1 to MM #2 and DD #2 to DD #1
-                date_split[0], date_split[2] = date_split[2], date_split[0] # Changes DD #1 to DD #3 and YYYY #3 to YYYY #1
-                # joins the list to a string joined wir "-"
-                date_split = '-'.join(date_split)
-                return date_split
+                outdated = date_convert(date_split)
+                return outdated
 
             # check if the month is fully written, for example: september 01, 1992
             if date_split[0].isalpha() and date_split[1].isdigit() and date_split[3].isdigit() == True:
