499c984ee0070546eecb829c1972980febb95a76
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-05 20:16:42 +0000

    Mon, Feb 5, 2024, 9:16 PM +01:00

--- ProblemSet_3/outdated/outdated.py
+++ ProblemSet_3/outdated/outdated.py
@@ -22,7 +22,8 @@ def get_date(prompt):
                 # change format MM.DD.YYYY to YYYY.MM.DD
                 date_split[0], date_split[1] = date_split[1], date_split[0] # Changes MM #1 to MM #2 and DD #2 to DD #1
                 date_split[0], date_split[2] = date_split[2], date_split[0] # Changes DD #1 to DD #3 and YYYY #3 to YYYY #1
-                print(date_split)
+                # joins the list to a string joined wir "-"
+                date_split = '-'.join(date_split)
 
                 return date_split
         except ValueError:
