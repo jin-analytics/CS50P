bdac776b25d8ea4a42fb563cffa8b098212ff478
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-05 19:28:08 +0000

    Mon, Feb 5, 2024, 8:28 PM +01:00

--- ProblemSet_3/outdated/outdated.py
+++ ProblemSet_3/outdated/outdated.py
@@ -1,11 +1,11 @@
 def main():
-    date = get_date(
+    date = get_date("What is the date? ")
     print(f"The date is {date}")
 
 def get_date(prompt):
     while True:
         try:
-            return (int(prompt))
+            return (int(input(prompt)))
         except ValueError:
             pass
 
