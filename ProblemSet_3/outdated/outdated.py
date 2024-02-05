931d0a2a59654b54700379c9a505713346d9f530
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-05 19:53:08 +0000

    Mon, Feb 5, 2024, 8:53 PM +01:00

--- ProblemSet_3/outdated/outdated.py
+++ ProblemSet_3/outdated/outdated.py
@@ -23,6 +23,7 @@ def get_date(prompt):
                 date_split[1] = date_split[0] # MM is now #2
                 date_split[2] = date_split[1] # DD is now #3
                 date_split[0] = date_split[2] # YYYY is now #1
+                print(date_split)
 
                 return date_split
         except ValueError:
