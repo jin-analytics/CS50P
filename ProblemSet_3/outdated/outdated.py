6d4e405f4b982bf6f4c7759f7e73dd6591f4d232
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-06 14:35:54 +0000

    Tue, Feb 6, 2024, 3:35 PM +01:00

--- ProblemSet_3/outdated/outdated.py
+++ ProblemSet_3/outdated/outdated.py
@@ -35,6 +35,8 @@ def get_date(prompt):
             # Update the original list with the modified list
             date_split = new_list
 
+            if int(outdated[1]) <= 12 and int(outdated[2]) <= 31: # Checks if the month and day is legit:
+
             # check if all inputs beside "/" are numbers
             if date_split[0].isdigit() and date_split[1].isdigit() and date_split[2].isdigit() == True:
                     outdated = date_convert(date_split)
