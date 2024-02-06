902757d3d31f0976c30f013982946d26ae49b036
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-06 14:38:14 +0000

    Tue, Feb 6, 2024, 3:38 PM +01:00

--- ProblemSet_3/outdated/outdated.py
+++ ProblemSet_3/outdated/outdated.py
@@ -37,33 +37,30 @@ def get_date(prompt):
 
             if int(outdated[1]) <= 12 and int(outdated[2]) <= 31: # Checks if the month and day is legit:
 
-            # check if all inputs beside "/" are numbers
-            if date_split[0].isdigit() and date_split[1].isdigit() and date_split[2].isdigit() == True:
-                    outdated = date_convert(date_split)
-                    print(int(outdated[2]))
-                    if int(outdated[1]) <= 12 and int(outdated[2]) <= 31: # Checks if the month and day is legit
-                        print(outdated)
+                # check if all inputs beside "/" are numbers
+                if date_split[0].isdigit() and date_split[1].isdigit() and date_split[2].isdigit() == True:
+                        outdated = date_convert(date_split)
                         return outdated
                     else:
-                        exit()
-
-
-            # check if the month is fully written, for example: september 01, 1992
-            if date_split[0].isalpha() and date_split[1].isdigit() and date_split[2].isdigit() == True:
-
-                for month_name in month:
-                    #print(month_name)
-                    if month_name == date_split[0]:
-                        date_split[0] = str(month[month_name])
-
-                # changes the fully written month in entree data_split[0] to the number from the list "month"
-                        outdated = date_convert(date_split)
-
-                        if int(outdated[1]) <= 12 and int(outdated[2]) <= 31: # Checks if the month and day is legit
-                            print(outdated)
-                            return outdated
-                        else:
                             exit()
+
+
+                # check if the month is fully written, for example: september 01, 1992
+                if date_split[0].isalpha() and date_split[1].isdigit() and date_split[2].isdigit() == True:
+
+                    for month_name in month:
+                        #print(month_name)
+                        if month_name == date_split[0]:
+                            date_split[0] = str(month[month_name])
+
+                    # changes the fully written month in entree data_split[0] to the number from the list "month"
+                            outdated = date_convert(date_split)
+
+                            if int(outdated[1]) <= 12 and int(outdated[2]) <= 31: # Checks if the month and day is legit
+                                print(outdated)
+                                return outdated
+                            else:
+                                exit()
 
         except ValueError:
             pass
