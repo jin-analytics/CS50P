c992a1bfa544e8a3b1903b573317ff7d4697802c
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-06 14:19:55 +0000

    Tue, Feb 6, 2024, 3:19 PM +01:00

--- ProblemSet_3/outdated/outdated.py
+++ ProblemSet_3/outdated/outdated.py
@@ -38,7 +38,7 @@ def get_date(prompt):
             # check if all inputs beside "/" are numbers
             if date_split[0].isdigit() and date_split[1].isdigit() and date_split[2].isdigit() == True:
                     outdated = date_convert(date_split)
-                    if int(outdated[1]) <= 12 and int(outdated[0]) <= 31: # Checks if the month and day is legit
+                    if int(outdated[1]) <= 12 and int(outdated[2]) <= 31: # Checks if the month and day is legit
                         return outdated
                     else:
                         exit()
@@ -54,7 +54,7 @@ def get_date(prompt):
 
                 # changes the fully written month in entree data_split[0] to the number from the list "month"
                         outdated = date_convert(date_split)
-                        if int(outdated[1]) <= 12 and int(outdated[0]) <= 31: # Checks if the month and day is legit
+                        if int(outdated[1]) <= 12 and int(outdated[2]) <= 31: # Checks if the month and day is legit
                             return outdated
                         else:
                             exit()
