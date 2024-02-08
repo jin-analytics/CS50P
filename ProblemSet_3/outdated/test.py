933c732304e934e169d5c062f7f0af078564358e
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-08 17:41:59 +0000

    Thu, Feb 8, 2024, 6:41 PM +01:00

--- ProblemSet_3/outdated/test.py
+++ ProblemSet_3/outdated/test.py
@@ -25,7 +25,7 @@ def get_date(prompt):
     while True:
         try:
             # First will be checked if its "September 9, 1999" or "9/9/1999", because these are the only valid inputs
-            if sign_detection_comma(prompt) == True:
+            sign_detection_comma(prompt)
                 # Split the date string wherever "/", " " is
                 date_split = re.split('[ ,]', input(prompt))
                 date_split = remove_spaces(date_split)
@@ -105,5 +105,6 @@ def sign_detection_comma(prompt):
     for signs in prompt:
         if signs in "," == True:
             print("detected")
+            return True
 
 main()
