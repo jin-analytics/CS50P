f8e2b13eaaa3a9d54ae06d424ce44cb4fe396e71
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-09 18:32:07 +0000

    Fri, Feb 9, 2024, 7:32 PM +01:00

--- ProblemSet_3/grocery/grocery.py
+++ ProblemSet_3/grocery/grocery.py
@@ -8,20 +8,18 @@
 def main():
     # Empty dictionary
     list = {}
-    new_key = input()
-    new_value = 1
-    list.update({new_key: new_value})
-    
+
     while True:
 
         new_key = input()
         new_value = 1
         for i in list:
-            if i == new_key:
-                list.update({new_key: new_value + 1})
-            else:
+            if new_key ==i:
+                new_value = new_v
+        #    if i == new_key:
+        #        list.update({new_key: new_value + 1})
                 # Add a key-value pair to empty dictionary in python
-                list.update({new_key: new_value})
+        list.update({new_key: new_value})
 
         print(list)
 
