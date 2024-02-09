193e1a788d4ad1c2c48ce7ab42d4a279e7754864
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-09 19:00:00 +0000

    Fri, Feb 9, 2024, 8:00 PM +01:00

--- ProblemSet_3/grocery/grocery.py
+++ ProblemSet_3/grocery/grocery.py
@@ -15,14 +15,14 @@ def main():
             new_key = input()
             shopping_list = {new_key:new_value}
             print("0",shopping_list)
-            
+
             for i in shopping_list:
                 if i == shopping_list.get(i):
                     shopping_list[i] = shopping_list.get(i) +1
                     print("1",shopping_list)
                     print("2",shopping_list[1])
-            shopping_list = shopping_list.update({new_key:new_value})
-            print("3",shopping_list)
+            #shopping_list = shopping_list.update({new_key:new_value})
+            #print("3",shopping_list)
 
 
 
