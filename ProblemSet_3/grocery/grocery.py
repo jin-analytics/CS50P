fa0e69cdc3818b3f835cd96afe293345b2caea4d
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-09 18:08:10 +0000

    Fri, Feb 9, 2024, 7:08 PM +01:00

--- ProblemSet_3/grocery/grocery.py
+++ ProblemSet_3/grocery/grocery.py
@@ -1,15 +1,22 @@
+####### try to do this solution:
+#1 create a dictionary with inputs
+#2 associate a number to each input
+#3 if the same input occurs, add +1 to count the entrees
+
 def main():
 
-    list = []
+    list = {}
 
     while True:
         try:
             item = input()
             list.append(item)
             print(list)
+
         except EOFError:
-            for 
-            print(list[0:len(list)])
+            #for entrees in list:
+            #   print(list(entrees))
+            print(list)
             break
 
 
