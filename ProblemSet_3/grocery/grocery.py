3129df3706c11c202850ca5b411e0ab5bbe17dc7
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-09 17:56:39 +0000

    Fri, Feb 9, 2024, 6:56 PM +01:00

--- ProblemSet_3/grocery/grocery.py
+++ ProblemSet_3/grocery/grocery.py
@@ -1,13 +1,16 @@
 def main():
+
+    list = []
+
     while True:
         item = input()
-        list = add_item_to_list(item)
-        list = list + list
+     #  list = add_item_to_list(item)
+        list = list.append(item)
         print(list)
 
 
-def add_item_to_list(i):
-    new_list = i
-    return new_list
+#def add_item_to_list(i):
+#    new_list = i
+#   return new_list
 
 main()
