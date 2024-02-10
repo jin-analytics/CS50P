9ed1eda6c7ca709da51445fc115a92e19f4ff8ac
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-10 17:42:35 +0000

    Sat, Feb 10, 2024, 6:42 PM +01:00

--- ProblemSet_3/grocery/grocery.py
+++ ProblemSet_3/grocery/grocery.py
@@ -3,14 +3,8 @@
 
 einkaufsliste = {}
 while True:
-    var = input()
-    for k in einkaufsliste:
-        if k == var:
-            einkaufsliste[var] = 2
-            print(einkaufsliste)
-        else:
-            einkaufsliste[var] = 1
-            print(einkaufsliste)
+    einkaufsliste[input()] = 1
+    print(einkaufsliste)
 
 
 
