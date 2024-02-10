ba82bb8ac0e1c40249cee38813e95ed38ba01e1e
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-10 17:15:09 +0000

    Sat, Feb 10, 2024, 6:15 PM +01:00

--- ProblemSet_3/grocery/grocery.py
+++ ProblemSet_3/grocery/grocery.py
@@ -1,15 +1,12 @@
 
 def main():
     # Empty dictionary
-    shopping_list = {"key": 1}
+    shopping_list = {}
 
     while True:
         try:
-            shopping_list = input()
-            # sets the default value "v" +1
-            #shopping_list.setdefault(input()[,v + 1])
+            shopping_list.update(input())
             print(shopping_list)
-
         except EOFError:
            exit()
 
