c02d8484ce001c52872f02988d238c94e563aa53
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-10 17:56:08 +0000

    Sat, Feb 10, 2024, 6:56 PM +01:00

--- ProblemSet_3/grocery/grocery.py
+++ ProblemSet_3/grocery/grocery.py
@@ -1,13 +1,44 @@
+product = []
+amount  = [] # amout
+letter  = {} # grocery list with products and the assigned amount
 
 
 
-einkaufsliste = {}
-while True:
-    var = input()
-    einkaufsliste[var] = 1
-    for k in einkaufsliste:
-        print(k)
-    print(einkaufsliste)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#einkaufsliste = {}
+#while True:
+#    var = input()
+ #   einkaufsliste[var] = 1
+#    for k in einkaufsliste:
+ #       print(k)
+#    print(einkaufsliste)
 
 
 
