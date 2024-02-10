1f45a276853a3d189ebe52136d8f09f686624ee0
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-10 19:14:41 +0000

    Sat, Feb 10, 2024, 8:14 PM +01:00

--- ProblemSet_3/grocery/grocery.py
+++ ProblemSet_3/grocery/grocery.py
@@ -1,26 +1,22 @@
-einkaufsliste = {}
-alphalist = []
-letter = {}
+
+einkaufsliste = {} #creates empty dictionary which will be filled in the while loop
+
 while True:
     try:
-        var = input()
+        var = input() #creates variable where user inputs grocerie products
+        #if the product doesnt exist in the "einkaufsliste", then product gets value 1
+        #else the value of the product gets increased by 1
         if einkaufsliste.get(var) == None:
             einkaufsliste[var] = 1
         else:
             einkaufsliste[var] = einkaufsliste.get(var) + 1
-        einkaufsliste = dict(sorted(einkaufsliste.items()))
-        # sorts alphabeticaly
-        #alphalist = sorted(einkaufsliste)
-        print(einkaufsliste)
-        #for i in alphalist:
-
-
-
 
     except EOFError:
-        #einkaufsliste = {}
-        einkaufsliste = sorted(einkaufsliste.values())
+        # sorts the keys from dict(einkaufsliste) alphabetically
+        #einkaufsliste = dict(sorted(einkaufsliste.items()))
+        einkaufsliste = sorted(einkaufsliste.items())
         for k,v in einkaufsliste.items():
+            # changes value and key position and capitalizes all letters
             print(v,k.upper())
         exit()
 
