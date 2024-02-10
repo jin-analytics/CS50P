3cc11f1ed8f7fb4154367151b3f48fc07716e32c
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-10 18:35:26 +0000

    Sat, Feb 10, 2024, 7:35 PM +01:00

--- ProblemSet_3/grocery/grocery.py
+++ ProblemSet_3/grocery/grocery.py
@@ -5,15 +5,16 @@ while True:
         var = input()
         if einkaufsliste.get(var) == None:
             einkaufsliste[var] = 1
-            print(einkaufsliste)
+            #print(einkaufsliste)
         else:
             einkaufsliste[var] = einkaufsliste.get(var) + 1
-            print(einkaufsliste)
+            #print(einkaufsliste)
 
-        for k,v in einkaufsliste.items():
-            print(v,k.capitalize())
+
 
     except EOFError:
+        for k,v in einkaufsliste.items():
+            print(v,k.upper())
         exit()
 
 
