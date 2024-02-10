2222229eec1ad06f48d71843ba09fe8ceeccc9a9
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-10 17:25:20 +0000

    Sat, Feb 10, 2024, 6:25 PM +01:00

--- ProblemSet_3/grocery/grocery.py
+++ ProblemSet_3/grocery/grocery.py
@@ -1,16 +1,8 @@
 
-def main():
-    # Empty dictionary
-    shopping_list = {}
-
-    while True:
-        try:
-            shopping_list = shopping_list.update(input())
-            print(shopping_list)
-        except EOFError:
-           exit()
 
 
+buch = {"Jakob": 29}
+var = input()
+buch.update(var, 1)
+print (buch)
 
-
-main()
