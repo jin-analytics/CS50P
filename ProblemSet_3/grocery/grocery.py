66f5c2eed0ebdfdac075c8a78caee7172757ace4
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-10 17:04:55 +0000

    Sat, Feb 10, 2024, 6:04 PM +01:00

--- ProblemSet_3/grocery/grocery.py
+++ ProblemSet_3/grocery/grocery.py
@@ -1,9 +1,3 @@
-####### try to do this solution:
-#1 create a dictionary with inputs
-#2 associate a number to each input
-#3 if the same input occurs, add +1 to count the entrees
-
-
 
 def main():
     # Empty dictionary
