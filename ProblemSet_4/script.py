796c42ac89ac20c2c3ec9df24fd76722c3d96162
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-11 07:43:54 +0000

    Sun, Feb 11, 2024, 8:43 AM +01:00

--- ProblemSet_4/script.py
+++ ProblemSet_4/script.py
@@ -1,12 +1,14 @@
 #import random
-from random import shuffle
+import statistics
 
-for i in range(3):
+#for i in range(3):
     #coin = random.choice(["heads","tails"])
     #coin = choice(["heads","tails"])
-    cards = ["jack","queen","king"]
-    shuffle(cards)
-    for card in cards:
-        print(card)
-
+    #cards = ["jack","queen","king"]
+    #shuffle(cards)
+    #for card in cards:
+    #   print(card)
     #print(coin)
+
+grade = statistics.mean([100, 90])
+print(grade)
