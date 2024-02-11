ef083fee67d0f3fb593245c62a45c7177fbe4cbe
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-11 07:39:24 +0000

    Sun, Feb 11, 2024, 8:39 AM +01:00

--- ProblemSet_4/script.py
+++ ProblemSet_4/script.py
@@ -1,8 +1,12 @@
 #import random
-from random import choice
+from random import shuffle
 
-for i in range(100):
+for i in range(3):
     #coin = random.choice(["heads","tails"])
-    coin = choice(["heads","tails"])
+    #coin = choice(["heads","tails"])
+    cards = ["jack","queen","king"]
+    shuffle(cards)
+    for card in cards:
+        print(card)
 
-    print(coin)
+    #print(coin)
