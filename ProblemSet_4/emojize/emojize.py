156846d59eb3c76a08ba28a1649970228626d4ea
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-11 17:13:10 +0000

    Sun, Feb 11, 2024, 6:13 PM +01:00

--- ProblemSet_4/emojize/emojize.py
+++ ProblemSet_4/emojize/emojize.py
@@ -1,2 +1,10 @@
+import json
+import requests
+import sys
 
-"https://carpedm20.github.io/emoji/all.html?enableList=enable_list_alias"
+if len(sys.argv) != 2:
+    sys.exit()
+response = requests.get("https://carpedm20.github.io/emoji/all.html?enableList=enable_list_alias")
+
+o = response.json()
+for result in o:
