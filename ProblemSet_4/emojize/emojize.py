99ef35529eb639210709230ba4441fc962a22607
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-12 18:23:23 +0000

    Mon, Feb 12, 2024, 7:23 PM +01:00

--- ProblemSet_4/emojize/emojize.py
+++ ProblemSet_4/emojize/emojize.py
@@ -4,9 +4,9 @@ import requests
 
 
 
-r = requests.get('https://carpedm20.github.io/emoji/all.html?enableList=enable_list_alias')
+r = requests.get('https://carpedm20.github.io/emoji/all.html?enableList=enable_list_alias', auth=('user', 'pass'))
 
-print(r.json())
+print(r.encoding())
 #https://carpedm20.github.io/emoji/all.html?enableList=enable_list_alias
 
 
