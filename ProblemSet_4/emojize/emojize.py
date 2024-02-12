56fd045fd065d6044b05f347b50ceff71ae67c74
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-12 18:37:00 +0000

    Mon, Feb 12, 2024, 7:37 PM +01:00

--- ProblemSet_4/emojize/emojize.py
+++ ProblemSet_4/emojize/emojize.py
@@ -7,10 +7,13 @@ if len(sys.argv) != 2:
     sys.exit()
 
 #r = requests.get('https://carpedm20.github.io/emoji/all.html?enableList=enable_list_alias', auth=('user', 'pass'))
-r= requests.get("https://itunes.apple.com/search?entity=song&limit1&term=trackname=" + sys.argv[1])
-print(json.dumps(r.json(), indent=2))
+r= requests.get("https://itunes.apple.com/search?entity=song&limit1&term=" + sys.argv[1])
 
 
+o = r.json()
+for result in o["results"]:
+    #print(json.dumps(r.json(), indent=2))
+    print(result["trackName"])
 
 #https://carpedm20.github.io/emoji/all.html?enableList=enable_list_alias
 
