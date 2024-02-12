27401b8a03ac2dc7ffd26fdfaa89b5aba0ccba87
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-12 18:44:20 +0000

    Mon, Feb 12, 2024, 7:44 PM +01:00

--- ProblemSet_4/emojize/emojize.py
+++ ProblemSet_4/emojize/emojize.py
@@ -3,17 +3,17 @@ import requests
 import json
 
 
-if len(sys.argv) != 2:
-    sys.exit()
+#if len(sys.argv) != 2:
+#    sys.exit()
 
 #r = requests.get('https://carpedm20.github.io/emoji/all.html?enableList=enable_list_alias', auth=('user', 'pass'))
-response = requests.get("https://itunes.apple.com/search?entity=song&limit1&term=" + sys.argv[1])
+response = requests.get("https://carpedm20.github.io/emoji/all.html?enableList=enable_list_alias")
 
 
 object = response.json()
 for result in object["results"]:
     #print(json.dumps(r.json(), indent=2))
-    print(result["trackName"])
+    print(result)
 
 #https://carpedm20.github.io/emoji/all.html?enableList=enable_list_alias
 
