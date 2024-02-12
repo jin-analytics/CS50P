c02c5f2c2bcc95bdda6a1a766e44cb8d50c8fb86
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-12 17:47:51 +0000

    Mon, Feb 12, 2024, 6:47 PM +01:00

--- ProblemSet_4/emojize/emojize.py
+++ ProblemSet_4/emojize/emojize.py
@@ -8,3 +8,13 @@
 #print(emoji.emojize('Python is :thumbs_up:'))
 
 
+def main():
+    height = int(input())
+    pyramid(height)
+
+def pyramid(n):
+    for i in range(n):
+        print("#" * i)
+
+if __name__ == "__main__":
+    main()
