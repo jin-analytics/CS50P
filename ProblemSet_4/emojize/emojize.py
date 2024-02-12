c343531533160b8ecf34f1a8b23951a3cf09e8ce
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-12 18:13:32 +0000

    Mon, Feb 12, 2024, 7:13 PM +01:00

--- ProblemSet_4/emojize/emojize.py
+++ ProblemSet_4/emojize/emojize.py
@@ -1,10 +1,9 @@
-import cowsay
+import sys
 
-cowsay.stegosaurus('Hello World')
+
+
 
 
 #https://carpedm20.github.io/emoji/all.html?enableList=enable_list_alias
-#print(emoji.emojize('Python is :thumbs_up:'))
-
 
 
