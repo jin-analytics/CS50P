975a269c12896aa7fa422ea72b17dc0074310197
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-12 18:50:49 +0000

    Mon, Feb 12, 2024, 7:50 PM +01:00

--- ProblemSet_4/emojize/emojize.py
+++ ProblemSet_4/emojize/emojize.py
@@ -8,12 +8,13 @@ import json
 
 #r = requests.get('https://carpedm20.github.io/emoji/all.html?enableList=enable_list_alias', auth=('user', 'pass'))
 response = requests.get("https://carpedm20.github.io/emoji/all.html?enableList=enable_list_alias")
-r = requests.post('https://httpbin.org/post', data={'key': 'value'})
+#r = requests.post('https://httpbin.org/post', data={'key': 'value'})
 
 #object = response.json()
 #for result in object["results"]:
     #print(json.dumps(r.json(), indent=2))
-print(r)
+r.tec
+print(response)
 
 #https://carpedm20.github.io/emoji/all.html?enableList=enable_list_alias
 
