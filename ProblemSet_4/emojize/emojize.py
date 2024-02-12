03832a238f683960067fa9c53dfcdaf71e690e9d
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-12 18:30:51 +0000

    Mon, Feb 12, 2024, 7:30 PM +01:00

--- ProblemSet_4/emojize/emojize.py
+++ ProblemSet_4/emojize/emojize.py
@@ -1,12 +1,17 @@
 import sys
 import requests
+import json
 
 
-
+if len(sys.argv) != 2:
+    sys.exit()
 
 #r = requests.get('https://carpedm20.github.io/emoji/all.html?enableList=enable_list_alias', auth=('user', 'pass'))
 r= requests.get("https://itunes.apple.com/search?entity=song&limit01&term=" + sys.argv[1])
-print(r.json())
+print(json.dumps(r.json(), indent=2))
+
+
+
 #https://carpedm20.github.io/emoji/all.html?enableList=enable_list_alias
 
 
