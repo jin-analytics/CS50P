70f8b1925a934db7288695e29d1a2f148320bd3c
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-13 11:33:57 +0000

    Tue, Feb 13, 2024, 12:33 PM +01:00

--- ProblemSet_4/emojize/emojize.py
+++ ProblemSet_4/emojize/emojize.py
@@ -7,11 +7,12 @@ import json
 #    sys.exit()
 
 response = requests.get("https://carpedm20.github.io/emoji/all.html?enableList=enable_list_alias")
-response = requests.response("https://carpedm20.github.io/emoji/all.html?enableList=enable_list_alias")
+#response = requests.response("https://carpedm20.github.io/emoji/all.html?enableList=enable_list_alias")
 #response.json()
 #for result in response:
     #print(json.dumps(r.json(), indent=2))
     #print(result["WRESTLERS"])
-print(response.headers)
+res_json = response.json()
+print(res_json)
 
 #print(response.text)
