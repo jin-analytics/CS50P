3dc3d41cf23c0ae610e8e036f52403b35f125641
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-17 07:37:25 +0000

    Sat, Feb 17, 2024, 8:37 AM +01:00

--- ProblemSet_4/emojize/emojize.py
+++ ProblemSet_4/emojize/emojize.py
@@ -2,10 +2,9 @@ import sys
 import requests
 import json
 
-.get("https://carpedm20.github.io/emoji/all.html?enableList=enable_list_alias")
+response = requests.get("https://carpedm20.github.io/emoji/all.html?enableList=enable_list_alias")
 
-data = response.json()
-thumbs = data["detective"]
+data = response.content
 print(data)
 
 
