75ab669c99c35ab586a3ef482080d84ea3bfcbd6
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-22 06:17:11 +0000

    Thu, Feb 22, 2024, 7:17 AM +01:00

--- ProblemSet_4/emojize/emojize.py
+++ ProblemSet_4/emojize/emojize.py
@@ -2,6 +2,8 @@ import sys
 import requests
 import json
 
+import emoji
+
 
 #payload = {'key1': 'value1', 'key2': 'value2'}
 #r = requests.get('https://httpbin.org/get', params=payload)
@@ -9,9 +11,9 @@ import json
 #r = requests.get('https://api.github.com/user', auth=('user', 'pass'))
 #print(r)
 
-response = requests.get("https://carpedm20.github.io/emoji/all.html?enableList=enable_list_alias")
-response = requests.get("https://www.corporationwiki.com/search/results?term=jim%20smith")
-response.raw
+#response = requests.get("https://carpedm20.github.io/emoji/all.html?enableList=enable_list_alias")
+#response = requests.get("https://www.corporationwiki.com/search/results?term=jim%20smith")
+##response.raw
 
 #data = response.raw
 print(response.text)
@@ -19,4 +21,5 @@ print(response.text)
 #for i in range(100):
  #   print("Buongiorno Bella")
 
-
+import emoji
+print(emoji.emojize('Python is :thumbs_up:'))
