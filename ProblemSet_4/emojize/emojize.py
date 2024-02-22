f69d23a8081f73de232e34d283fd0e43f8873afc
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-22 17:44:08 +0000

    Thu, Feb 22, 2024, 6:44 PM +01:00

--- ProblemSet_4/emojize/emojize.py
+++ ProblemSet_4/emojize/emojize.py
@@ -5,7 +5,7 @@ def main():
     #line_check(emo)
     #print(emoji.emojize(f"Output: {emo}", language='alias'))
 
-    emoji_selected = emo.split(":")
+    emoji_selected = emo.split(':')
     print(emoji_selected)
 
 #__________________________________________________
