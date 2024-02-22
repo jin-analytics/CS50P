214abdc656ae8ac7572bc2e04e045260e4e17812
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-22 17:30:31 +0000

    Thu, Feb 22, 2024, 6:30 PM +01:00

--- ProblemSet_4/emojize/emojize.py
+++ ProblemSet_4/emojize/emojize.py
@@ -1,10 +1,19 @@
 import emoji
 
-emo = input("Input: ")
+def main():
+    emo = input("Input: ")
+    line_check(emo)
+    print(emoji.emojize(f"Output: {emo}", language='alias'))
 
-for i in emo:
-    #print(i)
-    if i == "_":
-        print(emoji.emojize(f"Output: {emo}"))
 
-print(emoji.emojize(f"Output: {emo}", language='alias'))
+
+#__________________________________________________
+# Checks if ther is a _ inside the string "emo"
+def line_check(e):
+
+    for i in e:
+        if i == "_":
+            print(emoji.emojize(f"Output: {e}"))
+
+
+main()
