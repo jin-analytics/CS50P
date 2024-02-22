87e6fdb72e5d4306aa0d865a4426c10e2ae4cd5a
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-22 17:16:24 +0000

    Thu, Feb 22, 2024, 6:16 PM +01:00

--- ProblemSet_4/emojize/emojize.py
+++ ProblemSet_4/emojize/emojize.py
@@ -1,7 +1,11 @@
 import emoji
-print(emoji.emojize('Python is :thumbs_up:'))
-print(emoji.emojize('Python is :thumbsup:', language='alias'))
+#print(emoji.emojize('Python is :thumbs_up:'))
+#print(emoji.emojize('Python is :thumbsup:', language='alias'))
 
 
 emo = input("Input: ")
+
+for _ in emo:
+    print(emo)
+
 print(f"Output: {emo}")
