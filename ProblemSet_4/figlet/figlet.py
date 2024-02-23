6fe8f740c794c5b220712b262555bad21b7471b1
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-23 07:01:38 +0000

    Fri, Feb 23, 2024, 8:01 AM +01:00

--- ProblemSet_4/figlet/figlet.py
+++ ProblemSet_4/figlet/figlet.py
@@ -1,10 +1,11 @@
 import sys
+import Figlet
 
 
 if len(sys.argv) <2:
     sys.exit("too few") # sys.exit | exits the program
 
-for arg in sys.argv[1:]:
-    print("hello", arg)
+for argument in sys.argv[1:]: # Slices | the brackets [] scliding up a list - in this case start a No. 1 until infinity
+    print("hello", argument)
 
 #print(sys.argv[1])
