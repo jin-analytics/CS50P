97f2b0f12394a2914db40d036664b695b993ede1
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-23 19:34:02 +0000

    Fri, Feb 23, 2024, 8:34 PM +01:00

--- ProblemSet_4/figlet/figlet.py
+++ ProblemSet_4/figlet/figlet.py
@@ -1,7 +1,13 @@
 import sys
 from pyfiglet import Figlet
 from pyfiglet import fonts.zip
-print()
+# specifying the zip file name
+file_name = "my_python_files.zip"
+# opening the zip file in READ mode
+with ZipFile(file_name, 'r') as zip:
+# printing all the contents of the zip file
+zip.printdir()
+# extracting all the files
 #Figlet.system("python yourfile.py")
 
 #Font: italic
