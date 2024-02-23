791532d37e9335b89eeeb1fd0a201fa58d905ed8
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-23 06:40:57 +0000

    Fri, Feb 23, 2024, 7:40 AM +01:00

--- ProblemSet_4/emojize/emojize.py
+++ ProblemSet_4/emojize/emojize.py
@@ -1,22 +1,13 @@
 import emoji
-import re
-
-#x = "This is a sentence. (once a day) [twice a day]"
-#re.sub("([\(\[]).*?([\)\]])", "\g<1>\g<2>", x)
 
 def main():
     emo = input("Input: ")
     line_check(emo)
     print(emoji.emojize(f"Output: {emo}", language='alias'))
 
-    #re.sub("::", "", emo)
-    #emoji_selected = emo.split(':', maxsplit = 1)
-    #print(re.sub("::", "", emo))
 
-#__________________________________________________
 # Checks if ther is a _ inside the string "emo"
 def line_check(e):
-
     for i in e:
         if i == "_":
             print(emoji.emojize(f"Output: {e}"))
