e6c902765078a3b379afc90d9da015424d2706f9
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-23 19:58:20 +0000

    Fri, Feb 23, 2024, 8:58 PM +01:00

--- ProblemSet_4/figlet/figlet.py
+++ ProblemSet_4/figlet/figlet.py
@@ -4,28 +4,31 @@ import pyfiglet
 import random
 
 # gets the list of usable fonts
-font = pyfiglet.FigletFont.getFonts()
+randomfont= pyfiglet.FigletFont.getFonts()
 # gets the length of the fontlist
-l = len(font)
+l = len(randomfont)
 # choose random number of font list
 z = random.randint(0,l)
 # use the font with the random number
-font = font[z]
+font = randomfont[z]
 
-#if len(sys.argv) <2:
-#    sys.exit("too few") # sys.exit | exits the program
+if len(sys.argv) > 2:
+    sys.exit("too many arguments") # sys.exit | exits the program
 
 #for argument in sys.argv[1:]: # Slices | the brackets [] scliding up a list - in this case start a No. 1 until infinity
 #    print("hello", argument)
 
-#f = Figlet(font='italic')
-if len(sys.argv) = 2: #if "python figlet.py -f font"
+
+elif len(sys.argv) == 2: #if "python figlet.py -f font"
 #print(sys.argv)
     f = Figlet(font = sys.argv[2])
-    t = input("Input: ")
-    print(f.renderText(t))
+    text = input("Input: ")
+    print(f.renderText(text))
 
 elif len(sys.argv) < 2: #if only "python figlet.py"
-    t = input("Input: ")
-    #f = Figlet(font = FigletFonts.getFonts())
-    print(pyfiglet.FigletFont.getFonts())
+    text = input("Input: ")
+    f = Figlet(font = randomfont)
+    print(f.renderText(text))
+
+else:
+    exit()
