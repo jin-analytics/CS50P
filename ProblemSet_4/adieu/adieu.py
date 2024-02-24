9f22f5741ff19f9d577cf501c5a7986a7eb568e1
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-24 14:19:10 +0000

    Sat, Feb 24, 2024, 3:19 PM +01:00

--- ProblemSet_4/adieu/adieu.py
+++ ProblemSet_4/adieu/adieu.py
@@ -5,7 +5,13 @@ def main():
     while True:
 
         try:
-            n = input("Name: ")
+            n = input("Name: ") #creates variable where the name gets added to the dictionary "adieulist"
+            #if the product doesnt exist in the "einkaufsliste", then product gets value 1
+            #else the value of the product gets increased by 1
+            if adieulist.get(n) == None:
+                adieulist[n] = 1
+            else:
+                einkaufsliste[n] = einkaufsliste.get(n) + 1
 
         except EOFError:
             print(f"Adieu, adieu, to {n}")
