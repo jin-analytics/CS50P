229cf5ff3c3d4df2f612427c01dde57d29e3c523
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-24 14:06:51 +0000

    Sat, Feb 24, 2024, 3:06 PM +01:00

--- ProblemSet_4/adieu/adieu.py
+++ ProblemSet_4/adieu/adieu.py
@@ -1,2 +1,20 @@
 
 
+def main():
+    while True:
+
+        try:
+            n = input("Name: ")
+
+        except EOFError:
+            print(f"Adieu {n}")
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
