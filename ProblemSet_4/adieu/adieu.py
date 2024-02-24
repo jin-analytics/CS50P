506413a28b76841b23769467619b200dea293904
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-24 18:24:23 +0000

    Sat, Feb 24, 2024, 7:24 PM +01:00

--- ProblemSet_4/adieu/adieu.py
+++ ProblemSet_4/adieu/adieu.py
@@ -25,5 +25,11 @@ def main():
 
             exit()
 
+def name_string(adieulist):
+    comma = adieulist[-len(adieulist):-1] # creates a list without the last entree
+    comma = ", ".join(comma) # makes a string out of the "comma" list
+    return comma
+
+
 if __name__ == "__main__":
     main()
