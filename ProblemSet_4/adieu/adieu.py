2b98d903a506cd4a076cd27ce7d44052b2b72e04
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-24 15:21:28 +0000

    Sat, Feb 24, 2024, 4:21 PM +01:00

--- ProblemSet_4/adieu/adieu.py
+++ ProblemSet_4/adieu/adieu.py
@@ -18,7 +18,8 @@ def main():
                 #print(len(adieulist))
                 print(f"Adieu, adieu, to {adieulist[-len(adieulist)]} and {adieulist[-n]}") #last entree of somelist[-n]
             elif len(adieulist) > 2:
-                print(f"{name_string(adieulist)}")
+                #print(f"{name_string(adieulist)}")
+                print(f"{adieulist[-n]}")
                 #print(f"Adieu, adieu, to {name_string(adieulist)} and {adieulist[-n]}")
 
 
