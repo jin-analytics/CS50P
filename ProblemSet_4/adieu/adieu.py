4a081dfa5ecbbc6d0e573c8ac4b60695429c5e6c
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-24 14:53:54 +0000

    Sat, Feb 24, 2024, 3:53 PM +01:00

--- ProblemSet_4/adieu/adieu.py
+++ ProblemSet_4/adieu/adieu.py
@@ -11,20 +11,26 @@ def main():
 
         except EOFError:
             if len(adieulist) == 1:
-              print(len(adieulist))
-              print(f"Adieu, adieu, to {adieulist[0]}")
+                #print(len(adieulist))
+                print(f"Adieu, adieu, to {adieulist[-len(adieulist)]}") #first entree of somelist[-len(somelist)]
 
             elif len(adieulist) == 2:
-              print(len(adieulist))
-              print(f"Adieu, adieu, to {adieulist[0]} and {adieulist[1]}")
-
+                #print(len(adieulist))
+                print(f"Adieu, adieu, to {adieulist[-len(adieulist)]} and {adieulist[-n]}") #last entree of somelist[-n]
             elif len(adieulist) > 2:
+                #print(len(adieulist))
+                print(f"Adieu, adieu, to {name_string(adieulist)} and {adieulist[-n]}")
 
 
             exit()
 
+# creates a string of all names (except the last one), wihich will all be seperated
+# by a comma and then returns the single string of these names
+def name_string(adieulist):
+    for entrees in adieulist:
+        print(entrees)
+    exit()
 
-def 
 
 
 
