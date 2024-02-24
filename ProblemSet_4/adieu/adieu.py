6327ff1ab0690d288203bab69f4739a8fa891026
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-24 14:26:58 +0000

    Sat, Feb 24, 2024, 3:26 PM +01:00

--- ProblemSet_4/adieu/adieu.py
+++ ProblemSet_4/adieu/adieu.py
@@ -1,4 +1,4 @@
-adieulist = [] #creates empty list which will be filled in the while loop
+einkaufsliste = {} #creates empty list which will be filled in the while loop
 
 
 def main():
@@ -6,9 +6,15 @@ def main():
 
         try:
             #n = input("Name: ") #creates variable where the name gets added to the list "adieulist"
+            var = input() #creates variable where user inputs grocerie products
+            #if the product doesnt exist in the "einkaufsliste", then product gets value 1
+            #else the value of the product gets increased by 1
+            if einkaufsliste.get(var) == None:
+                einkaufsliste[var] = 1
+            else:
+                einkaufsliste[var] = einkaufsliste.get(var) + 1
 
-            adieulist[input("Name: ")]
-            print(adieulist)
+            print(einkaufsliste)
 
         except EOFError:
             print(f"Adieu, adieu, to {n}")
