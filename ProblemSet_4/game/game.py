ab55977c22fd9bbec4cf24ed2ad8f091bbe668a2
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-24 17:51:01 +0000

    Sat, Feb 24, 2024, 6:51 PM +01:00

--- ProblemSet_4/game/game.py
+++ ProblemSet_4/game/game.py
@@ -1,11 +1,24 @@
 import random
 
-while True:
-    try:
-        lev = int(input("Level: "))
-        num = random.randint(1,lev)
-        print(num)
+def main():
+    lev = int(input("Level: "))
+    num = random.randint(1,lev)
 
-        
-    except EOFError:
-        exit()
+    while True:
+        try:
+            print(num)
+            gue = int(input("Guess: "))
+
+            if gue == num:
+                print("Just right!")
+                exit()
+            elif gue < num:
+                print("Too small!")
+            elif gue > num:
+                print("Too large!")
+
+        except EOFError:
+            exit()
+
+if __name__ == "__main__":
+    main()
