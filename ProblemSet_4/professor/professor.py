e31276bcabd1e367bb822058ef12608ed544c9d8
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-25 11:41:22 +0000

    Sun, Feb 25, 2024, 12:41 PM +01:00

--- ProblemSet_4/professor/professor.py
+++ ProblemSet_4/professor/professor.py
@@ -3,7 +3,18 @@ import random
 
 def main():
     level = get_level()
-    generate_integer(level)
+    numbers = generate_integer(level)
+    x = numbers[0]
+    y = numbers[1]
+    solution = x + y
+    print(x,"+",y,"= ", end = "")
+    guess = int(input())
+    #print(solution)
+    #if guess != solution:
+        #print("EEE")
+    #else:
+        #print("correct")
+        #return True
 
 def get_level():
     while True:
@@ -11,49 +22,35 @@ def get_level():
             lev = int(input("Level: "))
             if 1 <= lev <= 3: #just accepts input of [1;3]
                 return lev
-
         except ValueError:
             print("", end = "")
-
         except EOFError:
             return False
 
 
 def generate_integer(level):
-
-        #__________________________________Level 1
     try:
+#__________________________________Level 1
         if level == 1:
             x = random.randint(1,9)
             y = random.randint(1,9)
-                    print(x,"+",y,"= ", end = "")
-                    solution = x + y
-                    guess = int(input())
-                    print(solution)
-                    if guess != solution:
-                        print("EEE")
-                    else:
-                        print("correct")
-                        return True
-        #__________________________________Level 2
-            while True:
-            elif level == 2:
-                x = random.randint(1,99)
-                y = random.randint(1,99)
-                print(x,"+",y,"= ", end = "")
-                int(input())
-        #__________________________________Level 3
-            while True:
-            elif level == 3:
-                x = random.randint(1,999)
-                y = random.randint(1,999)
-                print(x,"+",y,"= ", end = "")
-                int(input())
-
-        except ValueError:
-            print("", end = "")
-        except EOFError:
-            return False
+            print(x,"+",y,"= ", end = "")
+            return x,y
+#__________________________________Level 2
+        elif level == 2:
+            x = random.randint(1,99)
+            y = random.randint(1,99)
+            print(x,"+",y,"= ", end = "")
+            return x,y
+#__________________________________Level 3
+        elif level == 3:
+            x = random.randint(1,999)
+            y = random.randint(1,999)
+            return x,y
+    except ValueError:
+        print("", end = "")
+    except EOFError:
+        return False
 
 if __name__ == "__main__":
     main()
