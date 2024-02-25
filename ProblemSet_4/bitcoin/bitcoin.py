8d192c882f226da0cb036bdde0a804d8bdc33f2b
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-25 15:43:05 +0000

    Sun, Feb 25, 2024, 4:43 PM +01:00

--- ProblemSet_4/bitcoin/bitcoin.py
+++ ProblemSet_4/bitcoin/bitcoin.py
@@ -2,7 +2,12 @@ import sys
 import requests
 
 try:
-    coin = sys.argv[1]
+    if len(sys.argv) <2: #catches if there is less then one entree
+        sys.exit("too few")
+    if len(sys.argv) >2: #catches if there is more then one entree
+        sys.exit("too many")
+
+    coin = float(sys.argv[1]) #defines the n-amounts of coins
     print(coin)
     data = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
     print(data.json())
