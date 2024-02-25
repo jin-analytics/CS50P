46b0ddb843f8ebb1c21f75375c1941d074e809d3
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-25 11:14:46 +0000

    Sun, Feb 25, 2024, 12:14 PM +01:00

--- ProblemSet_4/professor/professor.py
+++ ProblemSet_4/professor/professor.py
@@ -2,25 +2,46 @@ import random
 
 
 def main():
-    get_level()
+    level = get_level()
+    generate_integer(level)
 
 def get_level():
     while True:
         try:
             lev = int(input("Level: "))
             if 1 <= lev <= 3: #just accepts input of [1;3]
-                num = random.randint(1,lev)
-                print(num)
-                break
+                return lev
+
         except ValueError:
             print("", end = "")
+
         except EOFError:
             return False
 
 
 def generate_integer(level):
-    ...
+    #_________Level 1_________
+    if level == 1:
+        x = random.randint(1,9)
+        y = random.randint(1,9)
+        #print(x,"+",y,"=")
+        exercise = str((x,"+",y,"="))
+        int(input(exercise))
 
+
+    #_________Level 2_________
+    elif level == 2:
+        x = random.randint(1,99)
+        y = random.randint(1,99)
+        #print(x,"+",y,"=")
+        int(input(x,"+",y,"="))
+
+    #_________Level 3_________
+    elif level == 3:
+        x = random.randint(1,999)
+        y = random.randint(1,999)
+        #print(x,"+",y,"=")
+        int(input(x,"+",y,"="))
 
 if __name__ == "__main__":
     main()
