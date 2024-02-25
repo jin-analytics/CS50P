df7a8b9e0fe1365d94915d614dd98a6b79b95c5c
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-25 16:32:32 +0000

    Sun, Feb 25, 2024, 5:32 PM +01:00

--- ProblemSet_4/bitcoin/bitcoin.py
+++ ProblemSet_4/bitcoin/bitcoin.py
@@ -13,8 +13,8 @@ try:
     data = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
     bitcoin = data.json()
     print(json.dumps(bitcoin, indent = 2))
-    for result in bitcoin["bpi"]["USD"]:
-        value = result[3]
+    for result in bitcoin["bpi"]["USD"]["rate_float"]:
+        value = float[0]
         print("$", value)
 
 except requests.RequestException:
