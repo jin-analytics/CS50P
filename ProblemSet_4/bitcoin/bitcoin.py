500e2047865d804c1532006826acca38d2501bd3
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-25 15:38:53 +0000

    Sun, Feb 25, 2024, 4:38 PM +01:00

--- ProblemSet_4/bitcoin/bitcoin.py
+++ ProblemSet_4/bitcoin/bitcoin.py
@@ -4,6 +4,7 @@ import requests
 try:
     coin = sys.argv[1]
     print(coin)
-    data = re
+    data = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+    print(data.json)
 except requests.RequestException:
     ...
