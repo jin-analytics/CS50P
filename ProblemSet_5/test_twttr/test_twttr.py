3757ff485cf460d925be17e05e59088254b3ea6b
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-27 13:08:23 +0000

    Tue, Feb 27, 2024, 2:08 PM +01:00

--- ProblemSet_5/test_twttr/test_twttr.py
+++ ProblemSet_5/test_twttr/test_twttr.py
@@ -1,14 +1,13 @@
+from twttr import shorten
+
 def main():
     tweet = input("Input: ")
     print("Output:", shorten(tweet))
 
-# checks if any vowel is inside the "tweet" and removes it, then gives it ("word") back
-def shorten(word):
-    for vowels in word:
-        match vowels:
-            case "a" | "e" | "i" | "o" | "u" | "A" | "E" | "I" | "O" | "U":
-                word = word.replace(vowels,"")
-    return word
+def test_lower():
+
+def test_upper():
+    
 
 # if __name__ == "__main__": - helps to prevent errors due to not defined global variables yet, if we call main() inside there
 if __name__ == "__main__":
