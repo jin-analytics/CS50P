7e6a6e392f0b18f49401d716d0c15a0549289e95
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-27 13:47:06 +0000

    Tue, Feb 27, 2024, 2:47 PM +01:00

--- ProblemSet_5/test_twttr/test_twttr.py
+++ ProblemSet_5/test_twttr/test_twttr.py
@@ -4,6 +4,7 @@ def main():
     tweet = input("Input: ")
     #print("Output:", shorten(tweet))
     test_lower(tweet)
+    test_upper(tweet)
 
 # compare the selected vowes with the one removed
 def test_lower(tw):
@@ -16,9 +17,15 @@ def test_lower(tw):
       except AssertionError:
          print("Still some vowels left... ->", vowels)
 
-
-
-#def test_upper():
+def test_upper(tw):
+    shortened_word = shorten(tw)
+    for letters in shortened_word:
+      try:
+        vowelslist = ["A","E","I","O","U"]
+        for vowels in vowelslist:
+            assert letters != vowels
+      except AssertionError:
+         print("Still some vowels left... ->", vowels)
 
 
 # if __name__ == "__main__": - helps to prevent errors due to not defined global variables yet, if we call main() inside there
