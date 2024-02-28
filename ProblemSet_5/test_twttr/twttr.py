a96e50380030412d0b845feecd94c1181dd11331
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-28 15:00:27 +0000

    Wed, Feb 28, 2024, 4:00 PM +01:00

--- ProblemSet_5/test_twttr/twttr.py
+++ ProblemSet_5/test_twttr/twttr.py
@@ -3,7 +3,8 @@ def main():
     print("Output:", shorten(tweet))
 
 # checks if any vowel is inside the "tweet" and removes it, then gives it ("word") back
-def shorten(word = "twitter123#"):
+#def shorten(word = "twitter123#"):
+def shorten(word):
     for _ in word:
         match _: #vowels
             case "a" | "e" | "i" | "o" | "u" | "A" | "E" | "I" | "O" | "U":
