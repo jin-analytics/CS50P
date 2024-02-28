53f381a96871ddd27ae37c8125c1473a8374813d
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-28 16:21:37 +0000

    Wed, Feb 28, 2024, 5:21 PM +01:00

--- ProblemSet_5/test_bank/bank.py
+++ ProblemSet_5/test_bank/bank.py
@@ -7,13 +7,14 @@ def main():
 def value(greeting):
     # Split it in a matrix (1 x n) to select the first "Hello"
     greeting = greeting.split()
+    print(greeting)
     match greeting:
     # Select different types of hello in the first column of the matrix...
     # ... with *rest will be the rest of the column entrees ignored!
         case ["Hello," | "Hello" | "hello", *rest]:
             #print("$0")
             val = int(0)
-            
+
         case _:
             greeting = ' '.join(greeting)
             if greeting[0] == "H":
