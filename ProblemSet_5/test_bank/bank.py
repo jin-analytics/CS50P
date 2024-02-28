adcbb43e639297457ff26d0d3c2c61bd11cf37a1
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-28 16:07:45 +0000

    Wed, Feb 28, 2024, 5:07 PM +01:00

--- ProblemSet_5/test_bank/bank.py
+++ ProblemSet_5/test_bank/bank.py
@@ -1,28 +1,26 @@
-greeting = input("Greeting: ")
-
-# Split it in a matrix (1 x n) to select the first "Hello"
-greeting = greeting.split()
-
-match greeting:
-    # Select different types of hello in the first column of the matrix...
-    # ... with *rest will be the rest of the column entrees ignored!
-    case ["Hello," | "Hello" | "hello", *rest]:
-        print("$0")
-    case _:
-        # Joing the matrix back together, so that you can...
-        # ... select the letter and not the full word!
-        greeting = ' '.join(greeting)
-        if greeting[0] == "H":
-            print("$20")
-        else:
-            print("$100")
 
 def main():
-    ...
+    greeting = input("Greeting: ")
+    value(greeting)
 
 
 def value(greeting):
-    ...
+    # Split it in a matrix (1 x n) to select the first "Hello"
+    greeting = greeting.split()
+    match greeting:
+    # Select different types of hello in the first column of the matrix...
+    # ... with *rest will be the rest of the column entrees ignored!
+        case ["Hello," | "Hello" | "hello", *rest]:
+            print("$0")
+        case _:
+        # Joing the matrix back together, so that you can...
+        # ... select the letter and not the full word!
+            greeting = ' '.join(greeting)
+            if greeting[0] == "H":
+                print("$20")
+            else:
+                print("$100")
+
 
 
 if __name__ == "__main__":
