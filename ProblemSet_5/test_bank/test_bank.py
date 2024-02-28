e1d587d372af0b041cd626da25e19acefe2a18ae
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-28 16:35:28 +0000

    Wed, Feb 28, 2024, 5:35 PM +01:00

---
+++ ProblemSet_5/test_bank/test_bank.py
@@ -0,0 +1,12 @@
+from test_bank import value
+
+def main():
+    greeting = str(input("Greeting: "))
+    test_hello(greeting)
+
+def test_hello():
+    value()
+
+
+if __name__ == "__main__":
+    main()
