91d78200d7d00c5454b7353b29e7992f263eafdd
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-29 19:53:58 +0000

    Thu, Feb 29, 2024, 8:53 PM +01:00

--- ProblemSet_5/test_twttr/twttr.py
+++ ProblemSet_5/test_twttr/twttr.py
@@ -11,8 +11,8 @@ def shorten(word):
                 word = word.replace(_,"")
             case "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9":
                 word = word.replace(_,"")
-            case "!" | "§" | "$" | "%" | "&" | "/" | "=" | "?" | "." | "," | "-" | "*":
-                word = word.replace(_,"")
+            #case "!" | "§" | "$" | "%" | "&" | "/" | "=" | "?" | "." | "," | "-" | "*":
+                #word = word.replace(_,"")
         #if _.isspace() != True:
             #if _.isalpha() == False:
                 #word = word.replace(_,"")
