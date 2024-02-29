48568f08632b8ec3aaace83601ee10bba751c359
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-29 19:42:59 +0000

    Thu, Feb 29, 2024, 8:42 PM +01:00

--- ProblemSet_5/test_twttr/test_twttr.py
+++ ProblemSet_5/test_twttr/test_twttr.py
@@ -2,13 +2,13 @@ from twttr import shorten
 
 # compare the selected vowes with the one removed
 def test_lowercase_vowels(): #a,e,i,o,u
-    tweet = ("twitter") # Output should be: "twttr"
-    assert shorten(tweet) == "twttr"
+    word = ("twitter") # Output should be: "twttr"
+    assert shorten(word) == "twttr"
 
 def test_lowercase_vowels_and_sentences(): #a,e,i,o,u
-    tweet = ("twitter and ouchie") # Output should be: "twttr nd ch"
-    assert shorten(tweet) == "twttr nd ch"
+    word = ("twitter and ouchie") # Output should be: "twttr nd ch"
+    assert shorten(word) == "twttr nd ch"
 
 def test_numbers_spaces_and_signs(): #1,2,3,4,5,6,7,8,9
-    tweet = ("combo 1-2-3, left 4,5,6 & 7 8 9") # Output should be: "cmb  lft     "
-    assert shorten(tweet) == "cmb  lft     "
+    word = ("combo 1-2-3, left 4,5,6 & 7 8 9") # Output should be: "cmb  lft     "
+    assert shorten(word) == "cmb  lft     "
