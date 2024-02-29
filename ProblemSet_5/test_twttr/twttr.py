8bf8be541bcc524ceff1bb0710ba737bf23ebf83
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-29 19:30:34 +0000

    Thu, Feb 29, 2024, 8:30 PM +01:00

--- ProblemSet_5/test_twttr/twttr.py
+++ ProblemSet_5/test_twttr/twttr.py
@@ -11,7 +11,7 @@ def shorten(word):
                 word = word.replace(_,"")
             case "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9":
                 word = word.replace(_,"")
-        if _.isalpha() and _.isspace() == False:
+        if _.isalpha() == False:
             word = word.replace(_,"")
 
     return word
