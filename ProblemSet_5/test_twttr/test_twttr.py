8ff468b9ced954a47027e571cd8299fb00fdd92a
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-29 19:18:37 +0000

    Thu, Feb 29, 2024, 8:18 PM +01:00

--- ProblemSet_5/test_twttr/test_twttr.py
+++ ProblemSet_5/test_twttr/test_twttr.py
@@ -1,13 +1,5 @@
 from twttr import shorten
 
-def main():
-    #tweet = str("1234567890!§$%&/=twitter")
-    #tweet = input("Input: ")
-    test_lower()
-    test_upper()
-#    test_numbers()
-#    test_signs()
-
 # compare the selected vowes with the one removed
 def test_lower():
    shortened_word = shorten()
@@ -16,12 +8,6 @@ def test_lower():
       vowelslist = ["a","e","i","o","u"]
       for vowels in vowelslist:
           assert letters != vowels
-#      try:
-#        vowelslist = ["a","e","i","o","u"]
-#        for vowels in vowelslist:
-#            assert letters != vowels
-#      except AssertionError:
-#         print("Still some vowels left... ->", vowels)
 
 def test_upper():
     shortened_word = shorten()
@@ -29,25 +15,3 @@ def test_upper():
         vowelslist = ["A","E","I","O","U"]
         for vowels in vowelslist:
             assert letters != vowels
-#      try:
-#        vowelslist = ["A","E","I","O","U"]
-#        for vowels in vowelslist:
-#            assert letters != vowels
-#      except AssertionError:
-#         print("Still some vowels left... ->", vowels)
-
-#def test_numbers():
-#    input = shorten()
-#    for number in input:
-#        assert number.isdigit() != True
-
-#def test_signs():
-#    input = shorten()
- #   for signs in input:
- #       assert signs.isalpha() == True
-
-
-# if __name__ == "__main__": - helps to prevent errors due to not defined global variables yet, if we call main() inside there
-if __name__ == "__main__":
-    main()
-
