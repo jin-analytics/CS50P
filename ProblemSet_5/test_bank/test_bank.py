8ba927db21f8d1bccbad58b1e561b680d8d7f56b
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-02-29 17:42:12 +0000

    Thu, Feb 29, 2024, 6:42 PM +01:00

--- ProblemSet_5/test_bank/test_bank.py
+++ ProblemSet_5/test_bank/test_bank.py
@@ -1,12 +1,14 @@
 from test_bank import value
 
-def main():
-    greeting = str(input("Greeting: "))
-    test_hello(greeting)
+#def main():
+#    greeting = str(input("Greeting: "))
+#    test_hello(greeting)
 
-def test_hello():
-    assert value() == 100
+#def test_hello():
+#    assert value() == 100
+#______________________________________
 
+def test_gives_100_for_hello():
 
 if __name__ == "__main__":
     main()
