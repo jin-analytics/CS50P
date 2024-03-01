689d96c573b9c635f11a6c921f6a0e1dab0df104
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-01 17:30:46 +0000

    Fri, Mar 1, 2024, 6:30 PM +01:00

--- ProblemSet_5/test_plates/test_plates.py
+++ ProblemSet_5/test_plates/test_plates.py
@@ -1,5 +1,6 @@
 from plates import is_valid
+from plates import length
 
 def test_if_the_length_is_between_2_and_6():
     plate = ("CS50")
-    assert len(is_valid(plate)) == 4
+    assert length(plate) == None
