124ef47b61ef32ad0535dd4ad37847a8c5f0672e
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-01 18:00:16 +0000

    Fri, Mar 1, 2024, 7:00 PM +01:00

--- ProblemSet_5/test_plates/test_plates.py
+++ ProblemSet_5/test_plates/test_plates.py
@@ -1,6 +1,8 @@
 from plates import is_valid
 from plates import length
 from plates import sign_detection
+from plates import letterblock_atleast_two_char
+from plates import splitter
 import pytest
 
 @pytest.fixture
@@ -15,4 +17,4 @@ def test_if_the_length_is_between_2_and_6(plate):
 def test_if_signs_or_spaces_get_detected(plate):
     assert sign_detection(plate) == None
 
-#letterblock_atleast_two_char
+    assert letterblock_atleast_two_char(splitter(plate)) == True
