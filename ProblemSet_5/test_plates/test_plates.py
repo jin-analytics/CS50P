26d000761ca1546ef149f3b8ca8b36b25c99c683
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-01 18:16:30 +0000

    Fri, Mar 1, 2024, 7:16 PM +01:00

--- ProblemSet_5/test_plates/test_plates.py
+++ ProblemSet_5/test_plates/test_plates.py
@@ -19,14 +19,14 @@ def test_if_the_length_is_between_2_and_6():
 def test_if_signs_or_spaces_get_detected():
     assert sign_detection() == None
 
-def test_if_letterblock_has_atleast_two_chars():
-    plate_split = splitter()
-    assert letterblock_atleast_two_char(plate_split) == True
+#def test_if_letterblock_has_atleast_two_chars():
+    #plate_split = splitter()
+    #assert letterblock_atleast_two_char(plate_split) == True
 
-def test_if_numberblock_has_only_numbers():
-    plate_split = splitter()
-    assert numberblock_only_number(plate_split) == True
+#def test_if_numberblock_has_only_numbers():
+ #   plate_split = splitter()
+ #   assert numberblock_only_number(plate_split) == True
 
-def test_if_the_first_number_is_zero():
-    plate_split = splitter()
-    assert first_number_zero(plate_split) == True
+#def test_if_the_first_number_is_zero():
+ #   plate_split = splitter()
+  #  assert first_number_zero(plate_split) == True
