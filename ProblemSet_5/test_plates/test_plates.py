102b84fa657fe9f9c3d0caf6bc82577749263504
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-01 17:26:18 +0000

    Fri, Mar 1, 2024, 6:26 PM +01:00

--- ProblemSet_5/test_plates/test_plates.py
+++ ProblemSet_5/test_plates/test_plates.py
@@ -1,6 +1,4 @@
 from plates import is_valid
 
-def test_input_is_not_str():
-    s = ("1")
-    assert is_valid(s) == False
-#def test_length_between_2_and_6():
+def test_if_the_length_is_between_2_and_6():
+    
