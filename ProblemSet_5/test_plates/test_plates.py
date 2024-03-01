a6ac8b2c7bdafb97c6283c4f687747cf78080d5c
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-01 17:39:54 +0000

    Fri, Mar 1, 2024, 6:39 PM +01:00

--- ProblemSet_5/test_plates/test_plates.py
+++ ProblemSet_5/test_plates/test_plates.py
@@ -2,7 +2,9 @@ from plates import is_valid
 from plates import length
 
 @pytest.fixture
-def 
+def plate():
+    plate("CS50")
+
 
 def test_if_the_length_is_between_2_and_6():
     plate = (input)
