a0980d17ffec2dd70dcb85c429f3012469c4918c
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-02 11:04:43 +0000

    Sat, Mar 2, 2024, 12:04 PM +01:00

--- ProblemSet_5/test_twttr/test_twttr.py
+++ ProblemSet_5/test_twttr/test_twttr.py
@@ -2,9 +2,11 @@ from twttr import shorten
 
 def main():
 
-def
+def test_if_lower_vowels_gets_replaced():
+    a
 
-
+def test_if_lower_vowels_gets_replaced():
+    ...
 
 
 if __name__ == "__main__":
