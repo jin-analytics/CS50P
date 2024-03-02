d5df31159aa6c6059bb9ce41ddecf1566c8c051e
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-02 11:26:41 +0000

    Sat, Mar 2, 2024, 12:26 PM +01:00

---
+++ ProblemSet_5/test_fuel/test_fuel.py
@@ -0,0 +1,6 @@
+# input "x/y" is an string
+# x is integer and y is integer
+# returned fraction is precentage and rounded to the nearest int between 0 and 100
+# if x or y is not int then raise ValueError
+# if x is greater than y then raise an ValueError
+# if y is 0 than raise an ZeroDivisionError
