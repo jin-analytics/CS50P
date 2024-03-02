3a6950f7acd96917440b1b3d2577662de7e0f1cb
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-02 09:54:58 +0000

    Sat, Mar 2, 2024, 10:54 AM +01:00

--- ProblemSet_5/test_plates/test_plates.py
+++ ProblemSet_5/test_plates/test_plates.py
@@ -10,30 +10,35 @@ import pytest
 
 @pytest.fixture
 def plate():
-    return ("OUTATIME")
+    return ("CS50")
     #return ("")
 
 
 
 def test_length(plate):
+    is_valid(plate)
     assert length(plate) == True
 
 def test_sign_detection(plate):
+    is_valid(plate)
     assert sign_detection(plate) == None
 
 def test_if_letterblock_has_atleast_two_chars(plate):
+    is_valid(plate)
     if splitter(plate) == None:
         pass
     else:
         assert letterblock_atleast_two_char(splitter(plate)) == True
 
 def test_if_numberblock_has_only_numbers(plate):
+    is_valid(plate)
     if splitter(plate) == None:
         pass
     else:
         assert numberblock_only_number(splitter(plate)) == True
 
 def test_if_the_first_number_is_zero(plate):
+    is_valid(plate)
     if splitter(plate) == None:
         pass
     else:
