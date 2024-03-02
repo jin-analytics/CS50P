642f7092cddacf8ce712a7f94f8120062d954c0d
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-02 11:12:45 +0000

    Sat, Mar 2, 2024, 12:12 PM +01:00

--- ProblemSet_5/test_twttr/test_twttr.py
+++ ProblemSet_5/test_twttr/test_twttr.py
@@ -9,5 +9,12 @@ def test_if_lower_vowels_gets_replaced():
 def test_if_lower_vowels_gets_replaced():
     assert shorten("TWITTERAEIOU") == "TWTTR"
 
+def test_if_numbers_dont_get_changed():
+    assert shorten("1234567890") == "1234567890"
+
+def test_if_spaces_dont_get_changed():
+    assert shorten("twitter   ") == "twttr   "
+
+
 if __name__ == "__main__":
     main()
