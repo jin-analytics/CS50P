b0c7761907ad6c453297482aa3dea9687334a6e0
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-02 10:39:59 +0000

    Sat, Mar 2, 2024, 11:39 AM +01:00

--- ProblemSet_5/test_plates/test_plates.py
+++ ProblemSet_5/test_plates/test_plates.py
@@ -3,11 +3,22 @@ from plates import is_valid
 def main():
     test_letter_size()
 
+# test if length smaller 2 or bigger 6 returns false
 def test_letter_size():
     assert is_valid("CS") == True
     assert is_valid("CSCSCS") == True
     assert is_valid("C") == False
     assert is_valid("CSCSCSC") == False
+
+# test if letter block is atleast 2 chars
+
+# test if number block has no zero at beginning
+
+# test if number block has no letter in it
+
+
+
+
 
 
 if __name__ == "__main__":
