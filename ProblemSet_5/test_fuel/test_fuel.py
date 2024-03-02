b581a7902af1b0f00ebb8ce1b6d966dc2ab32b66
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-02 16:25:39 +0000

    Sat, Mar 2, 2024, 5:25 PM +01:00

--- ProblemSet_5/test_fuel/test_fuel.py
+++ ProblemSet_5/test_fuel/test_fuel.py
@@ -22,23 +22,17 @@ def main():
     test_if_F_comes()
     test_if_percentage_correct()
 
-def test_x_and_y_is_a_int():
+#def test_x_and_y_is_a_int():
 #    f1 = ("1/2").split("/")
 #    f2 = ("a/2").split("/")
 #    f3 = ("1/b").split("/")
-    assert convert(['a','b']) == False
+#    assert convert(['a','b']) == False
 #    assert convert(f2) == False
 #    assert convert(f3) == False
 
-#def test_returned_fraction_is_percentage():
-  #  assert convert("1/2") == 50
- #   assert convert("3/2") == False
-   # assert convert("1/b") == False
-##
-#def test_x_and_y_is_a_str():
- #   assert convert("1/2") == 50
- #   assert convert("a/2") == False
- #   assert convert("1/b") == False
+def test_if_0_raise_error():
+    with pytest.raises(ZeroDivisionError):
+        convert("1/0")
 
 def test_if_E_comes():
     assert gauge(1) == "E"
