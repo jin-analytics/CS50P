d7f5ddfcbde375b68a8670891c3959c9d5d2c7dd
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-02 10:48:09 +0000

    Sat, Mar 2, 2024, 11:48 AM +01:00

--- ProblemSet_5/test_plates/test_plates.py
+++ ProblemSet_5/test_plates/test_plates.py
@@ -2,6 +2,10 @@ from plates import is_valid
 
 def main():
     test_letter_size()
+    test_if_there_are_2_letter()
+    test_if_there_is_no_zero_at_beginning_of_numberblock
+    test_if_numberblock_has_no_letter_in_it()
+    test_if_there_is_no_sign_or_space()
 
 # test if length smaller 2 or bigger 6 returns false
 def test_letter_size():
@@ -11,12 +15,24 @@ def test_letter_size():
     assert is_valid("CSCSCSC") == False
 
 # test if letter block is atleast 2 chars
+def test_if_there_are_2_letter():
+    assert is_valid("CS50") == True
+    assert is_valid("C50") == False
 
 # test if number block has no zero at beginning
+def test_if_there_is_no_zero_at_beginning_of_numberblock():
+    assert is_valid("CS50"): True
+    assert is_valid("CS05"): False
 
 # test if number block has no letter in it
+def test_if_numberblock_has_no_letter_in_it():
+    assert is_valid("CS50") == True
+    assert is_valid("CS50C") == False
 
-
+# test if no spaces or signs get detected
+def test_if_there_is_no_sign_or_space():
+    assert is_valid("CS#50") == False
+    assert is_valid("CS 50") == False
 
 
 
