61db87546e7045a712d1de3d6d732dc4321e5c79
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-02 11:37:32 +0000

    Sat, Mar 2, 2024, 12:37 PM +01:00

--- ProblemSet_5/test_fuel/fuel.py
+++ ProblemSet_5/test_fuel/fuel.py
@@ -1,9 +1,15 @@
 def main():
     while True:
-        percentage = convert(input("Fraction: "))
-        gauge(percentage)
+        try:
+            fraction = input("Fraction: ")
+            fraction = fraction.split("/")
+            if len(fraction) == 2:
+                percentage = convert(fraction)
+                gauge(percentage)
+        except ValueError:
+            return False
 
-def convert(fraction):
+def convert(f):
     # Checks if x and y is a number, then divides it to get the percentage of the fraction
     try:
         if f[0].isdigit() and f[1].isdigit() == True:
@@ -14,7 +20,27 @@ def convert(fraction):
 
 
 def gauge(percentage):
-    ...
+    try:
+        # ]1%;99%[ print the actual number
+        if 1 < percentage < 99:
+            print(round(percentage), "%", sep="")
+            exit()
+        # <= 1% prints E for Empty
+        elif percentage <= 1:
+            print("E")
+            exit()
+        # <= 99% prints F for Full
+        elif 99 <= percentage <= 100 :
+            print("F")
+            exit()
+        # if x = y, for example 100/100, it prints F
+        elif f[0] == f[1]:
+            print("F")
+            exit()
+        else:
+            return False
+    except UnboundLocalError:
+        return False
 
 
 if __name__ == "__main__":
