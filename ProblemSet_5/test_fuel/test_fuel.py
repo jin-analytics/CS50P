2c08ef09b2f43f7f3bedfc78487723196b297a11
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-02 11:31:02 +0000

    Sat, Mar 2, 2024, 12:31 PM +01:00

--- ProblemSet_5/test_fuel/test_fuel.py
+++ ProblemSet_5/test_fuel/test_fuel.py
@@ -1,6 +1,15 @@
+#_________convert
 # input "x/y" is an string
 # x is integer and y is integer
 # returned fraction is precentage and rounded to the nearest int between 0 and 100
 # if x or y is not int then raise ValueError
 # if x is greater than y then raise an ValueError
 # if y is 0 than raise an ZeroDivisionError
+
+#_________gauge
+# expects an int and returns a str
+# "E" if int is less/equal 1
+# "F" if in is greater/equal 99
+# "z%" otherwise, wherein z is the same int
+
+
