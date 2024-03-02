892d49c0fd8d16f174eee222b6e95664a22e2ab6
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-02 10:35:37 +0000

    Sat, Mar 2, 2024, 11:35 AM +01:00

--- ProblemSet_5/test_plates/test_plates.py
+++ ProblemSet_5/test_plates/test_plates.py
@@ -5,6 +5,9 @@ def main():
 
 def test_letter_size():
     assert is_valid("CS") == True
+    assert is_valid("CSCSCS") == True
+    assert is_valid("C") == False
+    assert is_valid("CSCSCSC") == False
 
 
 if __name__ == "__main__":
