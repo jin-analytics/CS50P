c91d27c6985a54c9d9a6ecd12c179220a1da21b3
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-03 15:40:29 +0000

    Sun, Mar 3, 2024, 4:40 PM +01:00

--- ProblemSet_6/lines/lines.py
+++ ProblemSet_6/lines/lines.py
@@ -1,5 +1,8 @@
 # Purpose:
-#
+# In a file called lines.py, implement a program that expects exactly one command-line argument,
+# ...the name (or path) of a Python file, and outputs the number of lines of code in that file,
+# ...excluding comments and blank lines.
+
 # Condition:
 # If the user does not specify exactly one command-line argument,
 # ...or if the specified file’s name does not end in .py,
