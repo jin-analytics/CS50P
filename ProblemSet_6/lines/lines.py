983bc13fa968160edd3616aea961f16a68e9b4bc
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-03 16:02:49 +0000

    Sun, Mar 3, 2024, 5:02 PM +01:00

--- ProblemSet_6/lines/lines.py
+++ ProblemSet_6/lines/lines.py
@@ -23,6 +23,8 @@ import sys
 filename = sys.argv[1:]
 #print(filename[1])
 #open(filename, mode='r', buffering=-1, encoding=None, errors=None, newline=None, closefd=True, opener=None)
-f = open(f'{filename}', 'w', encoding="utf-8")
-with open(f'{filename}', encoding="utf-8") as f:
+f = open(f'{filename[2]}', 'w', encoding="utf-8")
+with open(f'{filename[2]}', encoding="utf-8") as f:
     read_data = f.read()
+    print(f.readline())
+##f.readline()
