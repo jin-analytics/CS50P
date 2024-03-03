e83bd1530d8779eaa4649bec7858ce5a47b7572a
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-03 15:37:26 +0000

    Sun, Mar 3, 2024, 4:37 PM +01:00

---
+++ ProblemSet_6/lines/lines.py
@@ -0,0 +1,6 @@
+# Purpose:
+#
+# 
+# If the user does not specify exactly one command-line argument, or if the specified file’s name does not end in .py, or if the specified file does not exist, the program should instead exit via sys.exit.
+
+
