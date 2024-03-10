c3c6f7089217f8ca73800c5f3c3d44fc51e6cb4c
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-10 17:57:01 +0000

    Sun, Mar 10, 2024, 6:57 PM +01:00

--- ProblemSet_6/demo.py
+++ ProblemSet_6/demo.py
@@ -1,16 +1,15 @@
-con = []
+counter = []
 
 with open('test.txt','r') as f:
     content = f.readlines()
 
     for line in content:
-        #con = con.append(lines)
-        #print('...',line)
-        #print(line[0])
         pass
+
         if line == '\n':
-            #print('check')
-            pass
+            counter.append(1)
+
         elif line[0] == '#':
-            pass
-            print('check2')
+            counter.append(1)
+
+    print(len(content) - len(counter))
