1be88704b5fc95c0461418cd533b283653290ec4
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-10 17:48:50 +0000

    Sun, Mar 10, 2024, 6:48 PM +01:00

--- ProblemSet_6/demo.py
+++ ProblemSet_6/demo.py
@@ -6,5 +6,7 @@ with open('test.txt','r') as f:
     for line in content:
         #con = con.append(lines)
         print('...',line)
-        if line == ''
-        print('...',content[line])
+        if line == '\n':
+            print('check')
+        elif line[1:] == '#':
+            print('check2')
