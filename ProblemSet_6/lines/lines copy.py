0fb5a8694ce926ef6a4f8a620ee1019afe5c4c28
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-10 18:30:57 +0000

    Sun, Mar 10, 2024, 7:30 PM +01:00

---
+++ ProblemSet_6/lines/lines copy.py
@@ -0,0 +1,43 @@
+# Purpose:
+# In a file called lines.py, implement a program that expects exactly one command-line argument,
+# ...the name (or path) of a Python file, and outputs the number of lines of code in that file,
+# ...excluding comments and blank lines.
+
+# Condition:
+# If the user does not specify exactly one command-line argument,
+# ...or if the specified file’s name does not end in .py,
+# ...or if the specified file does not exist,
+# ...the program should instead exit via sys.exit.
+
+
+
+
+### Too few command-line arguments
+### Too many command-line arguments
+### Not a Python file
+### File does not exist
+
+import sys
+
+counter = [] #empty list which raises when in the readen file is a command or empty line
+while True:
+    try:
+        filename = sys.argv[1]
+
+        with open(filename, 'r') as f:
+            content = f.readlines()
+            for line in content:
+                line = line.strip(' ')
+                if line == '\n':
+                    counter.append(1)
+                elif line[0] == '#':
+                    counter.append(1)
+
+            print(len(content) - len(counter))
+            sys.exit()
+    except EOFError:
+        sys.exit()
+    except FileNotFoundError:
+        sys.exit('Not found')
+    #except EOFError:
+    #sys.exit()
