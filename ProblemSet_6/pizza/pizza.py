69a68f812c6478d93dfec0f77b5dd3698a18a074
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-12 16:52:42 +0000

    Tue, Mar 12, 2024, 5:52 PM +01:00

--- ProblemSet_6/pizza/pizza.py
+++ ProblemSet_6/pizza/pizza.py
@@ -1,23 +1,8 @@
 from tabulate import tabulate
-#import os
 import sys
+import csv
 
-##finds the path of the input from the commandline and returns it
-#def find_files(filename, search_path):
-#   result = []
-#  for root, dirs, files in os.walk(search_path):
-#      if filename in files:
-#         result.append(os.path.join(root, filename))
-#   return result
-
-#rootdir = os.getcwd()
-#folder = find_files(sys.argv[1],rootdir)
-#folder = folder[0]
-
-with open(sys.argv[1]) as file:
-    for entrees in file:
-        lines = file.readlines()
-        print(lines[0])
-       #print(tabulate(p))
-       #print(tabulate(p, headers=["1","2", "3"]))
-
+with open(sys.argv[1]) as csvfile:
+    for entrees in csvfile:
+        lines = csv.reader(csvfile)
+        print(lines)
