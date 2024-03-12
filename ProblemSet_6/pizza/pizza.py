d0a372a963d00da2e0b019750fee7850f7bf72c3
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-12 19:10:12 +0000

    Tue, Mar 12, 2024, 8:10 PM +01:00

--- ProblemSet_6/pizza/pizza.py
+++ ProblemSet_6/pizza/pizza.py
@@ -2,13 +2,30 @@ from tabulate import tabulate
 import sys
 import csv
 
-order = []
-table = []
 
-with open(sys.argv[1]) as file:
-    reader = csv.reader(file)
 
-    for row in reader:
-        order.append(row)
+def main():
+    order = []
+    try:
+        if len(sys.argv) < 2:
+            sys.exit("Too few command-line arguments")
+        elif len(sys.argv) > 2:
+            sys.exit("Too many command-line arguments")
+        elif sys.argv[1][-4:] != ".csv":
+            sys.exit("Not a CSV file")
 
-    print(tabulate(order, headers="firstrow", tablefmt="grid"))
+        with open(sys.argv[1]) as file:
+            reader = csv.reader(file)
+
+            for row in reader:
+                order.append(row)
+
+            print(tabulate(order, headers="firstrow", tablefmt="grid"))
+
+    except FileNotFoundError:
+        sys.exit('File does not exist')
+    except IndexError:
+        sys.exit('File does not exist')
+
+if __name__ == "__main__":
+    main()
