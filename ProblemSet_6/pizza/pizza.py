fd1c67ef612165f00bf24a6995375dea73661687
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-12 19:06:48 +0000

    Tue, Mar 12, 2024, 8:06 PM +01:00

--- ProblemSet_6/pizza/pizza.py
+++ ProblemSet_6/pizza/pizza.py
@@ -9,11 +9,6 @@ with open(sys.argv[1]) as file:
     reader = csv.reader(file)
 
     for row in reader:
-        #order.append(f"{row[0]},{row[1]},{row[2]}")
         order.append(row)
-    #print([order[0]],[order[1]])
 
-    for entrees in order:
-        table.append([entrees])
-    print(table)
-    print(tabulate(table, headers="firstrow", tablefmt="grid"))
+    print(tabulate(order, headers="firstrow", tablefmt="grid"))
