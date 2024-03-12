021703ff550f0ab3cf7c6280b3f858ce8dca6cbc
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-12 10:08:31 +0000

    Tue, Mar 12, 2024, 11:08 AM +01:00

--- ProblemSet_6/lines/lines.py
+++ ProblemSet_6/lines/lines.py
@@ -21,7 +21,7 @@ import os
 #finds the path of the input from the commandline and returns it
 def find_files(filename, search_path):
    result = []
-   for root, dirs, files in os.walk(search_path, topdown=False):
+   for root, dirs, files in os.walk(search_path):
       if filename in files:
          result.append(os.path.join(root, filename))
    return result
