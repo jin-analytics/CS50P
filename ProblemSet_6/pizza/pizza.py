15683017be3cc8e6124784535cd9c45072b4570a
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-12 16:23:11 +0000

    Tue, Mar 12, 2024, 5:23 PM +01:00

--- ProblemSet_6/pizza/pizza.py
+++ ProblemSet_6/pizza/pizza.py
@@ -10,11 +10,11 @@ def find_files(filename, search_path):
          result.append(os.path.join(root, filename))
    return result
 
-# uses input from commandline and then gives first entree from list
-rootdir = os.getcwd() #gets current working directory from commandline
-print(os.getcwd())
-folder = find_files(sys.argv[1],rootdir) #searches for file from commandline and directory from commandline
-folder = folder[0] #returns the first entree of a file which was found
+#rootdir = os.getcwd()
+#folder = find_files(sys.argv[1],rootdir)
+#folder = folder[0]
 
 with open(sys.argv[1]) as p:
-    print(p.readlines())
+    for entrees in p:
+        #print(p.readlines())
+       print(entrees)
