51786d7d8a1059f3d22673d50ed7d5c5894a7a38
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-12 16:20:21 +0000

    Tue, Mar 12, 2024, 5:20 PM +01:00

--- ProblemSet_6/pizza/pizza.py
+++ ProblemSet_6/pizza/pizza.py
@@ -1,5 +1,20 @@
 from tabulate import tabulate
+import os
+import sys
 
+#finds the path of the input from the commandline and returns it
+def find_files(filename, search_path):
+   result = []
+   for root, dirs, files in os.walk(search_path):
+      if filename in files:
+         result.append(os.path.join(root, filename))
+   return result
 
-with open() as p:
-    
+# uses input from commandline and then gives first entree from list
+rootdir = os.getcwd() #gets current working directory from commandline
+print(os.getcwd())
+folder = find_files(sys.argv[1],rootdir) #searches for file from commandline and directory from commandline
+folder = folder[0] #returns the first entree of a file which was found
+
+with open(sys.argv[1]) as p:
+    print(p.readlines())
