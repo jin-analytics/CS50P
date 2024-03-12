7d50ac512502f3e0d249c9e9c5daa883109e67e3
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-12 18:28:11 +0000

    Tue, Mar 12, 2024, 7:28 PM +01:00

--- ProblemSet_6/pizza/pizza.py
+++ ProblemSet_6/pizza/pizza.py
@@ -4,7 +4,8 @@ import csv
 
 with open(sys.argv[1]) as file:
     reader = csv.reader(file)
-    for 
+    for item, small, large in reader:
+        
 
     #for line in file:
         ###splits by comma and remoceves with rstrip garbage like "/n"
