3d2622f8ed397d31355244c547d6051711254c55
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-12 09:04:28 +0000

    Tue, Mar 12, 2024, 10:04 AM +01:00

--- folder.py
+++ folder.py
@@ -8,6 +8,7 @@ os.chdir("/workspaces/155905672")
 #file_path = os.path.join(os.environ.get('HOME'), 'test.txt')
 #print(file_path)
 print(os.getcwd())
+print(os.path.join("/workspaces/155905672", sys.argv[1]))
 print(os.walk("/workspaces/155905672"))
 
 #print(os.path.isfile(sys.argv[1]))
