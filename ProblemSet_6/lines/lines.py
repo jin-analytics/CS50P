c6cbc4ffed7d3744718e42c0da5d1affe4f97f86
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-12 08:46:43 +0000

    Tue, Mar 12, 2024, 9:46 AM +01:00

--- ProblemSet_6/lines/lines.py
+++ ProblemSet_6/lines/lines.py
@@ -46,11 +46,11 @@ def main():
         os.chdir("/workspaces/155905672")
 
         for dirpath, dirnames, filenames in os.walk("/workspaces/155905672"):
-        # print("Current Path:", dirpath)
-        # print("Directories:", dirnames)
+            print("Current Path:", dirpath)
+            print("Directories:", dirnames)
             for entrees in filenames:
             #print("Files:", filenames)
-                print(entrees)
+                #print(entrees)
                 if entrees == sys.argv[1]:
                     folder = entrees
 
