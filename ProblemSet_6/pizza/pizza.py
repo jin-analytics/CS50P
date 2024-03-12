2f5872ac430c3a0492d313e0bde58856d9a7a400
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-12 16:54:03 +0000

    Tue, Mar 12, 2024, 5:54 PM +01:00

--- ProblemSet_6/pizza/pizza.py
+++ ProblemSet_6/pizza/pizza.py
@@ -2,7 +2,7 @@ from tabulate import tabulate
 import sys
 import csv
 
-with open(sys.argv[1]) as csvfile:
+with open(sys.argv[1], newline='') as csvfile:
     for entrees in csvfile:
         lines = csv.reader(csvfile)
         print(lines)
