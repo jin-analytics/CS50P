4a4472eeaca78c388ec5025b053cc496b6a5e95d
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-12 08:41:41 +0000

    Tue, Mar 12, 2024, 9:41 AM +01:00

--- ProblemSet_6/lines/lines.py
+++ ProblemSet_6/lines/lines.py
@@ -43,6 +43,15 @@ def main():
         #folder = find_files(sys.argv[1],"/")
         #folder = folder[0]
 
+        os.chdir("/workspaces/155905672")
+
+        for dirpath, dirnames, filenames in os.walk("/workspaces/155905672"):
+        # print("Current Path:", dirpath)
+        # print("Directories:", dirnames)
+            for entrees in filenames:
+            #print("Files:", filenames)
+                print(entrees)
+
 
 
         with open(folder, 'r') as f:
--- folder.py
+++ folder.py
@@ -6,5 +6,6 @@ for dirpath, dirnames, filenames in os.walk("/workspaces/155905672"):
    # print("Current Path:", dirpath)
    # print("Directories:", dirnames)
     for entrees in filenames:
-        print("Files:", filenames)
+        #print("Files:", filenames)
         print(entrees)
+        print()
