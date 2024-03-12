5b0206405c8bf61d4dca8de97c6fea46dc0a5b44
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-12 08:50:14 +0000

    Tue, Mar 12, 2024, 9:50 AM +01:00

--- folder.py
+++ folder.py
@@ -2,10 +2,4 @@ import os
 
 os.chdir("/workspaces/155905672")
 
-for dirpath, dirnames, filenames in os.walk("/workspaces/155905672"):
-   # print("Current Path:", dirpath)
-   # print("Directories:", dirnames)
-    for entrees in filenames:
-        #print("Files:", filenames)
-        print(entrees)
-        print()
+print(os.environ.get('HOME'))
