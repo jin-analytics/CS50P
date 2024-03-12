d04e36802264b243d1de37a8215b934349319f0c
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-12 18:37:44 +0000

    Tue, Mar 12, 2024, 7:37 PM +01:00

--- ProblemSet_6/pizza/pizza.py
+++ ProblemSet_6/pizza/pizza.py
@@ -9,7 +9,8 @@ with open(sys.argv[1]) as file:
     for row in reader:
         #print(row[0])
         order.append(f"{row[0]},{row[1]},{row[2]}")
-    print(order[1])
+        print([order[0]],[order[1]],[order[2]],[order[3]],[order[4]])
+        print(tabulate(order))
 
     #for line in file:
         ###splits by comma and remoceves with rstrip garbage like "/n"
