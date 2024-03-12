ee3c68eeabeb30e09f3002538c4bd88efed83a7e
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-12 17:01:33 +0000

    Tue, Mar 12, 2024, 6:01 PM +01:00

--- ProblemSet_6/pizza/pizza.py
+++ ProblemSet_6/pizza/pizza.py
@@ -2,9 +2,6 @@ from tabulate import tabulate
 import sys
 import csv
 
-with open(sys.argv[1], newline='') as csvfile:
-    for entrees in csvfile:
-        #spamreader = csv.reader(csvfile)
-        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    for row in spamreader:
-        print(', '.join(row))
+with open(sys.argv[1]) as file:
+    for line in file:
+        print(line.rstrip().split(','))
