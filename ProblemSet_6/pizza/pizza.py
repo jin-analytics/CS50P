29873ebbad43acdb900be2e33927d94e41b74cf7
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-12 18:21:41 +0000

    Tue, Mar 12, 2024, 7:21 PM +01:00

--- ProblemSet_6/pizza/pizza.py
+++ ProblemSet_6/pizza/pizza.py
@@ -4,7 +4,9 @@ import csv
 
 with open(sys.argv[1]) as file:
     for line in file:
-        #splits by comma and remoceves with rstrip garbage like "/n"
-        item, s, l = line.rstrip().split(',')
-        print(item, s, l)
+        ###splits by comma and remoceves with rstrip garbage like "/n"
+        #item, s, l = line.rstrip().split(',')
+        #print(item, s, l)
         #print(tabulate(item, small_price, large_price))
+
+        
