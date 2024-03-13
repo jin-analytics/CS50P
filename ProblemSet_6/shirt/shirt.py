a2d7c570934f2f377ba833cacecf01f12ae4c689
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-13 17:56:30 +0000

    Wed, Mar 13, 2024, 6:56 PM +01:00

--- ProblemSet_6/shirt/shirt.py
+++ ProblemSet_6/shirt/shirt.py
@@ -14,17 +14,21 @@ import os
 #    ImageOps.fit(image, size, bleed=0.0, centering=(0.5, 0.5))
 #    Image.paste(sys.argv[2], box=None, mask=None)
 
-#for f in os.listdir('.'):
-#    if f.endswith('.jpg'):
-#       print(f)
+for f in os.listdir('.'):
+    if f.endswith('.png'):
+        image2 = Image.open(f)
+        filename, filetype = os.path.splitext(f)
+
+        image2.save(''.format(filename, filetype))
+
 
 size = (300,300)
 
 image1 = Image.open(sys.argv[1])
 #image1.save('test.png') #saves image
 
-image2 = Image.open(sys.argv[2])
-image2.save('shirt.jpg') #saves image
+#image2 = Image.open(sys.argv[2])
+#image2.save('shirt.jpg'.format('.jpg')) #saves image
 
 image1_crop = ImageOps.fit(image1, size, bleed=0.0, centering=(0.5, 0.5))
 #image2_crop = ImageOps.fit(image2, size, bleed=0.0, centering=(0.5, 0.5))
