bd558fa812b981b7ea909b04d4bce1fd1debcef1
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-13 11:08:19 +0000

    Wed, Mar 13, 2024, 12:08 PM +01:00

--- ProblemSet_6/scourgify/students.py
+++ ProblemSet_6/scourgify/students.py
@@ -4,10 +4,8 @@ with open("before.csv") as csvfile:
     next(csvfile)
 ### Option 1 - Read out every single line, split it by comma and append to an empty list
     for line in csvfile:
-        #name, house = line.rstrip().split(",")
         line = line.replace('"', '')
-#        line = line.rstrip().split(",")
-#        students.append(line)
-#    print(students[0])
-        line = line.rstrip().split(",")
-        students.append(line)
+        lastname, firstname, house = line.rstrip().split(",")
+        student = {"firstname":firstname, "lastname":lastname, "house":house}
+        students.append(student)
+    print(students["firstname"])
