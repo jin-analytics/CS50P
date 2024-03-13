9d6a11b967491782c1ad6e484c0ce5a82b642f60
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-13 15:44:12 +0000

    Wed, Mar 13, 2024, 4:44 PM +01:00

--- ProblemSet_6/shirt/shirt.py
+++ ProblemSet_6/shirt/shirt.py
@@ -7,7 +7,7 @@ import sys
 #using default values for method, bleed, and centering, overlay the shirt with Image.paste
 #and save the result with Image.save
 
-image = Image.open(sys.argv[1], mode='r', formats=None)
-size = 128, 128
-ImageOps.fit(image, size, bleed=0.0, centering=(0.5, 0.5))
-Image.paste(sys.argv[2], box=None, mask=None)
+with open(Image.open(sys.argv[1], mode='r', formats=None)) as image:
+    size = 128, 128
+    ImageOps.fit(image, size, bleed=0.0, centering=(0.5, 0.5))
+    Image.paste(sys.argv[2], box=None, mask=None)
