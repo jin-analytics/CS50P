4fa0f8a7248f6ed9584e52c2b6937e5687c34514
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-13 19:33:50 +0000

    Wed, Mar 13, 2024, 8:33 PM +01:00

--- ProblemSet_6/shirt/shirt.py
+++ ProblemSet_6/shirt/shirt.py
@@ -23,7 +23,6 @@ image1 = Image.open(sys.argv[1])
 image2 = Image.open(sys.argv[2])
 
 size = (300,300) # crop size tupel
-size2 = (200,200) # crop size tupel
 
 for f in os.listdir('.'):
     # checks if image from cmd line is in the current path, then crops it and then saves under new name with _cropped in it
@@ -34,14 +33,31 @@ for f in os.listdir('.'):
         img1.save('{}_cropped{}'.format(filename1,filetype1)) #saves as 'before1_cropped.png'
 
     # checks if image from cmd line is in the current path, then crops it and then saves under new name with _cropped in it
-    elif f.endswith(filetype1) and f == sys.argv[2]:
+    elif f.endswith('.png') and f == sys.argv[2]:
         img2 = Image.open(f)
         img2 = ImageOps.fit(image2, size, bleed=0.0, centering=(0.5, 0.5))
         filename2, filetype2 = os.path.splitext(f) #filename zB 'before1' & filetype zB '.jpg'
         img2.save('{}_cropped{}'.format(filename2,filetype2)) #saves as 'before1_cropped.jpg'
 
-background = Image.open('{}_cropped{}'.format(filename1,filetype1))
-foreground = Image.open('{}_cropped{}'.format(filename2,filetype2))
+background = Image.open('{}_cropped{}'.format(filename1,filetype1)) #jpg
+foreground = Image.open('{}_cropped{}'.format(filename2,filetype2)) #png
+
 # lays foreground picture over background picture
 background.paste(foreground, (0, 0), foreground.convert('RGBA')) #paste(foreground picture, (x,y), applied mask (here 'RGBA' cause of PNG) )
 background.save('{}_cropped_overlayed{}'.format(filename2,filetype2)) #saves as 'before1_cropped_overlayed.jpg'
+
+
+
+
+
+
+###________________________________________________________________________________________________________________________________________________
+# call like this: image_fusion(background_filename, background_filetype, foreground_filename, foreground_filetype)
+def image_fusion(filename1,filetype1,filename2,filetype2):
+    background = Image.open('{}_cropped{}'.format(filename1,filetype1)) #jpg
+    foreground = Image.open('{}_cropped{}'.format(filename2,filetype2)) #png
+
+    # lays foreground picture over background picture
+    background.paste(foreground, (0, 0), foreground.convert('RGBA')) #paste(foreground picture, (x,y), applied mask (here 'RGBA' cause of PNG) )
+    background.save('{}_cropped_overlayed{}'.format(filename2,filetype2)) #saves as 'before1_cropped_overlayed.jpg'
+    
