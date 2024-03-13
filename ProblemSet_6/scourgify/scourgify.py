068324aa931e1b121de847eeac599fd5a14767ea
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-13 13:58:56 +0000

    Wed, Mar 13, 2024, 2:58 PM +01:00

--- ProblemSet_6/scourgify/scourgify.py
+++ ProblemSet_6/scourgify/scourgify.py
@@ -6,30 +6,35 @@ import sys
 import csv
 
 def main():
+    try:
+        if len(sys.argv) < 2:
+            sys.exit("Too few command-line arguments")
+        elif len(sys.argv) > 2:
+            sys.exit("Too many command-line arguments")
+        elif sys.argv[1][-4:] != ".csv":
+            sys.exit("Not a CSV file")
 
-    if len(sys.argv) < 2:
-        sys.exit("Too few command-line arguments")
-    elif len(sys.argv) > 2:
-        sys.exit("Too many command-line arguments")
-    elif sys.argv[1][-4:] != ".csv":
-        sys.exit("Not a CSV file")
+        students = []
+        # reads the before.csv file and creates a list "students" with this structure: [firstname, lastname, house]
+        with open("before.csv") as csvfile:
+            next(csvfile)
+            for line in csvfile:
+                line = line.replace('"', '')
+                lastname, firstname, house = line.rstrip().split(",")
+                firstname=firstname.replace(' ', '')
+                students.append([firstname, lastname, house])
 
-    students = []
-    # reads the before.csv file and creates a list "students" with this structure: [firstname, lastname, house]
-    with open("before.csv") as csvfile:
-        next(csvfile)
-        for line in csvfile:
-            line = line.replace('"', '')
-            lastname, firstname, house = line.rstrip().split(",")
-            firstname=firstname.replace(' ', '')
-            students.append([firstname, lastname, house])
-
-    # writes a new file "after.csv" with the structure from the list "studens" + header "firstname, lastname, house"
-    with open("after.csv", "w") as csvfile:
-        writer =  csv.writer(csvfile)
-        writer.writerow(['first name', 'last name', 'house']) #creates the header in the csv.file
-        for firstname, lastname, house in students:
-            writer.writerow([firstname, lastname, house])
+        # writes a new file "after.csv" with the structure from the list "studens" + header "firstname, lastname, house"
+        with open("after.csv", "w") as csvfile:
+            writer =  csv.writer(csvfile)
+            writer.writerow(['first name', 'last name', 'house']) #creates the header in the csv.file
+            for firstname, lastname, house in students:
+                writer.writerow([firstname, lastname, house])
+    
+    except FileNotFoundError:
+        sys.exit('File does not exist')
+    except IndexError:
+        sys.exit('File does not exist')
 
 if __name__ == "__main__":
     main()
