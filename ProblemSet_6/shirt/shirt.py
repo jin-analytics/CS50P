d37f3f98ca1673b8d550dd4ed2545868600d8629
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-13 17:47:59 +0000

    Wed, Mar 13, 2024, 6:47 PM +01:00

--- ProblemSet_6/shirt/shirt.py
+++ ProblemSet_6/shirt/shirt.py
@@ -23,11 +23,11 @@ size = (300,300)
 image1 = Image.open(sys.argv[1])
 #image1.save('test.png') #saves image
 
-image2 = Image.open(sys.argv[1])
+image2 = Image.open(sys.argv[2])
 image2.save('shirt.jpg') #saves image
 
 image1_crop = ImageOps.fit(image1, size, bleed=0.0, centering=(0.5, 0.5))
 image2_crop = ImageOps.fit(image2, size, bleed=0.0, centering=(0.5, 0.5))
 
-image1_crop.save('image1_crop.jpg') #saves image
-image2_crop.save('image2_crop.jpg') #saves image
+image1_crop.save('image1_crop.jpg') #saves cropped image
+image2_crop.save('image2_crop.jpg') #saves cropped image
