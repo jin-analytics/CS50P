43ce6d8050f3c77282fc28959db0231035834fac
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-13 14:19:27 +0000

    Wed, Mar 13, 2024, 3:19 PM +01:00

--- ProblemSet_6/scourgify/DictFromCSV.py
+++ ProblemSet_6/scourgify/DictFromCSV.py
@@ -3,7 +3,7 @@ students = []
 
 with open("before.csv") as csvfile:
     next(csvfile)
-### Option 1 - Read out every single line, split it by comma and append to an empty list
+###  Option 1 - Read out every single line, split it by comma and append to an empty list
     for line in csvfile:
         line = line.replace('"', '')
         lastname, firstname, house = line.rstrip().split(",")
