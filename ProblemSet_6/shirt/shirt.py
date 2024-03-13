f8b1fb0f4ee634f1e7233e61fc8130cdb432a720
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-13 17:45:03 +0000

    Wed, Mar 13, 2024, 6:45 PM +01:00

--- ProblemSet_6/shirt/shirt.py
+++ ProblemSet_6/shirt/shirt.py
@@ -26,5 +26,5 @@ image1 = Image.open(sys.argv[1])
 image2 = Image.open(sys.argv[1])
 image2.save('shirt.jpg') #saves image
 
-ImageOps.fit(image1, size, bleed=0.0, centering=(0.5, 0.5))
+image1ImageOps.fit(image1, size, bleed=0.0, centering=(0.5, 0.5))
 ImageOps.fit(image2, size, bleed=0.0, centering=(0.5, 0.5))
