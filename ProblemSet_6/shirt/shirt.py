74b92622cf23222f61b4e8dd1a6048705a0948d0
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-13 20:33:44 +0000

    Wed, Mar 13, 2024, 9:33 PM +01:00

--- ProblemSet_6/shirt/shirt.py
+++ ProblemSet_6/shirt/shirt.py
@@ -41,7 +41,7 @@ def main():
 #            img2.save('{}_cropped{}'.format(filename2,filetype2)) #saves as 'before1_cropped.jpg'
 
     image1 = Image.open(sys.argv[1])
-    image1 = crop_image(300,300,image1)
+    image1 = crop_image(300,300,sys.argv[1])
     print(image1)
     #image2 = crop_image(300,300,Image.open(sys.argv[2]))
     #print(image2)
