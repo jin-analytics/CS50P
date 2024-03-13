2918b9720331f3ac2611ce98b7ae01b7831e393d
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-13 10:53:36 +0000

    Wed, Mar 13, 2024, 11:53 AM +01:00

--- ProblemSet_6/scourgify/students.py
+++ ProblemSet_6/scourgify/students.py
@@ -1,9 +1,9 @@
 students = []
 
-with open("test.csv") as csvfile:
+with open("before.csv") as csvfile:
 
 ### Option 1 - Read out every single line, split it by comma and append to an empty list
     for line in csvfile:
         #name, house = line.rstrip().split(",")
-        name, house = line.rstrip().split(",")
+        name, house, test = line.rstrip().split(",")
         print(name, house)
