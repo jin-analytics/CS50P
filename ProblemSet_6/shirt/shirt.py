8b3db0e84e93011a8f55236df0bce722dcda118c
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-13 21:05:52 +0000

    Wed, Mar 13, 2024, 10:05 PM +01:00

--- ProblemSet_6/shirt/shirt.py
+++ ProblemSet_6/shirt/shirt.py
@@ -14,13 +14,13 @@ def main():
             sys.exit("Too few command-line arguments")
         elif len(sys.argv) > 3:
             sys.exit("Too many command-line arguments")
-        elif sys.argv[1][-4:] != ".csv":
-            sys.exit("Not a CSV file")
-        elif sys.argv[2][-4:] != ".jpg" or ".png":
-            sys.exit("Invalid output")
+        elif sys.argv[1][-4:] != ".jpg":
+            sys.exit("Invalid Output")
+        elif sys.argv[2][-4:] == ".jpg" and sys.argv[3][-4:] ==".png":
+            sys.exit("Input and output have different extensions")
 
         filename1 = image_crop(300,300,sys.argv[1])
-        filename2 = image_crop(300,300,sys.argv[2])
+        #filename2 = image_crop(300,300,sys.argv[2])
         image_fusion(filename1,filename2)
 
     except FileNotFoundError:
