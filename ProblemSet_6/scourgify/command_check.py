0dc6a218320912db800d559173fe03396a0ac82d
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-13 13:56:32 +0000

    Wed, Mar 13, 2024, 2:56 PM +01:00

--- ProblemSet_6/scourgify/command_check.py
+++ ProblemSet_6/scourgify/command_check.py
@@ -1,12 +1,12 @@
+import sys
 
-command_check(command_line_input, accepted_value, filetype)
+#command_check(command_line_input, accepted_value, filetype)
+def command_check(i,a,f):
+    accepted_value = a
 
-def command_check(i,a):
-        accepted_value = a
-
-        if len(sys.argv) < accepted_value:
-            sys.exit("Too few command-line arguments")
-        elif len(sys.argv) > accepted_value:
-            sys.exit("Too many command-line arguments")
-        elif sys.argv[1][-4:] != ".csv":
-            sys.exit("Not a CSV file")
+    if len(sys.argv) < accepted_value:
+        sys.exit("Too few command-line arguments")
+    elif len(sys.argv) > accepted_value:
+        sys.exit("Too many command-line arguments")
+    elif sys.argv[1][-4:] != ".csv":
+        sys.exit("Not a CSV file")
