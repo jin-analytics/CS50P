ca33f933ecadecb1d65b9838fe5de7be6fad3452
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-13 15:22:19 +0000

    Wed, Mar 13, 2024, 4:22 PM +01:00

--- ProblemSet_6/shirt.py
+++ ProblemSet_6/shirt.py
@@ -6,5 +6,5 @@ import sys
 #using default values for method, bleed, and centering, overlay the shirt with Image.paste
 #and save the result with Image.save
 
-PIL.Image.open(sys.argv[1], mode='r', formats=None)
+Image.open(sys.argv[1], mode='r', formats=None)
 
