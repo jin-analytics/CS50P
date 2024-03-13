76a7200e7837fb9d20a022a07889537f4d177cf9
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-13 18:47:38 +0000

    Wed, Mar 13, 2024, 7:47 PM +01:00

--- ProblemSet_6/shirt/shirt.py
+++ ProblemSet_6/shirt/shirt.py
@@ -27,16 +27,18 @@ size = (300,300) # crop size tupel
 for f in os.listdir('.'):
     # checks if image from cmd line is in the current path, then crops it and then saves under new name with _cropped in it
     if f.endswith('.jpg') and f == sys.argv[1]:
-        img = Image.open(f)
-        img = ImageOps.fit(image1, size, bleed=0.0, centering=(0.5, 0.5))
-        filename, filetype = os.path.splitext(f) #filename zB 'before1' & filetype zB '.jpg'
-        img.save('{}_cropped{}'.format(filename,".png")) #saves as 'before1_cropped.png'
+        img1 = Image.open(f)
+        img1 = ImageOps.fit(image1, size, bleed=0.0, centering=(0.5, 0.5))
+        filename1, filetype1 = os.path.splitext(f) #filename zB 'before1' & filetype zB '.jpg'
+        img1.save('{}_cropped{}'.format(filename1,".png")) #saves as 'before1_cropped.png'
 
     # checks if image from cmd line is in the current path, then crops it and then saves under new name with _cropped in it
     elif f.endswith('.png') and f == sys.argv[2]:
-        img = Image.open(f)
-        img = ImageOps.fit(image2, size, bleed=0.0, centering=(0.5, 0.5))
-        filename, filetype = os.path.splitext(f) #filename zB 'before1' & filetype zB '.jpg'
-        img.save('{}_cropped{}'.format(filename,filetype)) #saves as 'before1_cropped.jpg'
+        img2 = Image.open(f)
+        img2 = ImageOps.fit(image2, size, bleed=0.0, centering=(0.5, 0.5))
+        filename2, filetype2 = os.path.splitext(f) #filename zB 'before1' & filetype zB '.jpg'
+        img2.save('{}_cropped{}'.format(filename2,filetype2)) #saves as 'before1_cropped.jpg'
 
-Image.open(image1)
+img1 = Image.open(f'{filename1}_cropped.png')
+img2 = Image.open(f'{filename2}_cropped.png')
+
