ecc1f22ab9baa837f134a1dd05f03020ab132736
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-13 15:19:13 +0000

    Wed, Mar 13, 2024, 4:19 PM +01:00

--- ProblemSet_6/shirt.py
+++ ProblemSet_6/shirt.py
@@ -1,7 +1,10 @@
 from PIL import Image
+import sys
 
 #Open the input with Image.open
 #resize and crop the input with ImageOps.fit
 #using default values for method, bleed, and centering, overlay the shirt with Image.paste
 #and save the result with Image.save
+
+PIL.Image.open(sys.argv[1], mode='r', formats=None)
 
