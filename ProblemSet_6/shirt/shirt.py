07110d5c29b2978e0cce974d822ae5273bd8e7f0
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-13 19:40:41 +0000

    Wed, Mar 13, 2024, 8:40 PM +01:00

--- ProblemSet_6/shirt/shirt.py
+++ ProblemSet_6/shirt/shirt.py
@@ -54,12 +54,15 @@ background.save('{}_cropped_overlayed{}'.format(filename2,filetype2)) #saves as 
 ###________________________________________________________________________________________________________________________________________________
 # call like this: image_fusion(background_filename, background_filetype, foreground_filename, foreground_filetype)
 
-# purpose: returns image with filename "{filename2}_cropped_overlayed{}'."
+# purpose: returns image with filename "{filename2}_{}'."
 def image_fusion(filename1,filetype1,filename2,filetype2):
+
     background = Image.open('{}_cropped{}'.format(filename1,filetype1))
     foreground = Image.open('{}_cropped{}'.format(filename2,filetype2))
+
     # lays foreground picture over background picture
     background.paste(foreground, (0, 0), foreground.convert('RGBA')) #paste(foreground picture, (x,y), applied mask (here 'RGBA' cause of PNG) )
-    background.save('{}_cropped_overlayed{}'.format(filename2,filetype2)) #saves as 'before1_cropped_overlayed.jpg'
-    image = Image.open('{}_cropped_overlayed{}'.format(filename2,filetype2))
+    background.save('{}_overlayed{}'.format(filename2,filetype2)) #saves as 'filename2_overlayed.png'
+    image = Image.open('{}_overlayed{}'.format(filename2,filetype2))
+    
     return image
