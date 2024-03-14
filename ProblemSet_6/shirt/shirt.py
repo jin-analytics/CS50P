60786d24bd96cdceb8cdb7e21710b8edc44d05c3
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-14 18:37:31 +0000

    Thu, Mar 14, 2024, 7:37 PM +01:00

--- ProblemSet_6/shirt/shirt.py
+++ ProblemSet_6/shirt/shirt.py
@@ -14,7 +14,7 @@ def main():
             sys.exit("Too few command-line arguments")
         elif len(sys.argv) > 3:
             sys.exit("Too many command-line arguments")
-        elif sys.argv[1][-4:] != ".jpg":
+        elif sys.argv[1][-4:] != ".jpg" or sys.argv[1][-4:] != ".png":
             sys.exit("Invalid Output")
         elif sys.argv[1][-4:] != sys.argv[2][-4:]:
            sys.exit("Input and output have different extensions")
