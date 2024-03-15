24dcab922814b08429ffcd032a80cdb4116ee933
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-15 20:22:08 +0000

    Fri, Mar 15, 2024, 9:22 PM +01:00

--- ProblemSet_7/script.py
+++ ProblemSet_7/script.py
@@ -19,7 +19,8 @@ import re
 
 email = input('whats your mail? \n').strip()
 
-if re.search(r".+@.+\.com$", email):
+if re.search(r"^.+@.+\.com$", email):
     print('valid')
 else:
     print('invalid')
+
