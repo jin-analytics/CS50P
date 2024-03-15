623023b7b1a908a2b05e02c02edcd9fe625740af
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-15 17:05:45 +0000

    Fri, Mar 15, 2024, 6:05 PM +01:00

--- ProblemSet_6/shirt/shirt.py
+++ ProblemSet_6/shirt/shirt.py
@@ -4,7 +4,7 @@ import sys
 import os
 
 def main():
-   check_for_correct_input()
+   check_for_correct_input(sys.argv)
    check_for_correct_extension()
    sys.exit('TUTTO')
 
@@ -14,10 +14,11 @@ def main():
 #save image
 
 
-def check_for_correct_input():
-   if len(sys.argv) < 3:
+def check_for_correct_input(input):
+   print(input.split(" "))
+   if len(input) < 3:
       sys.exit("Too few command-line arguments")
-   elif len(sys.argv) > 3:
+   elif len(input) > 3:
       sys.exit("Too many command-line arguments")
    elif sys.argv[1][-4:] != sys.argv[2][-4:]:
       sys.exit("Input and output have different extensions")
