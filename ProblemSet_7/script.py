0504eab5ec904b3fc06a5a9be1c2e55daa004285
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-15 20:00:44 +0000

    Fri, Mar 15, 2024, 9:00 PM +01:00

--- ProblemSet_7/script.py
+++ ProblemSet_7/script.py
@@ -3,12 +3,15 @@
 #re.search(pattern,string,flags=0)
 import re
 
+#____pattern
 #     .         any character except a newline
 #     *         0 or more repetitions
 #     +         1 or more repetitions
 #     ?         0 or more repetitions
 #     {m}       m repetitions
 #     {m,n}     m-n repetitions
+
+
 
 email = input('whats your mail? \n').strip()
 
