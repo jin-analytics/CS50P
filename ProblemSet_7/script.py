0010ae81bcc19519d3f93c838943b75dbb632eac
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-15 20:21:22 +0000

    Fri, Mar 15, 2024, 9:21 PM +01:00

--- ProblemSet_7/script.py
+++ ProblemSet_7/script.py
@@ -10,12 +10,16 @@ import re
 #     ?         0 or more repetitions
 #     {m}       m repetitions
 #     {m,n}     m-n repetitions
+#     ^         matches the start of the string
+#     $         matches the end of the stringor just
+#               ...before the newline at the end of the string
+#
 
 
 
 email = input('whats your mail? \n').strip()
 
-if re.search(r".+@.+\.com", email):
+if re.search(r".+@.+\.com$", email):
     print('valid')
 else:
     print('invalid')
