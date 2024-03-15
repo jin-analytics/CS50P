c43eb8f9092d7d62361861dc3ae9daa8751a870c
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-15 19:56:02 +0000

    Fri, Mar 15, 2024, 8:56 PM +01:00

--- ProblemSet_7/script.py
+++ ProblemSet_7/script.py
@@ -1,3 +1,14 @@
 ### Script for Lecture 7
 
-    
+#re.search(pattern,string,flags=0)
+
+
+
+
+import re
+email = input('whats your mail? \n').strip()
+
+if re.search('@', email):
+    print('valid')
+else:
+    print('invalid')
