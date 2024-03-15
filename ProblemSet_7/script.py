89cbcedcbd9d60fb7dd116e109a7c69da1fd966c
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-15 19:59:51 +0000

    Fri, Mar 15, 2024, 8:59 PM +01:00

--- ProblemSet_7/script.py
+++ ProblemSet_7/script.py
@@ -1,11 +1,15 @@
 ### Script for Lecture 7
 
 #re.search(pattern,string,flags=0)
-
-
-
-
 import re
+
+#     .         any character except a newline
+#     *         0 or more repetitions
+#     +         1 or more repetitions
+#     ?         0 or more repetitions
+#     {m}       m repetitions
+#     {m,n}     m-n repetitions
+
 email = input('whats your mail? \n').strip()
 
 if re.search('@', email):
