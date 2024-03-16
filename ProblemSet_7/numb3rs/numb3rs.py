fa8f97b74c1dabb2450961a09a2314eb6da3b54f
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-16 18:01:00 +0000

    Sat, Mar 16, 2024, 7:01 PM +01:00

--- ProblemSet_7/numb3rs/numb3rs.py
+++ ProblemSet_7/numb3rs/numb3rs.py
@@ -8,14 +8,11 @@ def main():
 
 # validate() expects a str as input and return True or False
 def validate(ip):
-
     # IPv4 must have this format '#.#.#.#'
-    print('format check', number_format_in_dot_decimal(ip))
-
+    number_format_in_dot_decimal(ip)
     # each number in '#' must be [0:255]
-    print('number check',number_between_0_and_255(ip))
-
-    sys.exit()
+    number_between_0_and_255(ip)
+    return True
 
 
 def number_format_in_dot_decimal(ip):
@@ -30,7 +27,6 @@ def number_between_0_and_255(ip):
         ip = re.split(r'^(\d+).(\d+).(\d+).(\d+)$',ip)
         ip = ' '.join(ip).split()
         for numbers in ip:
-             print(int(numbers))
              if int(numbers) > 255: #negative numbers gets detected through format check
                   sys.exit('to big')
 
