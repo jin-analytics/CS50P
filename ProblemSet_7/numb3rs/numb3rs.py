f8e86fa2ab58b36159627f8709a64fed2b23b694
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-16 16:50:32 +0000

    Sat, Mar 16, 2024, 5:50 PM +01:00

--- ProblemSet_7/numb3rs/numb3rs.py
+++ ProblemSet_7/numb3rs/numb3rs.py
@@ -21,7 +21,7 @@ def validate(ip):
 def number_format_in_dot_decimal(ip):
     #test if i can split 3 dots
 
-    print(re.split('...', ip, maxsplit=3))
+    print(re.split('.', ip, maxsplit=0))
 
 
 
