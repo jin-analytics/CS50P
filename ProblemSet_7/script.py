bb6c8762fc684a87c37aebe65c13900efbfe174c
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-16 15:40:33 +0000

    Sat, Mar 16, 2024, 4:40 PM +01:00

--- ProblemSet_7/script.py
+++ ProblemSet_7/script.py
@@ -11,8 +11,7 @@ import re
 #     {m}       m repetitions
 #     {m,n}     m-n repetitions
 #     ^         matches the start of the string
-#     $         matches the end of the string or just
-#               ...before the newline at the end of the string
+#     $         matches the end of the string
 #     []        set of characters
 #     [^]       complementing the set (opposite)
 # [a-zA-Z0-9_ ] include a,b,...,z/A,B,...,Z/0,1,...,9,_,'space'
@@ -32,23 +31,36 @@ import re
 #    re.DOTALL
 
 
+# Example No.1
+def example1():
+    email = input('whats your mail? \n').strip()
 
-email = input('whats your mail? \n').strip()
+    # ^     matches at the start of the string
+    # /w    accepts 'a,b,...,z | A,B,...,Z | 0,1,...,9 | _'
+    # +     atleast one character of /w or more
+    # @     matches the previous string until @ arrives
+    # /w    accepts 'a,b,...,z | A,B,...,Z | 0,1,...,9 | _' after the @
+    # +     atleast one character of /w or more until the .com
+    # $     matches the end of the string
 
-# ^     matches at the start of the string
-# /w    accepts 'a,b,...,z | A,B,...,Z | 0,1,...,9 | _'
-# +     atleast one character of /w or more
-# @     matches the previous string until @ arrives
-# /w    accepts 'a,b,...,z | A,B,...,Z | 0,1,...,9 | _' after the @
-# +     atleast one character of /w or more until the .com
-# $     matches the end of the string
+    if re.search(r"^\w+.?\w*@\w+.?\w*\.com$", email, re.IGNORECASE):
+        print('valid')
+    else:
+        print('invalid')
 
-if re.search(r"^\w+.?\w*@\w+.?\w*\.com$", email, re.IGNORECASE):
-    print('valid')
-else:
-    print('invalid')
+# Example No. 2
+def example2():
+    name = input("whats your name? ").strip()
+    if matches := re.search(r"^(.+), (.+)$", name):
+        last = matches.group(1)
+        first = matches.group(2)
+        name = f"{first} {last}"
+        #name = matches.group(2) + " " matches.group(1 )
+    print(f"hello, {name}")
 
-
+# Example No.3
+def example3():
+    
 
 
 
