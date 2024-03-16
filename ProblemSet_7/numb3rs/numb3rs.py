739621af11e610bc6d7794f20751c8da94f19c04
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-16 17:21:30 +0000

    Sat, Mar 16, 2024, 6:21 PM +01:00

--- ProblemSet_7/numb3rs/numb3rs.py
+++ ProblemSet_7/numb3rs/numb3rs.py
@@ -28,9 +28,10 @@ def number_format_in_dot_decimal(ip):
 
 def number_between_0_and_255(ip):
     if matches := re.search(r'^(\d+).(\d+).(\d+).(\d+)$',ip):
+        ip = ip.split('.')
         for groups in matches.group():
             print(groups)
-            #if 0 <= int(matches.group(groups)) <= 255:
+            if 0 <= int(matches.group(groups)) <= 255:
                 #print('gut')
 
 
