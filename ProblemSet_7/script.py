d4ddd229172e7f062157aea313ec8c9591b48a79
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-16 11:48:42 +0000

    Sat, Mar 16, 2024, 12:48 PM +01:00

--- ProblemSet_7/script.py
+++ ProblemSet_7/script.py
@@ -7,7 +7,7 @@ import re
 #     .         any character except a newline
 #     *         0 or more repetitions
 #     +         1 or more repetitions
-#     ?         0 or more repetitions
+#     ?         0 or 1 repetition
 #     {m}       m repetitions
 #     {m,n}     m-n repetitions
 #     ^         matches the start of the string
