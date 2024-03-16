abe136687614ce9cdb749c8e570cb0f68ba5ae5e
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-16 17:36:20 +0000

    Sat, Mar 16, 2024, 6:36 PM +01:00

--- ProblemSet_7/numb3rs/numb3rs.py
+++ ProblemSet_7/numb3rs/numb3rs.py
@@ -29,6 +29,7 @@ def number_format_in_dot_decimal(ip):
 def number_between_0_and_255(ip):
     #if matches := re.search(r'^(\d+).(\d+).(\d+).(\d+)$',ip):
         #num1 = int(matches.group(1))
+        ip = ip.strip()
         ip = re.split(r'^(\d+).(\d+).(\d+).(\d+)$',ip)
         print(ip)
 
