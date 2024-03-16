09a996d84669b624960ea712872bd94dbc3d0846
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-16 16:11:13 +0000

    Sat, Mar 16, 2024, 5:11 PM +01:00

--- ProblemSet_7/script.py
+++ ProblemSet_7/script.py
@@ -67,7 +67,7 @@ def example3():
     url = input('URL: ').strip()
     #username = url.replace('https://twitter.com/', '')
     #username = url.removeprefix('https://twitter.com/')
-    username = (r'^(http?s://)?(www\.)?twitter\.com/', '', url)
+    if matches := re.search(r'^(http?s://)?(www\.)?twitter\.com/', '', url)
     print(f'Username: {username}')
 
 
