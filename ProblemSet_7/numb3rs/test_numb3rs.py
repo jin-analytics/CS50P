a73c7f1b1681ed66b4d7fc0e616eed30036975c0
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-17 10:11:27 +0000

    Sun, Mar 17, 2024, 11:11 AM +01:00

--- ProblemSet_7/numb3rs/test_numb3rs.py
+++ ProblemSet_7/numb3rs/test_numb3rs.py
@@ -4,7 +4,7 @@ def main():
     format_only_accepts_4_numberblocks()
 
 def format_only_accepts_4_numberblocks():
-    ip = "1.2.3."
+    ip = "cat"
     #ip = ip.split('.')
     #print(ip)
     assert validate(ip) == True
