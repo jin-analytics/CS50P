21242bd914b69fe5b6170e3870f87be77b85c3b2
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-17 10:17:44 +0000

    Sun, Mar 17, 2024, 11:17 AM +01:00

--- ProblemSet_7/numb3rs/test_numb3rs.py
+++ ProblemSet_7/numb3rs/test_numb3rs.py
@@ -4,9 +4,16 @@ def main():
     test_format_in_dot_decimal()
 
 def test_format_in_dot_decimal():
-    #ip = ip.split('.')
-    #print(ip)
-    assert validate("cat") == True
+    ip_list = [
+        'cat',
+        'cat.dog.mouse.bird',
+        '1.2.34',
+        '1.23.4',
+        '12.3.4',
+        '1234',
+        ]
+    for entrees in ip_list:
+        assert validate(entrees) == False
 
 
 if __name__ == "__main__":
