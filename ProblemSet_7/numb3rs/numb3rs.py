c912f9d08c27c6c7d2deeb76bd663c545bad05a5
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-17 10:40:41 +0000

    Sun, Mar 17, 2024, 11:40 AM +01:00

--- ProblemSet_7/numb3rs/numb3rs.py
+++ ProblemSet_7/numb3rs/numb3rs.py
@@ -34,6 +34,7 @@ def number_between_0_and_255(ip):
         #modification for first byte check:
     if re.search(r'^(\d+).',ip):
         ip = ' '.join(ip).split()
+        print(ip)
         #for numbers in ip:
         for numbers in ip:
              print(numbers)
