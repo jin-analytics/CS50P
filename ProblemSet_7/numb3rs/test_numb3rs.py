b7dd3bd03d6c370685f0a48479387df836a81759
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-17 10:25:30 +0000

    Sun, Mar 17, 2024, 11:25 AM +01:00

--- ProblemSet_7/numb3rs/test_numb3rs.py
+++ ProblemSet_7/numb3rs/test_numb3rs.py
@@ -2,9 +2,12 @@ from numb3rs import validate
 import pytest
 
 def main():
-    test_format_in_dot_decimal()
+    test_number_format_in_dot_decimal()
+    number_between_0_and_255()
 
-def test_format_in_dot_decimal():
+
+#______________________________________________________
+def test_number_format_in_dot_decimal():
     ip_list = [
         'cat',
         'cat.dog.mouse.bird',
@@ -12,6 +15,23 @@ def test_format_in_dot_decimal():
         '1.23.4',
         '12.3.4',
         '1,2,3,4',
+        ]
+
+    with pytest.raises(SystemExit):
+        for entrees in ip_list:
+            validate(entrees)
+
+#______________________________________________________
+def number_between_0_and_255():
+    ip_list = [
+        '256.0.0.0',
+        '0.256.0.0',
+        '0.0.256.0',
+        '0.0.0.256',
+        '-1,0,0,0',
+        '0,-1,0,0',
+        '0,0,-1,0',
+        '0,0,0,-1',
         ]
 
     with pytest.raises(SystemExit):
