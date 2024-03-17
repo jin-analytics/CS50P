55bf15f4221f0e5f82cd8e53733c4c4b30120b8a
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-17 10:50:01 +0000

    Sun, Mar 17, 2024, 11:50 AM +01:00

--- ProblemSet_7/numb3rs/numb3rs.py
+++ ProblemSet_7/numb3rs/numb3rs.py
@@ -22,7 +22,7 @@ def validate(ip):
 def number_format_in_dot_decimal(ip):
     #if re.search(r'^(\d+).(\d+).(\d+).(\d+)$',ip):
     #modification for first byte check:
-    if re.search(r'^(\d+).',ip):
+    if re.search(r'^(\d+)',ip):
         print(ip[0])
         return True
     else:
@@ -32,9 +32,9 @@ def number_format_in_dot_decimal(ip):
 def number_between_0_and_255(ip):
         #ip = re.split(r'^(\d+).(\d+).(\d+).(\d+)$',ip)
         #modification for first byte check:
-    if re.search(r'^(\d+).',ip):
+    if re.search(r'^(\d+)',ip):
         ip = ''.join(ip).split('.')
-        print(ip[0])
+        print(ip)
         #for numbers in ip:
         for numbers in ip:
              print(numbers)
