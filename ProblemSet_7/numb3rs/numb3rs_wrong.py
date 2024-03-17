ff63db90f89f7159bb464bebba369d45176c27d7
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-17 15:25:08 +0000

    Sun, Mar 17, 2024, 4:25 PM +01:00

--- ProblemSet_7/numb3rs/numb3rs_wrong.py
+++ ProblemSet_7/numb3rs/numb3rs_wrong.py
@@ -1,6 +1,28 @@
 import re
 import sys
 
+#____pattern_____________________________________________________________
+#     .         any character except a newline
+#     *         0 or more repetitions
+#     +         1 or more repetitions
+#     ?         0 or 1 repetition
+#     {m}       m repetitions
+#     {m,n}     m-n repetitions
+#     ^         matches the start of the string
+#     $         matches the end of the string
+#     []        set of characters
+#     [^]       complementing the set (opposite)
+# [a-zA-Z0-9_ ] include a,b,...,z/A,B,...,Z/0,1,...,9,_,'space'
+#     \d        decimal digit
+#     \D        not a decimal digit
+#     \s        whitespace character
+#     \S        not a whitespace character
+#     \w        word character...as well as numbers and the _
+#     \W        not a  word character...as well as numbers and the _
+#     A|B       either A or B
+#     (...)     a group
+#     (?:...)   non capturing version
+
 
 def main():
     print(validate(input("IPv4 Address: ")))
