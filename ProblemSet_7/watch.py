66b6902c3eda5f1493da3f5731e6020be1489d19
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-18 17:49:20 +0000

    Mon, Mar 18, 2024, 6:49 PM +01:00

--- ProblemSet_7/watch.py
+++ ProblemSet_7/watch.py
@@ -29,7 +29,7 @@ def main():
 def parse(s):
     #s = re.split(r"^\w[a-z0-9\"/.]*=+[a-z0-9\"/.]*$")
     #=(/w[\":/.?-])
-    s = re.findall(r"^(<)\.$", s)
+    s = re.findall(r"^[<]+$", s)
     print('found: ',s)
 
 
