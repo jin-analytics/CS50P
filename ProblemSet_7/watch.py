88189573ece4dd24c2ae6bbb545fe588eaa01760
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-18 16:56:35 +0000

    Mon, Mar 18, 2024, 5:56 PM +01:00

--- ProblemSet_7/watch.py
+++ ProblemSet_7/watch.py
@@ -3,3 +3,17 @@
 #title="YouTube video player" frameborder="0"
 #allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
 #allowfullscreen></iframe>
+
+
+# implement a function called parse():
+# - that expects a str of HTML as input,
+# - extracts any YouTube URL that’s the value of a src attribute of an iframe element therein,
+# - and returns its shorter,
+# - shareable youtu.be equivalent as a str
+#
+# Possible inputs:
+# input: http://youtube.com/embed/xvFZjo5PgG0
+# input: https://youtube.com/embed/xvFZjo5PgG0
+# input: https://www.youtube.com/embed/xvFZjo5PgG0
+#
+#
