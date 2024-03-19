b9ceaa01e7d01bbcdf87835e45e9d8c37c1467e9
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-19 20:13:27 +0000

    Tue, Mar 19, 2024, 9:13 PM +01:00

--- ProblemSet_7/watch/watch.py
+++ ProblemSet_7/watch/watch.py
@@ -30,11 +30,11 @@ def parse(s):
         # CHECK FOR HTTP, SO THAT IT WILL BE REPLACED WITH HTTPS
         if re.findall(r'https([a-zA-Z0-9:/.]+)$', s[0][1]):
             short = re.sub('be.com/embed','.be', s[0][1])
-            short = re.sub('http:','https:', s[0][1])
+            short = re.sub('https://www.','https://', s[0][1])
             if short != s[0][1]:
                 return short
 
-        short = re.sub('http:','https:', s[0][1])
+        short = re.sub('http://www.','https://', s[0][1])
         short = re.sub('be.com/embed','.be', s[0][1])
         if short != s[0][1]:
                 return short
