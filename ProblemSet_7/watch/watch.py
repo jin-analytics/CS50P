de0c7ac2e7fb5f57ccbaf750d694f65dcf735bbe
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-19 19:24:52 +0000

    Tue, Mar 19, 2024, 8:24 PM +01:00

--- ProblemSet_7/watch/watch.py
+++ ProblemSet_7/watch/watch.py
@@ -28,8 +28,8 @@ def parse(s):
     ###s = re.findall(r'^<iframe(.+)src=\"([a-zA-Z0-9:/.]+)\"(.+)$', s, re.MULTILINE) # THIS CATCHES THE REQUIRED STRING
     ###print(s[0][1])
     ###return True
-    if matches := re.search(r'^<iframe(.+)src=\"([a-zA-Z0-9:/.]+)\"(.+)$', s, re.MULTILINE):
-        print(f'Youtube address:', matches.group(0))
+    s = re.search(r'^<iframe(.+)src=\"([a-zA-Z0-9:/.]+)\"(.+)$', s, re.MULTILINE)
+    print(s)
 
 
 #<iframe width="560
