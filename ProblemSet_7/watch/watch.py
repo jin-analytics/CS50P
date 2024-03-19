51f2d60dc727365802015de72a165771ffee82a6
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-19 19:57:40 +0000

    Tue, Mar 19, 2024, 8:57 PM +01:00

--- ProblemSet_7/watch/watch.py
+++ ProblemSet_7/watch/watch.py
@@ -30,7 +30,7 @@ def parse(s):
         # CHECK FOR HTTP, SO THAT IT WILL BE REPLACED WITH HTTPS
         if matches := re.search(r'^<iframe(.+)src="http([a-zA-Z0-9:/.]+)\"(.+)$', s[0][1]):
             #short = re.sub('http','https', matches.group{0})
-            print(matches.group(0))
+            print(matches.group(1))
 
 
         short = re.sub('be.com/embed','.be', s[0][1])
