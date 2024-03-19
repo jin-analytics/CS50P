7a8e85e0ab24394b75948111d3c4f77f67cadafd
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-19 19:30:33 +0000

    Tue, Mar 19, 2024, 8:30 PM +01:00

--- ProblemSet_7/watch/watch.py
+++ ProblemSet_7/watch/watch.py
@@ -27,7 +27,7 @@ def parse(s):
         ####s = re.findall(r'^(\"[a-zA-Z0-9:/.]+\")+$', s) #THIS CATCHES A YOUTUBE STRING
         s = re.findall(r'^<iframe(.+)src=\"([a-zA-Z0-9:/.]+)\"(.+)$', s, re.MULTILINE) # THIS CATCHES THE REQUIRED STRING
         print(s[0][1])
-        short =
+        short = re.split(r'([a-zA-Z0-9:/.]+)', s[0][1])
         return True
 
     except IndexError:
