a82cbc8777566dc0b9f234c250948a5933831e78
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-19 19:14:41 +0000

    Tue, Mar 19, 2024, 8:14 PM +01:00

--- ProblemSet_7/watch/watch.py
+++ ProblemSet_7/watch/watch.py
@@ -37,7 +37,7 @@ def parse(s):
 
 
     #s = re.findall(r'^(\"[a-zA-Z0-9:/.]+\")+$', s) #THIS CATCHES A YOUTUBE STRING
-    s = re.findall(r'^^(.+)(\"[a-zA-Z0-9:/.]+\")+$', s)
+    s = re.findall(r'^(.+)(\"[a-zA-Z0-9:/.]+\")+(.+)$', s)
     print(s)
     return True
 
