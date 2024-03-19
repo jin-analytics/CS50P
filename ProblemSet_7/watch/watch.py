f5edd18f755eef1c609f39ca8b454b1731e876fc
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-19 12:51:09 +0000

    Tue, Mar 19, 2024, 1:51 PM +01:00

--- ProblemSet_7/watch/watch.py
+++ ProblemSet_7/watch/watch.py
@@ -30,8 +30,8 @@ def parse(s):
     #=(/w[\":/.?-])
     #s = re.findall(r"^(\<\w+)\s+(\w+).+[\"]+(\w+)[\"]+$", s)
     #s = re.search(r"^(.+)src$", s)
-    s = re.findall(r"^(.+)src=\"(.\")(.+)$", s)
-    print(s[0][1])
+    s = re.findall(r"^(.+)src=\"(.+)(\s.+)$", s)
+    print(s[0])
     return True
 
 
