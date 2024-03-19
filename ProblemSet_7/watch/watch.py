6b743b4b5493941d24d5b38b021a246c4182f057
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-19 20:31:25 +0000

    Tue, Mar 19, 2024, 9:31 PM +01:00

--- ProblemSet_7/watch/watch.py
+++ ProblemSet_7/watch/watch.py
@@ -1,19 +1,3 @@
-# <iframe src="https://www.youtube.com/embed/xvFZjo5PgG0"></iframe>
-
-# implement a function called parse():
-# - that expects a str of HTML as input,
-# - extracts any YouTube URL that’s the value of a src attribute of an iframe element therein,
-# - and returns its shorter,
-# - shareable youtu.be equivalent as a str
-#
-# Possible inputs:
-# input: http://youtube.com/embed/xvFZjo5PgG0
-# input: https://youtube.com/embed/xvFZjo5PgG0
-# input: https://www.youtube.com/embed/xvFZjo5PgG0
-# Assume that the value of src will be surrounded by double quotes
-# If the input does not contain any such URL at all, return None
-
-
 import re
 import sys
 
@@ -26,7 +10,6 @@ def parse(s):
     try:
         ####s = re.findall(r'^(\"[a-zA-Z0-9:/.]+\")+$', s) #THIS CATCHES A YOUTUBE STRING
         s = re.findall(r'^<iframe(.+)src=\"([a-zA-Z0-9:/.]+)\"(.+)$', s, re.MULTILINE) # THIS CATCHES THE REQUIRED STRING
-        #print(s[0][1])
 
         if re.findall(r'https([a-zA-Z0-9:/.]+)$', s[0][1]):
             short = re.sub('be.com/embed','.be', s[0][1])
@@ -34,6 +17,7 @@ def parse(s):
             short = re.sub('www.','', short)
             if short != s[0][1]:
                 return short
+
         # HTTP WILL BE REPLACED WITH HTTPS
         short = re.sub('be.com/embed','.be', s[0][1])
         short = re.sub('http://','https://', short)
