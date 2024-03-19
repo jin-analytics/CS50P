0d77f906a71783bc2f150a9d749b961713b4c53d
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-19 20:01:40 +0000

    Tue, Mar 19, 2024, 9:01 PM +01:00

--- ProblemSet_7/watch/watch.py
+++ ProblemSet_7/watch/watch.py
@@ -28,9 +28,9 @@ def parse(s):
         s = re.findall(r'^<iframe(.+)src=\"([a-zA-Z0-9:/.]+)\"(.+)$', s, re.MULTILINE) # THIS CATCHES THE REQUIRED STRING
         #print(s[0][1])
         # CHECK FOR HTTP, SO THAT IT WILL BE REPLACED WITH HTTPS
-        if re.findall(r'^<iframe(.+)src="https([a-zA-Z0-9:/.]+)\"(.+)$', s[0][1]):
+        if re.findall(r'https([a-zA-Z0-9:/.]+)$', s[0][1]):
             #short = re.sub('http','https', matches.group{0})
-            print()
+            print('found')
 
 
         short = re.sub('be.com/embed','.be', s[0][1])
