3145404574e7e42037ef9d4cecb482cc02d20b8b
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-20 09:35:56 +0000

    Wed, Mar 20, 2024, 10:35 AM +01:00

--- ProblemSet_7/working/working.py
+++ ProblemSet_7/working/working.py
@@ -1,7 +1,9 @@
 import re
 import sys
+import csv
 
 # create csv file which has the 12-Hour and 24-Hour implemented
+# make a dict out of it
 
 
 def main():
@@ -10,6 +12,11 @@ def main():
 
 
 def convert(s):
+    with open('hour_format.csv') as file:
+            reader = csv.reader(file)
+            print(reader)
+
+
     #       9:AM to 5:00PM
     s = re.findall(r'^$', s) # THIS CATCHES THE REQUIRED STRING
 
