546bb62c5456df2a3caedc0a121fcf5f63d12c2f
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-20 19:42:17 +0000

    Wed, Mar 20, 2024, 8:42 PM +01:00

--- ProblemSet_7/working/working.py
+++ ProblemSet_7/working/working.py
@@ -41,8 +41,9 @@ def main():
 def convert(s):
 
     s = re.findall(r'^(\d+\:*\d*\s+[AM]*[PM]*) to+ (\d+\:*\d*\s+[AM]*[PM]*)$', s) # THIS CATCHES THE REQUIRED STRING
-    print('Found: ', s[0][0], s[0][1])
-    
+    print('Timeframe: ', s[0][0],'to', s[0][1])
+    re.sub('9:00 AM','',s[0][0])
+
 
     #for key,value in table.items():
     #    if s[0][0] == key:
