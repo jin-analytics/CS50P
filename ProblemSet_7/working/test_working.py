a2b4f914db1f75fb27abe694799e9ac6fabd330f
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-23 06:40:29 +0000

    Sat, Mar 23, 2024, 7:40 AM +01:00

--- ProblemSet_7/working/test_working.py
+++ ProblemSet_7/working/test_working.py
@@ -1,7 +1,29 @@
 from working import convert
 
 def main():
-    
+    test_ante_end_post_meridiem()
+    test_invalid_minutes_in_time()
+    test_invalid_input()
+
+# check if is possible to start at any timeframe the day/night
+def test_ante_end_post_meridiem():
+    meridiem = ['5:00 PM to 9:00 AM', '5:00 AM to 9:00 PM']
+    for entrees in meridiem:
+        assert convert(entrees) == True
+
+# check if invalid minute input gets detected
+def test_invalid_minutes_in_time():
+    meridiem = ['5:60 PM to 9:00 AM', '5:00 AM to 9:60 PM']
+    for entrees in meridiem:
+        assert convert(entrees) == False
+
+# check if invalid input gets detected...
+# correct input: "hh:mm AM/PM to hh:mm AM/PM" or "hh AM/PM to hh AM/PM"
+def test_invalid_input():
+    meridiem = ['5:00 PM - 9:00 AM', '5:00 AM']
+    for entrees in meridiem:
+        assert convert(entrees) == False
+
 
 if __name__ == "__main__":
     main()
