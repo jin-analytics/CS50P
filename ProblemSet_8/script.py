f346c78627df043ed887509aa654be29b5b6eafb
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-24 18:33:13 +0000

    Sun, Mar 24, 2024, 7:33 PM +01:00

--- ProblemSet_8/script.py
+++ ProblemSet_8/script.py
@@ -5,10 +5,16 @@
 
 def main():
     student = get_student()
-    print(f'{student[0]} is in {student[1]}')
+    print(f'{student[]} is in {student[1]}')
 
 def get_student():
     name = input('Name:')
     house = input('House:')
     return (name, house) #is a tuple (is not mutable)
     #return [name,house] #is a list (is mutable)
+
+def get_student():
+    student = {}
+    student['name'] = input('Name: ')
+    student['house'] = input('House: ')
+    return student
