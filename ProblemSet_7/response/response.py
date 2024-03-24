1fcebc191b1aad18da9e09234633ef5890c78edb
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-24 16:30:52 +0000

    Sun, Mar 24, 2024, 5:30 PM +01:00

--- ProblemSet_7/response/response.py
+++ ProblemSet_7/response/response.py
@@ -11,16 +11,13 @@ def email_validation(email_adress):
         # The value of email_address will now be "test@domain.dev"
         email_address = validators.email(email_adress)
         #email_address = validators.email("test@domain.dev")
-
         if checkers.is_email(email_address) == True:
             print('Valid')
 
 
     except errors.EmptyValueError:
         sys.exit('Invalid')
-        # Handling logic goes here
     except errors.InvalidEmailError:
-        # More handlign logic goes here
         sys.exit('Invalid')
 
 if __name__ == "__main__":
