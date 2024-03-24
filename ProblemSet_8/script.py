ff4fadb6c44dea4f834996b20a61545a5103cafc
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-24 18:36:49 +0000

    Sun, Mar 24, 2024, 7:36 PM +01:00

--- ProblemSet_8/script.py
+++ ProblemSet_8/script.py
@@ -14,7 +14,11 @@ def get_student():
     #return [name,house] #is a list (is mutable)
 
 def get_student():
-    student = {}
-    student['name'] = input('Name: ')
-    student['house'] = input('House: ')
-    return student
+    #student = {}
+    #student['name'] = input('Name: ')
+    #student['house'] = input('House: ')
+    #return student
+    name = input('Name:')
+    house = input('House:')
+    return {'name': name, 'house': house} #is a dict (is mutable)
+
