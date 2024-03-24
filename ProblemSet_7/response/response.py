91dc641278ed041762d19bdf1ff0db3b95a314a7
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-24 16:25:55 +0000

    Sun, Mar 24, 2024, 5:25 PM +01:00

--- ProblemSet_7/response/response.py
+++ ProblemSet_7/response/response.py
@@ -1,16 +1,24 @@
+import sys
 from validator_collection import validators, checkers, errors
 # Validator:    email
 # Checkers:     is_email
 
 def main():
-    # The value of email_address will now be "test@domain.dev"
-    #email_address = validators.email(input('What's your email adress '))#
-    email_address = validators.email("testdomain.dev")
+    try:
+        # The value of email_address will now be "test@domain.dev"
+        #email_address = validators.email(input('What's your email adress '))#
+        email_address = validators.email("testdomain.dev")
 
-    if checkers.is_email(email_address) == True:
-        print('Valid')
-    elif checkers.is_email(email_address) != True:
-        print('Invalid')
+        if checkers.is_email(email_address) == True:
+            print('Valid')
+
+
+    except errors.EmptyValueError:
+        sys.exit('Invalid')
+        # Handling logic goes here
+    except errors.InvalidEmailError:
+        # More handlign logic goes here
+        sys.exit('Invalid')
 
 if __name__ == "__main__":
     main()
