1dae9354b2cf8d1daec778a7e3fe8f268ae36a32
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-24 16:16:50 +0000

    Sun, Mar 24, 2024, 5:16 PM +01:00

--- ProblemSet_7/response/response.py
+++ ProblemSet_7/response/response.py
@@ -2,6 +2,7 @@ from validator_collection import validators, checkers, errors
 
 email_address = validators.email('test@domain.dev')
 # The value of email_address will now be "test@domain.dev"
+print(is_email(email_address))
 
 # Validator:    email
 # Checkers:     is_email
