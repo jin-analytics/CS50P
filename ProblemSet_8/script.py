94bb022181462a90acbe000b0c78dadf9a3e5a2d
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-24 18:25:43 +0000

    Sun, Mar 24, 2024, 7:25 PM +01:00

--- ProblemSet_8/script.py
+++ ProblemSet_8/script.py
@@ -1,3 +1,13 @@
 # Object-Oriented Programming
 
+# Tuple
+# (variable1, variable2) is a tuple
 
+def main():
+    student = get_student()
+    print(f'{student[0]} is in {student[1]}')
+
+def get_student():
+    name = input('Name:')
+    house = input('House:')
+    return (name, house)
