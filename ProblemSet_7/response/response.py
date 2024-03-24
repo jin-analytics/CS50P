a1b81721a22c80c2e77641246c35d86686ea5f8a
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-24 16:23:23 +0000

    Sun, Mar 24, 2024, 5:23 PM +01:00

--- ProblemSet_7/response/response.py
+++ ProblemSet_7/response/response.py
@@ -1,8 +1,16 @@
 from validator_collection import validators, checkers, errors
-
-email_address = validators.email('testdomain.dev')
-# The value of email_address will now be "test@domain.dev"
-print(checkers.is_email(email_address))
-
 # Validator:    email
 # Checkers:     is_email
+
+def main():
+    # The value of email_address will now be "test@domain.dev"
+    #email_address = validators.email(input('What's your email adress '))#
+    email_address = validators.email("testdomain.dev")
+
+    if checkers.is_email(email_address) == True:
+        print('Valid')
+    elif checkers.is_email(email_address) != True:
+        print('Invalid')
+
+if __name__ == "__main__":
+    main()
