e8f2ab06c29890ad1db025e89e4b32dc20a12184
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-03-29 14:03:21 +0000

    Fri, Mar 29, 2024, 3:03 PM +01:00

--- ProblemSet_8/script.py
+++ ProblemSet_8/script.py
@@ -37,6 +37,7 @@ class Student:
         self.name = name
         self.house = house
 
+# option 1:
     def __str__(self):
         return f"{self.name} is in {self.house}"
 
@@ -52,6 +53,39 @@ def get_student():
      house = input("House: ")   #   attribute
      #student = Student(name, house)
      return Student(name, house)
+
+
+# option 2:
+
+class Student:
+    def __init__(self, name, house):
+        if not name:
+            raise ValueError('Missing name')
+        if house not in ['griffyndor','hufflepuff','ravenclaw','slytherin']:
+            raise ValueError['Invalid house']
+        self.name = name
+        self.house = house
+
+# option 1:
+    def __str__(self):
+        return f"{self.name} is in {self.house}"
+
+def main():
+    student = get_student()
+    print(f'{student.name} is in {student.house}')
+
+def get_student():
+     student = Student()                #   creates an object from a class
+     #student.name = input("Name: ")     #   attribute
+     #student.house = input("House: ")   #   attribute
+     name = input("Name: ")     #   attribute
+     house = input("House: ")   #   attribute
+     #student = Student(name, house)
+     return Student(name, house)
+
+
+
+
 
 # method: is a function, inside a class
 
