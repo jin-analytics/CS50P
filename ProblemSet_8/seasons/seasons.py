6aa7ae27a461878ea9cbdd238c5ccfc5ac258127
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-04-01 16:19:47 +0000

    Mon, Apr 1, 2024, 6:19 PM +02:00

--- ProblemSet_8/seasons/seasons.py
+++ ProblemSet_8/seasons/seasons.py
@@ -4,10 +4,17 @@ import inflect
 
 
 def main():
-    p = inflect.engine()
-    words = p.number_to_words(1111)
-    print(words)
+    #p = inflect.engine()
+    #words = p.number_to_words(1111)
+    #print(words)
+    ...
 
+
+
+def number_to_word():
+    ...
+
+def
 
 if __name__ == "__main__":
     main()
