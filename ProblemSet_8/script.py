5a54f7dc74e12e8d49363987a06583870b2002a5
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-04-01 08:09:15 +0000

    Mon, Apr 1, 2024, 10:09 AM +02:00

--- ProblemSet_8/script.py
+++ ProblemSet_8/script.py
@@ -111,9 +111,15 @@ import random
 class Hat:
     def __init__(self):
         self.houses = ['griffyndor','hufflepuff','ravenclaw','slytherin']
-
     def sort(self, name):
         print(name, "is in", random.choice(self.houses))
+
+class Hat:
+    houses = ['griffyndor','hufflepuff','ravenclaw','slytherin']
+    @classmethod
+    def sort(cls, name):
+        print(name, "is in", random.choice(self.houses))
+
 
 hat = Hat()
 hat.sort("Harry")
