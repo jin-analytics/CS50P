3bd58a804acc2a28790f2dacf0a22fcceb7970ba
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-04-01 16:13:15 +0000

    Mon, Apr 1, 2024, 6:13 PM +02:00

--- ProblemSet_8/script.py
+++ ProblemSet_8/script.py
@@ -120,8 +120,6 @@ class Hat:
     def sort(cls, name):
         print(name, "is in", random.choice(self.houses))
 
-
 hat = Hat()
 hat.sort("Harry")
-
 
---
+++ ProblemSet_8/seasons/seasons.py
@@ -0,0 +1,14 @@
+from datetime import date
+import inflect
+
+
+
+def main():
+    ...
+
+
+...
+
+
+if __name__ == "__main__":
+    main()
