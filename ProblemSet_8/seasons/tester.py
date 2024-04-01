010349feffc65205d78c69f58b8a0171f909800c
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-04-01 17:04:06 +0000

    Mon, Apr 1, 2024, 7:04 PM +02:00

--- ProblemSet_8/seasons/tester.py
+++ ProblemSet_8/seasons/tester.py
@@ -1,12 +1,13 @@
 from datetime import date
 from datetime import timedelta
 import inflect
+import datetime
 
 
 
 birthday = input()
 birthday = birthday.split('-')
-birthday = datetime.date(birthday[0], birthday[1], birthday[2])
+birthday = datetime.date(int(birthday[0]), int(birthday[1]), int(birthday[2]))
 
 date = date.today() - birthday
 print(date)
