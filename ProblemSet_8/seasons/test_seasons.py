476f84b73deb71c335602bcc3e67a270fec8d6f4
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-04-01 17:36:50 +0000

    Mon, Apr 1, 2024, 7:36 PM +02:00

---
+++ ProblemSet_8/seasons/test_seasons.py
@@ -0,0 +1,19 @@
+from seasons import time_delta
+from seasons import number_to_word
+
+def main():
+    test_time_delta()
+    test_number_to_word()
+
+
+def test_time_delta():
+    assert test_time_delta() == 
+
+
+def test_number_to_word():
+    ...
+
+
+if __name__ == "__main__":
+    main()
+
