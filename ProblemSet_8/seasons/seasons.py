6bca2a94c37277c12499838d4730e7caa7aa1d4a
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-04-01 17:29:07 +0000

    Mon, Apr 1, 2024, 7:29 PM +02:00

--- ProblemSet_8/seasons/seasons.py
+++ ProblemSet_8/seasons/seasons.py
@@ -11,17 +11,17 @@ def main():
 
 
 def time_delta(birthday):
-    birthday = birthday.split('-')
-    birthday = datetime.date(int(birthday[0]), int(birthday[1]), int(birthday[2]))
-    passed_days = date.today() - birthday
-    passed_minutes = int(passed_days.days)*24*60
-    return passed_minutes
+    try:
+        birthday = birthday.split('-')
+        birthday = datetime.date(int(birthday[0]), int(birthday[1]), int(birthday[2]))
+        passed_days = date.today() - birthday
+        passed_minutes = int(passed_days.days)*24*60
+        return passed_minutes
+    except :
 
 def number_to_word(d):
     p = inflect.engine()
     return p.number_to_words(d)
-
-
 
 
 if __name__ == "__main__":
