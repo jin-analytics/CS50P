8e6cd134a98d65734f7c698e0bc611263ec1a16f
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-04-01 17:10:21 +0000

    Mon, Apr 1, 2024, 7:10 PM +02:00

--- ProblemSet_8/seasons/tester.py
+++ ProblemSet_8/seasons/tester.py
@@ -9,5 +9,5 @@ birthday = input()
 birthday = birthday.split('-')
 birthday = datetime.date(int(birthday[0]), int(birthday[1]), int(birthday[2]))
 
-date = date.today() - birthday
-print(date.tseconds())
+passed_days = date.today() - birthday
+print(passed_days)
