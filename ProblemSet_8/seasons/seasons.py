875869fbad0521722e5aa55396edd593d9590c51
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-04-01 16:25:11 +0000

    Mon, Apr 1, 2024, 6:25 PM +02:00

--- ProblemSet_8/seasons/seasons.py
+++ ProblemSet_8/seasons/seasons.py
@@ -4,17 +4,21 @@ import inflect
 
 
 def main():
-    #p = inflect.engine()
-    #words = p.number_to_words(1111)
-    #print(words)
+    delta = time_delta(input("Date of Birth: ")) #format YYYY-MM-DD
     ...
 
 
+def time_delta(d):
+     birthdate = d
+     print(date.today())
 
 def number_to_word():
     ...
+    #p = inflect.engine()
+    #words = p.number_to_words(1111)
+    #print(words)
 
-def
+
 
 if __name__ == "__main__":
     main()
