110edb11a5da5e48415d349fa1ad9f4f0ab36fb7
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-04-01 17:07:10 +0000

    Mon, Apr 1, 2024, 7:07 PM +02:00

--- ProblemSet_8/seasons/tester.py
+++ ProblemSet_8/seasons/tester.py
@@ -10,4 +10,4 @@ birthday = birthday.split('-')
 birthday = datetime.date(int(birthday[0]), int(birthday[1]), int(birthday[2]))
 
 date = date.today() - birthday
-print(date.total_minutes())
+print(date.tseconds())
