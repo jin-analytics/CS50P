11c45889374dfac632d4dc933a304629cceba705
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-04-03 18:01:59 +0000

    Wed, Apr 3, 2024, 8:01 PM +02:00

--- ProblemSet_8/seasons/seasons.py
+++ ProblemSet_8/seasons/seasons.py
@@ -7,21 +7,14 @@ import re
 
 def main():
     delta = time_delta(input("Date of Birth: ")) #format YYYY-MM-DD
-    #delta = remove_and(delta)
     print(remove_and(number_to_word(delta)).capitalize() + " minutes")
-    #print(number_to_word(delta).capitalize() + " minutes")
-
 
 def time_delta(birthday):
-    #try:
-
     birthday = birthday.split('-')
     birthday = datetime.date(int(birthday[0]), int(birthday[1]), int(birthday[2]))
     passed_days = date.today() - birthday
     passed_minutes = int(passed_days.days)*24*60
     return passed_minutes
-    #except ValueError:
-        #sys.exit("Invalid date")
 
 def number_to_word(d):
     p = inflect.engine()
