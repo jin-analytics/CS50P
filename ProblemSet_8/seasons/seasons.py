ac6cd8959ffbccf3d2a12c2c10af7eb51ad5308c
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-04-05 10:42:41 +0000

    Fri, Apr 5, 2024, 12:42 PM +02:00

--- ProblemSet_8/seasons/seasons.py
+++ ProblemSet_8/seasons/seasons.py
@@ -12,11 +12,14 @@ def main():
     print(remove_and(number_to_word(delta)).capitalize() + " minutes")
 
 def time_delta(birthday):
-    birthday = birthday.split('-')
-    birthday = datetime.date(int(birthday[0]), int(birthday[1]), int(birthday[2]))
-    passed_days = date.today() - birthday
-    passed_minutes = int(passed_days.days)*24*60
-    return passed_minutes
+    try:
+        birthday = birthday.split('-')
+        birthday = datetime.date(int(birthday[0]), int(birthday[1]), int(birthday[2]))
+        passed_days = date.today() - birthday
+        passed_minutes = int(passed_days.days)*24*60
+        return passed_minutes
+    except ValueError:
+        sys.exit("Invalid date")
 
 def number_to_word(d):
     p = inflect.engine()
--- ProblemSet_8/seasons/test_seasons.py
+++ ProblemSet_8/seasons/test_seasons.py
@@ -8,15 +8,16 @@ def main():
     test_time_delta()
     test_number_to_word()
 
-
 def test_time_delta():
     today = str(date.today())
     assert time_delta(today) == 0
 
-
 def test_number_to_word():
     assert number_to_word(1) == "one"
 
+def test_ValueError():
+    with pytest.raises(SystemExit):
+        time_delta("26.12.1994")
 
 if __name__ == "__main__":
     main()
