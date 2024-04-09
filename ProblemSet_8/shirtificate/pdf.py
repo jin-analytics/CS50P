aaf3cc985989e8b30016b3334d764bb8b827b905
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-04-09 11:35:05 +0000

    Tue, Apr 9, 2024, 1:35 PM +02:00

--- ProblemSet_8/shirtificate/pdf.py
+++ ProblemSet_8/shirtificate/pdf.py
@@ -7,7 +7,7 @@ pdf.set_font("helvetica", "B", 16)
 pdf.cell(40, 10, "Hello World!")
 
 img = Image.open("shirtificate.png")
-pdf.image(img, x=100, y=100)
+pdf.image(img, x=0, y=10)
 
 #pdf.image("https://cs50.harvard.edu/python/2022/psets/8/shirtificate/shirtificate.png", h=pdf.eph/2, w=pdf.epw/2)
 pdf.output("pdf-with-image.pdf")
