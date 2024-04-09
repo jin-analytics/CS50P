dd752e7f3497fd0909dc8ca1485ba26edf951ac1
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-04-09 11:27:59 +0000

    Tue, Apr 9, 2024, 1:27 PM +02:00

--- ProblemSet_8/shirtificate/pdf.py
+++ ProblemSet_8/shirtificate/pdf.py
@@ -1,8 +1,14 @@
 from fpdf import FPDF
+from PIL import Image
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 pdf.add_page()
 pdf.set_font("helvetica", "B", 16)
 pdf.cell(40, 10, "Hello World!")
-pdf.image("https://cs50.harvard.edu/python/2022/psets/8/shirtificate/shirtificate.png", h=pdf.eph/2, w=pdf.epw/2)
+
+img = Image.open("https://cs50.harvard.edu/python/2022/psets/8/shirtificate/shirtificate.png")
+img = img.crop((10, 10, 490, 490)).resize((96, 96), resample=Image.NEAREST)
+pdf.image(img, x=80, y=100)
+
+#pdf.image("https://cs50.harvard.edu/python/2022/psets/8/shirtificate/shirtificate.png", h=pdf.eph/2, w=pdf.epw/2)
 pdf.output("pdf-with-image.pdf")
