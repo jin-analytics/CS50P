94b7ed0bc7c187bfb51fef9bf4218830fbc0ef8f
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-04-09 11:19:38 +0000

    Tue, Apr 9, 2024, 1:19 PM +02:00

--- ProblemSet_8/shirtificate/pdf.py
+++ ProblemSet_8/shirtificate/pdf.py
@@ -3,5 +3,5 @@ from fpdf import FPDF
 
 pdf = FPDF()
 pdf.add_page()
-pdf.image("docs/fpdf2-logo.png", x=20, y=60)
+pdf.image("https://cs50.harvard.edu/python/2022/psets/8/shirtificate/shirtificate.png", x=20, y=60)
 pdf.output("pdf-with-image.pdf")
