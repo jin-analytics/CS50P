083ba5f94199492801760b72d818e96acd4d16cb
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-04-12 12:36:41 +0000

    Fri, Apr 12, 2024, 2:36 PM +02:00

--- ProblemSet_8/shirtificate/pdf.py
+++ ProblemSet_8/shirtificate/pdf.py
@@ -1,6 +1,8 @@
 from fpdf import FPDF
 from PIL import Image
 
+name = input("Name: ")
+
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 pdf.add_page()
 pdf.set_font("helvetica", "B", 16)
@@ -9,7 +11,7 @@ pdf.cell(40, 50, "CS50 Shirtificate")
 img = Image.open("shirtificate.png")
 img = img.crop((0, 0, 593, 592)).resize((493, 492))
 pdf.image(img, x=20, y=90, keep_aspect_ratio=True)
-pdf.cell(100, 300, "Jakob Niedenhoff took CS50", new_x="LMARGIN", new_y="NEXT", align='C')
+pdf.cell(100, 300, f"{name} took CS50", new_x="LMARGIN", new_y="NEXT", align='C')
 
 #pdf.image("https://cs50.harvard.edu/python/2022/psets/8/shirtificate/shirtificate.png", h=pdf.eph/2, w=pdf.epw/2)
 pdf.output("pdf-with-image.pdf")
