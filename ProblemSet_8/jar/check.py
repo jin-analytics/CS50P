4d84829fb7927e33176419b500f2e4f79d7d5ae5
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-04-17 13:54:36 +0000

    Wed, Apr 17, 2024, 3:54 PM +02:00

--- ProblemSet_8/jar/check.py
+++ ProblemSet_8/jar/check.py
@@ -1,21 +1,17 @@
 class Jar:
-    # __init__ initilizes the capacity of the cookie jar
-    # when "capacity" is not positive, raises ValueError
-    def __init__(self, capacity=12):
-         if capacity < 0:
+    def __init__(self, capacity=12, initial):
+         if int(capacity) < 0:
             raise ValueError('Negative capacity')
         #self.capacity = capacity
-
-
+        #self.initial = initial
 
 def main():
     print(get_cookies())
 
-
 def get_cookies():
-    in_jar = input("How many cookies to the jar?")
-    jar = Jar(in_jar)
-    return jar
+    initial = input("How many cookies to the jar?")
+    #jar = Jar(initial)
+    return Jar(initial)
 
 if __name__ == "__main__":
     main()
