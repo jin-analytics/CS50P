e39964437d305acf6bde9693ff4270c0fb6d65c4
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-04-17 10:26:44 +0000

    Wed, Apr 17, 2024, 12:26 PM +02:00

--- ProblemSet_8/jar/jar.py
+++ ProblemSet_8/jar/jar.py
@@ -2,7 +2,9 @@ class Jar:
     # __init__ initilizes the capacity of the cookie jar
     # when "capacity" is not positive, raises ValueError
     def __init__(self, capacity=12):
-        ...
+         if capacity < 0:
+            raise ValueError('Negative capacity')
+        self.capacity = capacity
 
     # __str__ returns a string with n times "🍪" in the jar
     def __str__(self):
