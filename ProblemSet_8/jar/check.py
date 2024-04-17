ed533df0ed0c1e04335b3674583092d3062b3291
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-04-17 14:55:47 +0000

    Wed, Apr 17, 2024, 4:55 PM +02:00

--- ProblemSet_8/jar/check.py
+++ ProblemSet_8/jar/check.py
@@ -1,11 +1,12 @@
 class Jar:
     def __init__(self, initial_amount, deposit, withdraw):
         self.initial_amount = initial_amount
-        self.capacity = added_amount
-        self.size = size
+        self.deposit = deposit
+        self.withdraw = withdraw
 
     def __str__(self):
-        return f"At the beginning are {self.initial_amount} cookies in the jar..."
+        cookies_eaten = int(self.deposit) - int(self.withdraw)
+        return f"Out of {self.deposit} cookies where {self.withdraw} cookies eaten... {cookies_eaten} left"
 
 
 # Deposit of cookies
@@ -32,8 +33,8 @@ def main():
 def get_cookies():
     initial_amount = 0
     deposit = input("How many cookies to the jar? ")
-    size = input("How many cookies will be eaten from the jar? ")
-    return Jar(initial_amount, added_amount, size)
+    withdraw = input("How many cookies will be eaten from the jar? ")
+    return Jar(initial_amount, deposit, withdraw)
 
 if __name__ == "__main__":
     main()
