dfb1d7fbe7ad58c376e994b97a7cd00e5fa58230
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-04-18 09:08:38 +0000

    Thu, Apr 18, 2024, 11:08 AM +02:00

--- ProblemSet_8/jar/check.py
+++ ProblemSet_8/jar/check.py
@@ -1,5 +1,5 @@
 class Jar:
-    def __init__(self, initial_amount, deposit, withdraw, capacity=12):
+    def __init__(self, deposit, withdraw, initial_amount=0, capacity=12):
         self.initial_amount = initial_amount
         self.deposit = deposit
         self.withdraw = withdraw
@@ -9,7 +9,7 @@ class Jar:
         cookies_left = int(self.deposit) - int(self.withdraw)
         s = str_append_list_join("🍪", cookies_left)
         #return f"Out of {self.deposit} cookies where {self.withdraw} cookies eaten... {cookies_left} left"
-        return f"{s}, capacity: {self.capacity}"
+        return f"{s}, capacity: {self.capacity}, init amount: {self.initial_amount}"
 
     @property
     def capacity(self):
@@ -17,6 +17,13 @@ class Jar:
     @capacity.setter
     def capacity(self, capacity):
         self._capacity = capacity
+
+    @property
+    def initial_amount(self):
+        return self._initial_amount
+    @initial_amount.setter
+    def initial_amount(self, initial_amount):
+        self._initial_amount = initial_amount
 
 
 # Deposit of cookies
@@ -41,10 +48,10 @@ def main():
     print(get_cookies())
 
 def get_cookies():
-    initial_amount = 0
+    #initial_amount = 0
     deposit = input("How many cookies to the jar? ")
     withdraw = input("How many cookies will be eaten from the jar? ")
-    return Jar(initial_amount, deposit, withdraw)
+    return Jar(deposit, withdraw)
 
 def str_append_list_join(add_sign, n):
     l1 = []
