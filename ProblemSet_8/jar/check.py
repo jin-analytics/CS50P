6042d917b7215836c31ce41cdb1bcff50265be7b
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-04-18 11:06:54 +0000

    Thu, Apr 18, 2024, 1:06 PM +02:00

--- ProblemSet_8/jar/check.py
+++ ProblemSet_8/jar/check.py
@@ -32,7 +32,7 @@ class Jar:
         return self._size
     @size.setter
     def size(self, size):
-        size = int(self.deposit) - int(self.withdraw)
+        size = int(self._deposit) - int(self._withdraw)
         self._size = size
 
 
