795b60431f1bacaf6cf236c081471459e3b9f9b3
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-04-19 16:04:26 +0000

    Fri, Apr 19, 2024, 6:04 PM +02:00

--- ProblemSet_8/jar/check.py
+++ ProblemSet_8/jar/check.py
@@ -1,10 +1,10 @@
 class Jar:
-    def __init__(self,deposit, withdraw, size=0,  capacity=12):
+    def __init__(self, capacity=12):
          if capacity < 0:
             raise ValueError
-        self.deposit = deposit
-        self.withdraw = withdraw
-        self.size = size
+        #self.deposit = deposit
+        #self.withdraw = withdraw
+        #self.size = size
         self.capacity = capacity
 
 
@@ -28,7 +28,7 @@ class Jar:
 def main():
     #get_cookies()
     cookies = Jar(5, 3)
-    print(Jar.cookies)
+    print(cookies.deposit())
 
 def get_cookies():
     cookies = Jar(5, 3)
