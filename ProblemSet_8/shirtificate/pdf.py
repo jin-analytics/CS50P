cff5195ed1c26c583c41b50b7f9477709c5ab850
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-04-20 07:31:56 +0000

    Sat, Apr 20, 2024, 9:31 AM +02:00

--- ProblemSet_8/shirtificate/pdf.py
+++ ProblemSet_8/shirtificate/pdf.py
@@ -7,7 +7,8 @@ pdf = FPDF(orientation="P", unit="mm", format="A4")
 pdf.add_page()
 pdf.set_font("helvetica", "B", 16)
 #pdf.cell(40, 50, "CS50 Shirtificate")
-pdf.cell(XPos, new_y, "CS50 Shirtificate")
+pdf.cell(XPos(), new_y, "CS50 Shirtificate")
+XPos(Center)
 
 
 img = Image.open("shirtificate.png")
