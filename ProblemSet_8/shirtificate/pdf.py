63ddd08306b89e100a638f00801fa63532d15f42
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-04-20 09:43:54 +0000

    Sat, Apr 20, 2024, 11:43 AM +02:00

--- ProblemSet_8/shirtificate/pdf.py
+++ ProblemSet_8/shirtificate/pdf.py
@@ -10,17 +10,17 @@ h = 297 #A4 height
 
 pdf.set_font("Helvetica", "B", 16)
 pdf.add_page() # creates page nr. 1
-#pdf.cell(0,30,"CS50 Shirtificate", align = 'C', new_x="LMARGIN", new_y="NEXT", border = 1)
-pdf.cell(0,30,"CS50 Shirtificate", new_x="LMARGIN", new_y="NEXT", border = 1, align = 'C')
 
-pdf.cell(0,100,"", new_x="LMARGIN", new_y="NEXT", border = 1, align = 'C')
+pdf.cell(0,30,"CS50 Shirtificate", new_x="LMARGIN", new_y="NEXT", border = 1, align = 'C') #cell nr.1
+# Abstandshalter zwischen Cell Nr 1 und Cell Nr 3
+pdf.cell(0,90,"", new_x="LMARGIN", new_y="NEXT", border = 1, align = 'C') #cell nr.2
 
 
 img = Image.open("shirtificate.png")
-img = img.crop((0, 0, 593, 592)).resize((493, 492))
-pdf.image(img, x=20, y=90, keep_aspect_ratio=True)
-#pdf.cell(0, 30, f"{name} took CS50", new_x="Center", new_y="NEXT", align='C', border = 1)
-pdf.cell(0, 30, f"{name} took CS50", new_x="CENTER", new_y="NEXT", border = 1, align = 'C')
+#img = img.crop((0, 0, 593, 592)).resize((493, 492))
+pdf.image(img, x=0, y=90, keep_aspect_ratio=True)
+
+pdf.cell(0, 30, f"{name} took CS50", new_x="CENTER", new_y="NEXT", border = 1, align = 'C') # cell nr.3
 
 #pdf.image("https://cs50.harvard.edu/python/2022/psets/8/shirtificate/shirtificate.png", h=pdf.eph/2, w=pdf.epw/2)
 pdf.output("pdf-with-image.pdf")
