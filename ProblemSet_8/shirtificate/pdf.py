48230b88e46d3725d0297706cd585c571a6b1e0c
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-04-20 09:50:28 +0000

    Sat, Apr 20, 2024, 11:50 AM +02:00

--- ProblemSet_8/shirtificate/pdf.py
+++ ProblemSet_8/shirtificate/pdf.py
@@ -15,14 +15,14 @@ pdf.cell(0,30,"CS50 Shirtificate", new_x="LMARGIN", new_y="NEXT", border = 1, al
 # Abstandshalter zwischen Cell Nr 1 und Cell Nr 3
 
 #pdf.cell(0,90,"", new_x="LMARGIN", new_y="NEXT", border = 1, align = 'C') #cell nr.2
-pdf.set_xy(0,h/2.5)
+pdf.set_xy(0, h/2.5)
 
 
 img = Image.open("shirtificate.png")
 img = img.crop((0, 0, 593, 592)).resize((493, 492))
 pdf.image(img, x=20, y=90, keep_aspect_ratio=True)
 
-pdf.cell(0, 30, f"{name} took CS50", new_x="CENTER", new_y="NEXT", border = 1, align = 'C') # cell nr.3
+pdf.cell(0, 30, f"{name} took CS50", new_x="LMARGIN", new_y="NEXT", border = 1, align = 'C') # cell nr.3
 
 #pdf.image("https://cs50.harvard.edu/python/2022/psets/8/shirtificate/shirtificate.png", h=pdf.eph/2, w=pdf.epw/2)
 pdf.output("pdf-with-image.pdf")
