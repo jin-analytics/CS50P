e1f472432cbf8d72a64788a9a1272a096e25bd5b
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-04-20 10:30:29 +0000

    Sat, Apr 20, 2024, 12:30 PM +02:00

--- ProblemSet_8/shirtificate/pdf.py
+++ ProblemSet_8/shirtificate/pdf.py
@@ -20,16 +20,7 @@ pdf.cell(0,30,"CS50 Shirtificate", new_x="LMARGIN", new_y="NEXT", border = 1, al
 
 pdf.image("shirtificate.png", Align.C, y=100, w=180) #when just one param. (w or h) setted, the other gets autom. calculated
 
-#img = Image.open("shirtificate.png")
-#width, height = img.size   # Get dimensions
-#print(width, height)
-
-#img = img.crop((0, 0, 593, 592)).resize((493, 492))
-# 1 pixel = 0.2645833333 mm
-# 493 pixel = 130.4395833169 mm
-# (210mm - 130.43958mm)/2 = 39.78mm
-#pdf.image(img, x=19, y=90, keep_aspect_ratio=True)
-
+pdf.set_
 pdf.cell(0, 30, f"{name} took CS50", new_x="LMARGIN", new_y="NEXT", border = 1, align = 'C') # cell nr.3
 
 #pdf.image("https://cs50.harvard.edu/python/2022/psets/8/shirtificate/shirtificate.png", h=pdf.eph/2, w=pdf.epw/2)
