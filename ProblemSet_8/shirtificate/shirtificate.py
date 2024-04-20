01de6689bbf218ab1b0ec6c75e899068c983180b
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-04-20 10:51:52 +0000

    Sat, Apr 20, 2024, 12:51 PM +02:00

--- ProblemSet_8/shirtificate/shirtificate.py
+++ ProblemSet_8/shirtificate/shirtificate.py
@@ -7,9 +7,9 @@ pdf = FPDF(orientation="P", unit="mm", format="A4")
 
 pdf.set_font("Helvetica", "B", 16)
 pdf.add_page() # creates page nr. 1
-
 pdf.cell(0,30,"CS50 Shirtificate", new_x="LMARGIN", new_y="NEXT", border = 1, align = 'C') #cell nr.1
 
+pdf.allow_images_transparency = True
 pdf.image("shirtificate.png", Align.C, y=90, w=180) #when just one param. (w or h) setted, the other gets autom. calculated
 
 pdf.set_text_color(255, 255, 255)
