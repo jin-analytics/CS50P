6931cdd3607d6b9cb02da95769c652cf8ecb5c98
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-04-20 10:53:57 +0000

    Sat, Apr 20, 2024, 12:53 PM +02:00

--- ProblemSet_8/shirtificate/shirtificate.py
+++ ProblemSet_8/shirtificate/shirtificate.py
@@ -10,7 +10,7 @@ pdf.add_page() # creates page nr. 1
 pdf.cell(0,30,"CS50 Shirtificate", new_x="LMARGIN", new_y="NEXT", border = 1, align = 'C') #cell nr.1
 
 pdf.allow_images_transparency = True
-pdf.image("shirtificate.png", Align.C, y=90, w=180) #when just one param. (w or h) setted, the other gets autom. calculated
+im = pdf.image("shirtificate.png", Align.C, y=90, w=180) #when just one param. (w or h) setted, the other gets autom. calculated
 
 pdf.set_text_color(255, 255, 255)
 pdf.cell(0, 30, f"{name} took CS50", new_x="LMARGIN", new_y="NEXT", border = 1, align = 'C') # cell nr.3
