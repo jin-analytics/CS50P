65e521c73864503b3dd5e17abc818091b5bf8b0a
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-04-20 09:27:27 +0000

    Sat, Apr 20, 2024, 11:27 AM +02:00

--- ProblemSet_8/shirtificate/pdf.py
+++ ProblemSet_8/shirtificate/pdf.py
@@ -10,14 +10,14 @@ h = 297 #A4 height
 
 pdf.set_font("Helvetica", "B", 16)
 pdf.add_page() # creates page nr. 1
-pdf.cell(0,30,"CS50 Shirtificate", border = 0,ln = 1, align = 'C')
-
+pdf.cell(0,30,"CS50 Shirtificate", align = 'C')
+#pdf.cell(new_x=XPos.Center,new_y=YPos.Top,"CS50 Shirtificate", border = 0,ln = 1, align = 'C')
 
 
 img = Image.open("shirtificate.png")
 img = img.crop((0, 0, 593, 592)).resize((493, 492))
 pdf.image(img, x=20, y=90, keep_aspect_ratio=True)
-pdf.cell(100, 300, f"{name} took CS50", new_x="LMARGIN", new_y="NEXT", align='C')
+pdf.cell(0, 30, f"{name} took CS50", new_x="Center", new_y="NEXT", align='C')
 
 #pdf.image("https://cs50.harvard.edu/python/2022/psets/8/shirtificate/shirtificate.png", h=pdf.eph/2, w=pdf.epw/2)
 pdf.output("pdf-with-image.pdf")
