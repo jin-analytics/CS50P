8fc9319577f9917ac16d3b74a15ebc182c9ab1a7
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-04-20 10:49:17 +0000

    Sat, Apr 20, 2024, 12:49 PM +02:00

--- ProblemSet_8/shirtificate/shirtificate.py
+++ ProblemSet_8/shirtificate/shirtificate.py
@@ -6,14 +6,13 @@ name = input("Name: ")
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 
 pdf.set_font("Helvetica", "B", 16)
-pdf.set_text_color(20, 50, 50)
 pdf.add_page() # creates page nr. 1
 
 pdf.cell(0,30,"CS50 Shirtificate", new_x="LMARGIN", new_y="NEXT", border = 1, align = 'C') #cell nr.1
 
 pdf.image("shirtificate.png", Align.C, y=90, w=180) #when just one param. (w or h) setted, the other gets autom. calculated
 
-
+pdf.set_text_color(255, 255, 255)
 pdf.cell(0, 30, f"{name} took CS50", new_x="LMARGIN", new_y="NEXT", border = 1, align = 'C') # cell nr.3
 
 #pdf.image("https://cs50.harvard.edu/python/2022/psets/8/shirtificate/shirtificate.png", h=pdf.eph/2, w=pdf.epw/2)
