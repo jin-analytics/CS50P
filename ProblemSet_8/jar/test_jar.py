5d77797f2ea83e695bfac5bea3b1b432b2a3aa00
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-04-20 06:36:37 +0000

    Sat, Apr 20, 2024, 8:36 AM +02:00

--- ProblemSet_8/jar/test_jar.py
+++ ProblemSet_8/jar/test_jar.py
@@ -8,16 +8,18 @@ def test_init():
 
 
 def test_str():
-    jar = Jar(size=0, deposit=5, withdraw=1)
-    assert str(jar) == "🍪🍪🍪🍪"
-    #jar.deposit(1)
-    #assert str(jar) == "🍪"
-    #jar.deposit(11)
-    #assert str(jar) == "🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪"
+    jar = Jar()
+    assert str(jar) == ""
+    jar.deposit(1)
+    assert str(jar) == "🍪"
+    jar.deposit(11)
+    assert str(jar) == "🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪"
 
 
 def test_deposit():
-    ...
+    jar = Jar()
+    assert str(jar) == ""
+    jar.deposit(1)
 
 
 def test_withdraw():
