0a138681fb0121e75f48fd1ac82e9f3745ec3131
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-04-25 17:43:15 +0000

    Thu, Apr 25, 2024, 7:43 PM +02:00

--- ProblemSet_9/project/project.py
+++ ProblemSet_9/project/project.py
@@ -1,18 +1,20 @@
 import random
-main():
-    get_data():
+def main():
+    get_data()
 
-# first, use random data
-get_data():
-    temp = random.randrange(15, 25, 0.2)
+# first, use random data... later arduino
+def get_data():
+    while True:
+        temp = random.randrange(15, 25)
+        if temp == 18:
+            return False
+        print(get_data())
 
-...
+def create_window_for_data_plot():
+    ...
 
-create_window_for_data_plot():
-...
-
-plot_data():
-...
+def plot_data():
+    ...
 
 
 if __name__ == "__main__":
