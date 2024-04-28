dc230563616228bece7fb6b5d8bf7bb2a4731279
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-04-28 15:34:01 +0000

    Sun, Apr 28, 2024, 5:34 PM +02:00

---
+++ ProblemSet_9/project/animation.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+import matplotlib
+from matplotlib import FuncAnimation
+
+def main():
+    i = input("Number of data points: ")
+    plot_animated_data(i, get_data(i))
+
+# first, use random data... later arduino data
+def get_data(i):
+    templist = []
+    for _ in range(int(i)):
+        temp = random.randrange(17, 22)
+        templist.append(temp)
+        #time.sleep(1)
+    return templist
+
+def plot_animated_data(i, tempdata):
+    # evenly spaced values with given stepwidth (default stepwidth: 1)
+    x = np.arange(0, int(i))
+    # evenly spaced values with given a number of steps
+    x_float = np.linspace(0, int(i), int(i)+1)
+
+    #Figure settings
+    fig, ax = plt.subplots(figsize=(5, 3))
+    fig.subplots_adjust(bottom=0.15, left=0.2)
+    ax.plot(x, tempdata)
+    ax.set_xlabel('Datapoint [Nr.]')
+    ax.set_ylabel('Temprature [°C]')
+    ax.set_title('Temperature data from Arduino', loc='center')
+
+    plt.savefig('data.png', bbox_inches='tight') #fits figure with less whitespace around the image
+
+main()
