d08c0d58ffe57b6140d78a564b67698955c30aa4
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-04-28 11:47:51 +0000

    Sun, Apr 28, 2024, 1:47 PM +02:00

--- ProblemSet_9/project/plot.py
+++ ProblemSet_9/project/plot.py
@@ -3,25 +3,27 @@ import numpy as np
 import random
 
 def main():
-    get_data()
-    plot_data()
+    i = input("number of data points")
+    get_data(i)
+    plot_data(i)
 
 # first, use random data... later arduino data
-def get_data():
+def get_data(i):
     #while True:
     templist = []
-    for _ in range(10):
+    for _ in range(int(i)):
         temp = random.randrange(15, 25,)
         templist.append(temp)
         #print(templs)
         #time.sleep(1)
     return templist
 
-def plot_data():
+def plot_data(i):
     # evenly spaced values with given stepwidth (default stepwidth: 1)
-    y = np.arange(0, 10)
+    y = np.arange(0, int(i))
     # evenly spaced values with given a number of steps
-    y_float = np.linspace(0, 10, 11)
+    y_float = np.linspace(0, int(i), int(i)+1)
     print(y)
+    print(y_float)
 
 main()
