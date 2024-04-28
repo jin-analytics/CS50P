62d4e11c37844e0618445c0abc2686de3db719d7
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-04-28 11:34:31 +0000

    Sun, Apr 28, 2024, 1:34 PM +02:00

---
+++ ProblemSet_9/matplot.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+def main():
+    get_data()
+    #plot_data()
+
+# first, use random data... later arduino
+def get_data():
+    #while True:
+    templs = []
+    for _ in range(10):
+        temp = random.randrange(15, 25,)
+        templs.append(temp)
+        #print(templs)
+        #time.sleep(1)
