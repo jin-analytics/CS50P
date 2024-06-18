bccdb1995d524c67815eced8411e5d6d4ae0bc50
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-06-18 12:57:31 +0000

    Tue, Jun 18, 2024, 5:57 AM -07:00

--- ProblemSet_9/project/test_project.py
+++ ProblemSet_9/project/test_project.py
@@ -1,6 +1,8 @@
 import pytest
 from project import serial_data
 
+def main():
+    test_serial_data()
 
 
 def test_serial_data():
@@ -9,4 +11,5 @@ def test_serial_data():
     with pytest.raises(ModuleNotFoundError):
         serial_data(sPort, Baud)
 
-
+if __name__ == "__main__":
+    main()
