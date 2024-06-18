8bb2c217e7c61a89303a00d5a6b559b56723d12d
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-06-18 12:59:09 +0000

    Tue, Jun 18, 2024, 5:59 AM -07:00

--- ProblemSet_9/project/test_project.py
+++ ProblemSet_9/project/test_project.py
@@ -1,12 +1,12 @@
-import pytest
 from project import serial_data
+import pytest
 
 def main():
     test_serial_data()
 
 
 def test_serial_data():
-    sPort = "/dev/cu.usbmodem101_???"
+    sPort = "/dev/cu.usbmodem101"
     Baud = 9600
     with pytest.raises(ImportError):
         serial_data(sPort, Baud)
