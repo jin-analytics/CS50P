d6da9740ddd91319278e3bd3571e8b2926574d9e
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-06-18 12:51:41 +0000

    Tue, Jun 18, 2024, 5:51 AM -07:00

--- ProblemSet_9/project/test_project.py
+++ ProblemSet_9/project/test_project.py
@@ -1,21 +1,8 @@
 import pytest
 
 
-def test_init():
-    ...
-    #assert jar.size == 0
-
 
 def test_serial_data():
-    ...
-
-def test_function_1():
-    ...
+    assert serial_data()
 
 
-def test_function_2():
-    ...
-
-
-def test_function_n():
-    ...
