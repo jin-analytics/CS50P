ee6583e6d761ab17faa1b9027c05f5cab1701c19
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-07-11 16:33:26 +0000

    Thu, Jul 11, 2024, 9:33 AM -07:00

--- ProblemSet_9/project/test_project.py
+++ ProblemSet_9/project/test_project.py
@@ -5,11 +5,7 @@ def main():
     test_serial_data()
 
 
-def test_serial_data():
-    sPort = "/dev/cu.usbmodem101"
-    Baud = 9600
-    with pytest.raises(ImportError):
-        serial_data(sPort, Baud)
+def 
 
 if __name__ == "__main__":
     main()
