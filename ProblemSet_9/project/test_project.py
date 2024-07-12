4ee4c58b8b8d1f3a8238cb38047368f5d8e233b6
BoxAutomation <155905672+BoxAutomation@users.noreply.github.com>
2024-07-12 06:48:04 +0000

    Fri, Jul 12, 2024, 8:48 AM +02:00

--- ProblemSet_9/project/test_project.py
+++ ProblemSet_9/project/test_project.py
@@ -7,10 +7,19 @@ def main():
 def test_use_serialport():
     with pytest.raises(ConnectionError):
         use_serialport()
-def test_animation(frame):
-       assert frame = 4:
-       
-       ax1.set_xlim(left=frame - 3, right=frame + 1)
+
+def test_animation():
+    ...
+
+def test_csv_header():
+    ...
+
+def test_csv_file():
+     ...
+
+def test_serial_data():
+     ...
+
 
 
 if __name__ == "__main__":
